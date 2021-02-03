33314bdeeaf44d3f2fb9e3f5fcf2699b9250d742
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-03 21:21:24 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main - Entry point
  *
  * Return: Always 1
-*/
+ */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	write(2 ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ,58);
 	return (1);
 }
