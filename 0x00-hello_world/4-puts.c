036e7c04e2a339a0b8b50e12095f27e276695720
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-03 18:15:04 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - Entry point
 * 
@@ -6,6 +7,6 @@
 */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
