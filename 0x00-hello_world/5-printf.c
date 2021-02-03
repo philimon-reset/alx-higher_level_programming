b61179825496c5377440e2bae1129a8e4ebf9260
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-03 19:10:32 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-	printf("with proper grammar, but the outcome is a piece of art,");
-=======
 	printf("with proper grammar, but the outcome is a piece of art,\n");
->>>>>>> 408d8301f1989b73029213fcfb1874013a958109
 	return (0);
 }
