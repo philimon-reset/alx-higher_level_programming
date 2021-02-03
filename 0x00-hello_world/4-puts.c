613196718e7fe081ee1c1c09b9e13501ffba57b7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-03 18:13:17 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-/**
-* Write a C program that
-*prints exactly "Programming is like building
-*a multilingual puzzle, followed by a new line.
-*/
 #include <stdio.h>
+/**
+* main - Entry point
+* 
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
