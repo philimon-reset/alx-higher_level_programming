48166e3918dbd1daa3512e3bfa3d439bfa8a5a28
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-04 19:27:07 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	int lastDigit = n % 10;
 	
 	if (lastDigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
