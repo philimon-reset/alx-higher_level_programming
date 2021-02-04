65d570cfb65072fd9fae8758772cf2689e0af67e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-04 18:49:29 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/*
+
+/**
  * main - entry point
  *
  * Return: 0
@@ -13,8 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf('%d is positive\n',n);
+		printf("%d is positive\n",n);
+	else if (n == 0)
+		printf("%d is zero\n",n);
 	else
-		printf('%d is negative\n',n);
+		printf("%d is negative\n",n);
 	return (0);
 }
