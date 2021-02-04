be6fe218e595f59900bcf2718c374b329e8fe3aa
philimon-reset <philimonderib2019@gmail.com>
2021-02-04 17:14:46 +0000

    hi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l = l+1;
+	}
+	return (0);
+}
