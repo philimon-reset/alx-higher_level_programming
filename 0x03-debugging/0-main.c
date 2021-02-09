0404ef80684d50aeacc103db3beecce6dd877692
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-09 22:57:37 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,12 +4,10 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+        int i = 0;
 
-        i = 0;
         positive_or_negative(i);
 
         return (0);
