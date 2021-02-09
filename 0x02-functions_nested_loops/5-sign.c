35169f54cdd20a642095abf2f574c37b5d5c618a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-09 15:09:11 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,8 @@
 
 /**
  * print_sign - checks if greater, less than or equal to zero
- *
  *@n: character to be checked
+ *
  * Return: 1 if greater than 0
  * 0 if equal to 0
  * -1 if less than zero
