b5b13da0b69a781065382e2596456a1a50ec96be
philimon-reset <philimonderib2019@gmail.com>
2021-02-09 11:20:43 +0000

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,15 @@
  *
  * Return: null
  */
-void print_alphabet(void)                                                                                                                                                
-{                                                                                                                                                                         
-        char l;                                                                                                                                                           
-                                                                                                                                                                          
-        l = 'a';                                                                                                                                                          
-        while (l <= 'z')                                                                                                                                                  
-             	{                                                                                                                                                         
-                        _putchar(l);                                                                                                                                      
-                        l++;                                                                                                                                              
-                }                                                                                                                                                         
-        _putchar('\n');                                                                                                                                                   
-}   
+void print_alphabet(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l++;
+	{
+	_putchar('\n');
+}
