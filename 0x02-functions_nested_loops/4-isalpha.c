72bd5a749859926cfb60302c413f73a95be8d374
philimon-reset <philimonderib2019@gmail.com>
2021-02-09 11:55:17 +0000

    ew

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,24 @@
+#include "holberton.h"
+
 /**
- * main - entry point -> prints Holberton with line feed
+ * _isalpha - checks if its alpha char
  *
- * Return: 0
+ *@c: character to be checked
+ * Return: 1 if letter 0 if not
  */
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}		}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
