9d84b600c85920066e404949544c82be8f0fa0d7
philimon-reset <philimonderib2019@gmail.com>
2021-02-09 10:57:15 +0000

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,19 +9,17 @@ void print_alphabet_x10(void);
 {
 	char l;
 	int j;
-	
-	
+
 	j = 0;
 	while (j <= 10)
-{
-    l = 'a';
-    _putchar('\n');
-	    while (l <= 'z')
-    {
-		    _putchar(l); 
-		    l++;
-	
-    }
-	j++;
-}
+	{
+		l = 'a';
+		_putchar('\n');
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		j++;
+	}
 }
