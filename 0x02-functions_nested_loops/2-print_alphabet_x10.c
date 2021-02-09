214d864d8c3eb0ea6cc6d48ddcfc6a653bd3dc46
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-09 13:50:44 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,22 @@
 void print_alphabet_x10(void);
 {
 	char l;
-	int i;
-
-	while (i <= 10)
-	{
-		l = 'a';
-		_putchar('\n');
-	while (l <= "z");
-		{
-			_putchar(l);
-			l++;
-		}
-		i++;
-	}
+	int j;
+	
+	
+	j = 0;
+	while (j <= 10)
+{
+    l = 'a';
+    _putchar('\n');
+	    while (l <= 'z')
+    {
+		    _putchar(l); 
+		    l++;
+	
+    }
+	j++;
+}
+	_putchar('\n');
+	return (0);
 }
