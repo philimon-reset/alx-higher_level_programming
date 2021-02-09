1649b9d459d83f08e3cdcc1242cce32d1283430c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-09 22:30:16 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,29 +6,8 @@
  * @n: start of number
  * Return: void function
  */
-void print_to_98(int n)
+int print_to_98(int n)
 {
-	if (n > 98)
-	{
-		while (n >= 98)
-	{
-			if (n != 98)
-				printf("%d, ", n);
-			else
-				printf("%d", n);
-			n--;
-	}
-	}
-	else
-	{
-		while (n <= 98)
-	{
-			if (n != 98)
-				printf("%d, ", n);
-			else
-				printf("%d", n);
-			n++;
-	}
-	}
-	printf("\n");
+	int n = 1023;
+	
 }
