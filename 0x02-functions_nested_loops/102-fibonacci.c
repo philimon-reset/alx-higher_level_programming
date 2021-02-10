09d3fff573c26fa77b7378c2dee2630070427d98
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-10 20:09:33 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,32 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - psum of multiples of 3 and 5
+ * main - print the first 50 fibonacci characters
  *
  * Return: 0 if success
  */
 int main(void)
 {
-	int n, sum = 0;
+	int c;
+	long a;
+	long b;
+	long fibonacci;
 
-	for (n = 1023; n >= 0; n--)
+	while (c <= 49)
 	{
-		if (n % 3 == 0 || n % 5 == 0)
+		fibonacci = a + b;
+		if (c < 49)
 		{
-			sum += n;
+			printf("%d, ", fibonacci);
 		}
+		else
+		{
+			printf("%d", fibonacci);
+		}
+		a = b;
+		b = fibonacci;
+		c++;
 	}
-	printf("%d", sum);
 	printf("\n");
 	return (0);
 }
