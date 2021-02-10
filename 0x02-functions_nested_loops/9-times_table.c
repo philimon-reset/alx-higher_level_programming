4b68844278718d9f128ef329d15712133b8742bd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-10 12:24:23 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,29 +8,31 @@
  */
 void times_table(void)
 {
-	int a = 0, b = 0;
+	int a = 0, b = 0, l;
 
 	while (a < 10)
 	{
 		b = 0;
 		while (b < 10)
 		{
+			l = a * b;
 			if (b == 0)
 			{
 				_putchar('0');
 			}
-			else if (!(a * b > 9))
+			else if (!(l > 9))
 			{
 				_putchar(32);
 				_putchar(32);
 				_putchar(44);
-				_putchar('0' + a * b);
+				_putchar(l + '0');
 			}
-			else if (a * b > 9)
+			else if (l > 9)
 			{
 				_putchar(32);
 				_putchar(44);
-				_putchar('0' + a * b);
+				_putchar(l / 10 + '0');
+				_putchar(l % 10 + '0');
 			}
 		}
 	}
