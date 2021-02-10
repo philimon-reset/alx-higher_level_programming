a2cc374c90df1cd10ef0bf1d808ddca676270e28
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-10 21:46:53 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,11 +14,11 @@ int main(void)
 		fibonacci = a + b;
 		if (c < 98)
 		{
-			printf("%uld, ", fibonacci);
+			printf("%lud, ", fibonacci);
 		}
 		else
 		{
-			printf("%uld", fibonacci);
+			printf("%lud", fibonacci);
 		}
 		a = b;
 		b = fibonacci;
