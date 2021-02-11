6daf258230f093d7d4ff56a84065052da76b3730
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 20:19:35 +0300

    Update holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <math.h>
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,5 +14,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-
 void print_triangle(int size);
+void print_number(int n);
+
+#endif /*HOLBERTON_H*/
