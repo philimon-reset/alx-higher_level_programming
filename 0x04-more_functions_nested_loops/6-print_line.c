adf890cb9b30e29e745e8d4adc43cea6523fd2ce
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 20:54:27 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0);
-		{
-			for (i = 0; i <= n; i++)
-			{
-				_putchar(95);
-			}
-		}
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
 	_putchar('\n');
 }
