546b17d5abb7752d4a1a85f800c8c63331766473
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 22:13:24 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,17 +8,17 @@
  */
 void print_square(int size)
 {
-	int i = size, j = size;
+	int i, j;
 
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
-    {
-      for (j = 1; j <= size; j++)
-      {
-        _putchar('#');
-      }
-    }
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+		}
 	}
 	else
 	{
