ca9a64d0732d7c4df69f7996cf70c7549f0f4a2c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 23:07:36 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,21 @@
+#include "holberton.h"
 
+/**
+ * main - prints triangle in terminal
+ *
+ * @size: size of triangle
+ * Return: void functions
+ */
+int main(void)
+{
+	int size, i, j;
+
+	if (size > 0)
+	{
+    
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
