f49cfbe4894c0cda3a743e93dc9ea7506845d9e5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 22:22:56 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,29 @@
+#include "holberton.h"
 
+/**
+ * print_square - prints diagonal line in terminal
+ *
+ * @size: size of square
+ * Return: void functions
+ */
+int main(void)
+{
+	int output = "", i;
+
+	for (i = 0; i <= 100; i++)
+  {
+    if (i % 3)
+    {
+      output += "Fizz";
+    }
+    else if (i % 5)
+    {
+      output += "Buzz";
+    }
+    else if (output == "")
+    {
+      output = i;
+    }
+    printf(output);
+  }
+}
