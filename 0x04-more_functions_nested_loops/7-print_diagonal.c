fe87531509d96e496799c161b80d864ad761ae2f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 21:19:50 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,9 +10,9 @@ void print_numbers(int n)
 {
 	int i, j;
 
-	for (i = '0'; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = '0'; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			if (j == i)
 			{
