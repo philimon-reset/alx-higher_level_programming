ffc68e82d8b6a18ee07e640b2591e292c04420a0
philimon-reset <philimonderib2019@gmail.com>
2021-02-11 16:31:39 +0000

    ew

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,2 +1,22 @@
 #include "holberton.h"
-#include <stdio.h>
+
+/**
+ * _isupper - prints alphabet
+ *
+ * @c: charcter checked if its upper
+ * Return: 1 if its upper
+ * 0 if not
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
