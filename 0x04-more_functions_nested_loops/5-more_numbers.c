c63de57fb426abfaabe22501a2d0abf73b9a6797
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 21:25:27 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = '0'; j <= 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
-		for (i = '0'; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i <= 9)
 			{
@@ -20,7 +20,7 @@ void more_numbers(void)
 			else
 			{
 				_putchar(i / 10 + '0');
-				
+				_putchar(i % 10 + '0');
 			}
 		}
 		_putchar('\n');
