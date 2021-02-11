82ff735fd5117d9ac79cf10938cbd40f879190ed
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 22:10:12 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,27 @@
+#include "holberton.h"
 
+/**
+ * print_square - prints diagonal line in terminal
+ *
+ * @size: size of square
+ * Return: void functions
+ */
+void print_square(int size)
+{
+	int i = size, j = size;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+    {
+      for (j = 1; j <= size; j++)
+      {
+        _putchar('#');
+      }
+    }
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
