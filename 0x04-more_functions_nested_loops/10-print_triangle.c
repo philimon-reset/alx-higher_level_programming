2c886ab55cfb003658e824fcc538bc45922cc0e4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-11 23:23:51 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,20 +8,22 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, l;
 
 	if (size > 0)
 	{
 		for (i = 1; i <= size; ++i)
 		{
-			for (j = 1; j <= i; ++j)
+			for (j = 1; j <= size - i; ++j)
 			{
 				_putchar(" ");
 			}
-			_putchar("#");
-			_putchar("\n");
+			for (l = 0; l <= i; l++)
+			{
+				_putchar("#");
+				_putchar("\n");
+			}
 		}
-	}
 	}
 	else
 	{
