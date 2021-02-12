3ddac9cf269aab139fb0515b000ed76be1ec956c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-12 14:27:45 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
-#include <stdio.h>                                                                                                                                                        
-#include <math.h>                                                                                                                                                         
-                                                                                                                                                                          
-/**                                                                                                                                                                       
- * main - find the largest prime factor of 612852475143                                                                                                                   
- *                                                                                                                                                                        
- * Return: 0                                                                                                                                                              
- */                                                                                                                                                                       
-int main(void)                                                                                                                                                            
-{                                                                                                                                                                         
-        long n = 612852475143;                                                                                                                                            
-        int s = (int) sqrt(n);                                                                                                                                            
-                                                                                                                                                                          
-        for (; s < n; s++)                                                                                                                                                
-        {                                                                                                                                                                 
-                if (n % s == 0)                                                                                                                                           
-                {                                                                                                                                                         
-                        printf("%d\n", s);                                                                                                                                
-                        break;                                                                                                                                            
-                }                                                                                                                                                         
-        }                                                                                                                                                                 
-        return (0);                                                                                                                                                       
-}   
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - find the largest prime factor of 612852475143
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  long n = 612852475143;
+  int s = (int) sqrt(n);
+
+  for (; s < n; s++)
+  {
+     if (n % s == 0)
+     {
+       printf("%d\n", s);
+       break;
+     }
+  }
+  return (0);
+}
