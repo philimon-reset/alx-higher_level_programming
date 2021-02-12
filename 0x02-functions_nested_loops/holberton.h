71834c328a4d538830e1fe42a6d7b0417a167ae3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-12 20:24:56 +0300

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,6 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void print_tables(int n);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
