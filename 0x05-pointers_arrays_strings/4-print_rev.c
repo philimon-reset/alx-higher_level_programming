3e155e6b4a481f668d6c3a7f38cd023b0579bc0a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-16 01:33:20 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,12 @@
  * @s: string to be reversed
  *
  * Return: void funtion
- */
-
+ **/
 void print_rev(char *s)
 {
 	int var;
 
-	lent = _strlen(*s);
+	lent = _strlen(s);
 	for (var = lent - 1; var >= 0; var--)
 	{
 		_putchar(s[var]);
