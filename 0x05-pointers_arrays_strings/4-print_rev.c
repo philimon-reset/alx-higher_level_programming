ee3d3543690d87abff32cc294efbee9ea54e5227
philimon-reset <philimonderib2019@gmail.com>
2021-02-15 22:31:18 +0000

    ew

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_rev - print in reverse
+ * @s: string to be reversed
+ *
+ * Return: void funtion
+ */
+
+void print_rev(char *s)
+{
+	int var;
+
+	lent = _strlen(*s);
+	for (var = lent - 1; var >= 0; var--)
+	{
+		_putchar(s[var]);
+	}
+	_putchar('\n');
+}
