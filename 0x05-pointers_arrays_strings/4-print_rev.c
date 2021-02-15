b2eea13ec7c138ff4b48208456263e6cf0b2bdf7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-16 02:06:59 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -39,3 +39,20 @@ int _strlen(char *s)
 	}
 	return (output);
 }
+
+/**
+ * swap_int - swap integers of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void funtion
+ */
+
+void swap_int(int *a, int *b)
+{
+	int var;
+
+	var = *a;
+	*a = *b;
+	*b = var;
+}
