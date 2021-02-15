228cce05decd931a16c0115a63f21141e8e32ab4
philimon-reset <philimonderib2019@gmail.com>
2021-02-15 21:57:16 +0000

    asd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts - print character to stdout
+ * @str: string to be printed
+ *
+ * Return: void funtion
+ */
+
+void _puts(char *str)
+{
+	int var;
+
+	var = *str;
+	_putchar(var + '0')
+}
