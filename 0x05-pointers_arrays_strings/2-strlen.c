7abd6ab42b66f3540e642433b175fa347eede7a1
philimon-reset <philimonderib2019@gmail.com>
2021-02-15 21:41:21 +0000

    as

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of a a string
+ * @s: string to be checked
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int var;
+	s[] = *s
+
+		for (var = 0; s[var] != '\0'; var++)
+			return (var);
+}
