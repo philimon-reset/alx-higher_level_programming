7f27804455463a0395a36258cacfa0f20364a130
philimon-reset <philimonderib2019@gmail.com>
2021-02-17 22:25:23 +0000

    we

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,4 +10,16 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j, output;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		for (j = 0; *(dest + j) != '\0'; j++)
+		{
+			if (*(dest + j) == '\0')
+			{
+				*(dest + j) = *(src);
+			}
+			
+	}
 }
