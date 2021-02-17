26b313ab0aed58e1c33696df68ef6d5104357eae
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-18 00:56:32 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destenation of addition
+ * @src: string to be added
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
