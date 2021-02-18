6fd34e4b8b3629ebe727a0f1d268b0ab16962ff5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-18 23:35:25 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,6 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	char arr[] = {',', ';', '.', '!', '?', '"', '(', ')', '{',
-		      '}', '\t', '\n', ' '};
 
 	if (s[i] >= 'a' && s[i] <= 'z')
 	{
@@ -20,11 +18,8 @@ char *cap_string(char *s)
 	}
 	while (s[i] != '\0')
 	{
-		if (separator(s[i]))
-		{
-			s[i + 1] -= 32;
-		}
-		else if ((str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+		if (separator(s[i]) && (str[i + 1] >= 'a' && 
+					str[i + 1] <= 'z'))
 		{
 			s[i + 1] -= 32;
 		}
@@ -33,7 +28,7 @@ char *cap_string(char *s)
 	return (s)
 }
 
-int separator (char c)
+int separator(char c)
 {
 	char arr[13] = {',', ';', '.', '!', '?', '"', '(', ')', '{',
 		      '}', '\t', '\n', ' '};
