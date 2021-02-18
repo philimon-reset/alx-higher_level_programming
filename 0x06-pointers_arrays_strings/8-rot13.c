6c825e269c36d3f19f7fd39f01106411a3eeb6e3
philimon-reset <philimonderib2019@gmail.com>
2021-02-18 20:07:18 +0000

    as

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - function to reverse string
+ * @a: array to be reversed
+ * @n: number of elements of array
+ *
+ * Return: void function
+ */
+
+void reverse_array(int *a, int n)
+{
+	int j = 0;
+
+	n--;
+	while (j < n)
+	{
+		swap_int(a + n, a + j);
+		n--;
+		j++;
+	}
+}
+
+/**
+ * swap_int - swap integers of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: void funtion
+ */
+
+void swap_int(int *a, int *b)
+{
+	int var;
+
+	var = *a;
+	*a = *b;
+	*b = var;
+}
