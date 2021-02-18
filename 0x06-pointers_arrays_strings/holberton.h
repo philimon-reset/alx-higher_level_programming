e8bbdbd8fcfaf7246377131578802acb7f9e3d22
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-18 23:43:27 +0300

    Update holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +13,4 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *rot13(char *s);
 int separator(char c);
+#endif /*HOLBERTON_H*/
