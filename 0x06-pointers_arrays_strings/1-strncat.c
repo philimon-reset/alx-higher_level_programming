1983d6a51b312e90ace9d62a6d7cce405b8d7be8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-18 21:25:05 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,7 +23,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	if (output2 > n)
 	{
-		for (j = 0; j < n'; j++)
+		for (j = 0; j < n; j++)
 		     {
 			     dest[output + j] = src[j];
 		     }
