deffb3fb00e93951760974b3e94e67662439eb33
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-22 22:30:49 +0300

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,6 +2,8 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
+#include "string.h"
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
