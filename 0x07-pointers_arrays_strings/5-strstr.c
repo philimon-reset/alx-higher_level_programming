aaf91ebfac2c53727e0d9f562419693753b5c838
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-22 23:20:39 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,84 +11,34 @@
 char *_strstr(char *haystack, char *needle);
 {
 	int j = 0, i = 0;
-	size_t  len = 0;
+	int  len = 0;
 
-	for (; needle[i] != '\0')
+	if (*needle == '\0')
 	{
-		len += 1;
+		return (haystack);
 	}
-	while (*haystack)
+	
+	while (*haystack != '\0')
 	{
-		if (*haystack == *needle)
+		int i;
+
+		while (needle[i] != '\0')
 		{
-			if (!_strcmp(haystack, needle))
+			if (haystack[i + j] != needle[i])
 			{
-				return ((char *)haystack);
+				len = 0;
+				break;
 			}
-			haystack++;
+			else
+				len = 1;
+			i++;
 		}
-		return (NULL);
-}
-
-
-/**
- * _strchr - function that fills memory with a constant byte.
- * @s: destenation of string
- * @c: character to replace
- *
- * Return: a pointer to the resulting string
- */
-
-char *_strchr(char *s, char c)
-{
-	int j = 0;
-
-	for (; s[j] != '\0' && s[j] != c; j++)
-	{
-		continue;
+		if (len)
+			break;
 	}
-	if (s[j] == c)
-		return (s + j);
+	if (len)
+		return (haystack + j);
 	else
 		return (NULL);
-}
-
-/**
- * _strcmp - function compares two strings
- * @s1: destenation
- * @s2: string to be compared
- *
- * Return: difference
- */
-int _strcmp(char *s1, char *s2)
-{
-	int j = 0, var = 0;
-
-	while (1)
-	{
-		if (s1[j] == '\0' && s2[j] == '\0')
-		{
-			break;
-		}
-		else if (s1[j] != s2[j])
-		{
-			var = s1[j] - s2[j];
-			break;
-		}
-		else if (s1[j] == '\0')
-		{
-			var = s2[j];
-			break;
-		}
-		else if (s2[j] == '\0')
-		{
-			var = s1[j];
-			break;
-		}
-		else
-		{
-			j++;
-		}
 	}
-	return (var);
-}
+		return (NULL);
