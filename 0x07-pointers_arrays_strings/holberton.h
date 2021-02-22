a2c8c3511bafc9f66d45eb28130ee216399752d7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-22 22:51:40 +0300

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -14,4 +14,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+int _strcmp(char *s1, char *s2);
 #endif /*HOLBERTON_H*/
