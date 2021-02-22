04164f5de2fd0bae5cdfc05816ecd9513e4c4c0d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-22 21:53:23 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,34 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int j = 0, output = 0;
 
-	for (; s[j] != '\0' && s[j] != accept[j]; j++)
+	for (; s[j] != '\0'; j++)
+	{
+		if (_strchr(s, accept) == NULL)
+		{
+			output++;
+		}
+	}
+	return (output);
+}
+
+/**
+ * _strchr - function that fills memory with a constant byte.
+ * @s: destenation of string
+ * @c: character to replace
+ *
+ * Return: a pointer to the resulting string
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int j = 0;
+
+	for (; s[j] != '\0' && s[j] != c; j++)
 	{
 		continue;
 	}
-	if (s[j] == accept[j])
-	{
-		output++;
-	}
-	else (s[j] != accept[j])
-	{
-		output++;
-	}
-	return (output);
+	if (s[j] == c)
+		return (s + j);
+	else
+		return (NULL);
 }
