5dce0d2b0d682415e80bc74780b25661ee6391d2
philimon-reset <philimonderib2019@gmail.com>
2021-02-24 12:10:52 +0000

    as

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * checker - find is its prime
+ * @i: number to be checked
+ * @c: original input
+ *
+ * Return: 1 if prime 0 if not
+ */
+
+/**
+ * wildcmp - compare two strings and returns 1 if identical
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if identical otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int j = 0;
+
+	if (s1
+}
