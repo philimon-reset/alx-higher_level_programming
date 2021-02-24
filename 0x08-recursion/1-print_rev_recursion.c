3dceb00bf84e1307f066bfc27705430d94c46c5c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 12:46:21 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,11 +13,11 @@ void _print_rev_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		return;
 	}
 	else
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(s[j]);
 	}
 }
