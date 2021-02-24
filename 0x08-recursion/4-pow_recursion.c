5d19e0c237edd981d62a2e8f9678a5efd173d188
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 12:49:20 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,15 +10,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	int j = 0;
-
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	return (0);
+	return (x * _pow_recursion(x, y - 1));
 }
