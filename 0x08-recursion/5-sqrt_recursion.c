8a0dea4ee657818395d6cdcf2d98edcbedcc6497
philimon-reset <philimonderib2019@gmail.com>
2021-02-24 06:45:44 +0000

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,39 @@
-B#include "holberton.h"
+#include "holberton.h"
 
 /**
- * factorial - function to print factorial of n
+ * square_root: find square root to the nearest round
+ * @i: number to be checked
+ * @c: original input
+ *
+ * Return: sqaure root
+ */
+int square_root(int i, int c)
+{
+	if (i * i == c)
+		return (i);
+	else if (i * i > c)
+		return (i - 1);
+	else
+		return (square_root(i + 1, c));
+}
+
+/**
+ * _sqrt_recursion - function to print square root of n
  * @n: integer to be checked
  *
- * Return: factorial of n
+ * Return: square root of n
  */
 
-int factorial(int n)
+int _sqrt_recursion(int n)
 {
 	int j = 0;
 
 	if (n < 0)
 		return (-1);
-	if (n == '0')
+	if (n == 0)
 		return (1);
 	else
 	{
-		return (n * factorial(n - 1));
+		return (square_root(j, n));
 	}
-	return (0);
 }
