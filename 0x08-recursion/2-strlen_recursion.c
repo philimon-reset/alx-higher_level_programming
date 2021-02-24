b6380e88af1c96356aa0f9ff48fffc1fd8c133be
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 12:31:15 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,16 +9,7 @@
 
 int _strlen_recursion(char *s)
 {
-	int j = 0, output = 1;
-
-	if (s[j] == '\0')
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-		_putchar('\n');
-	}
-	return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
