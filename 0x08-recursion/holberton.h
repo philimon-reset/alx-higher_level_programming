35c8e349e3b0efb8f78be6bee7777a659d4b9f2c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 12:58:01 +0300

    Update holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -3,15 +3,15 @@
 
 #include <stdio.h>
 
-int _putchar(char a);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
-int square_root(int i, int c);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int square_root(int i, int c);
 int checker(int i, int c);
 
 #endif /*HOLBERTON_H*/
