699b97839ef1abfc4f24129554dfda2aeb00dff3
philimon-reset <philimonderib2019@gmail.com>
2021-02-24 18:50:41 +0000

    fix

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,12 +14,11 @@ int wildcmp(char *s1, char *s2)
 
 	if (*(s1 + j) == '\0' && *(s2 + j) == '\0')
 		return (0);
-	else if (*(s1 + j) == *(s2 + j) && (*(s1 + j) = '\0' && *(s1 + j) = '\0'))
-		return wildcmp(s1 + 1, s2 + 2);
+	if (*(s1 + j) == *(s2 + j) && (*(s1 + j) == '\0' && *(s1 + j) == '\0'))
+		return (wildcmp(s1 + 1, s2 + 2));
 	if (*(s2 + j) == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
 	return (0);
-		
 }
