13d9a9a556de29a8ae0304de4180989d03f3c397
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 21:14:22 +0300

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,14 +1,6 @@
 #include "holberton.h"
 
 /**
- * checker - find is its prime
- * @i: number to be checked
- * @c: original input
- *
- * Return: 1 if prime 0 if not
- */
-
-/**
  * wildcmp - compare two strings and returns 1 if identical
  * @s1: first string
  * @s2: second string
@@ -20,5 +12,13 @@ int wildcmp(char *s1, char *s2)
 {
 	int j = 0;
 
-	if (s1
+	if (*(s1 + j) = '\0' && *(s2 + j) = '\0')
+		return (0);
+	else if (*(s1 + j) = *(s2 + j) && (*(s1 + j) = '\0' && *(s1 + j)))
+		return wildcmp(s1 + 1, s2 + 2);
+	if (*(s2 + j) == '*')
+	{
+		wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)
+	}
+		
 }
