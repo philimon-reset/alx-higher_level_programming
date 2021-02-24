69e8fa8d0507cd261a645ed94032f2feaddfec07
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-24 22:29:34 +0300

    Update 100-wildcmp.c

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -15,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 	if (*(s1 + j) == '\0' && *(s2 + j) == '\0')
 		return (1);
 	if (*(s1 + j) == *(s2 + j))
-		return (*(s1 + j) != '\0' && wildcmp(s1 + 1, s2 + 1));
+		return (1 && wildcmp(s1 + 1, s2 + 1));
 	if (*(s2 + j) == '*')
 		return (wildcmp(s1, s2 + 1) || (*(s1 + j) != '\0' && wildcmp(s1 + 1, s2)));
 	return (0);
