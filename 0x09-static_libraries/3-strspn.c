ed7ea9a2a03756f602ac814eee7ca83a8c4c307f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-27 13:58:16 +0300

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -10,5 +10,37 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	return (0);
+	int j = 0, output = 0;
+
+	for (; s[j] != '\0'; j++)
+	{
+		if (_strchr(accept, s[j]) == NULL)
+		{
+			break;
+		}
+		output++;
+	}
+	return (output);
+}
+
+/**
+ * _strchr - function that fills memory with a constant byte.
+ * @s: destenation of string
+ * @c: character to replace
+ *
+ * Return: a pointer to the resulting string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	for (; s[j] != '\0' && s[j] != c; j++)
+	{
+		continue;
+	}
+	if (s[j] == c)
+		return (s + j);
+	else
+		return (NULL);
 }
