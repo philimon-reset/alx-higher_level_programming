ee58ed38ba57d2a348172fb50d6e9b5c4738ee97
philimon-reset <philimonderib2019@gmail.com>
2021-02-27 09:22:16 +0000

    hello

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -10,37 +10,5 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int j = 0;
-
-	for (; s[j] != '\0' && _strchr(accept, s[j]) == NULL; j++)
-	{
-		continue;
-	}
-	if (s[j] == '\0')
-		return (NULL);
-	else
-		return (s + j);
-}
-
-
-/**
- * _strchr - function that fills memory with a constant byte.
- * @s: destenation of string
- * @c: character to replace
- *
- * Return: a pointer to the resulting string
- */
-
-char *_strchr(char *s, char c)
-{
-	int j = 0;
-
-	for (; s[j] != '\0' && s[j] != c; j++)
-	{
-		continue;
-	}
-	if (s[j] == c)
-		return (s + j);
-	else
-		return (NULL);
+	return (0);
 }
