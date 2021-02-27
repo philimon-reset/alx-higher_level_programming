005477fc15c1c9cd0f7b306a56681abecf558e68
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-27 13:57:03 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -34,3 +34,17 @@ int main(int argc, char **argv)
 	}
 	return (0);
 }
+
+/**
+ * _isdigit - check if didigt
+ *
+ * @c: charcter checked if its digit
+ * Return: 1 if its digit
+ * 0 if not
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
