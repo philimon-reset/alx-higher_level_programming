044245a70de6bc5f3fe45e99ac85563fbbb80958
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-28 23:17:50 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -22,26 +22,16 @@ int main(int argc, char **argv)
 	sum = atoi(argv[1]);
 	for (n = 0; sum > 0; n++)
 	{
-		if (sum >= 25)
-		{
+		if (sum - 25 >= 0)
 			sum = sum - 25;
-		}
-		else if (sum >= 10)
-		{
+		else if (sum - 10 >= 0)
 			sum = sum - 10;
-		}
-		else if (sum >= 5)
-		{
+		else if (sum - 5 >= 0)
 			sum = sum - 5;
-		}
-		else if (sum >= 2)
-		{
+		else if (sum - 2 >= 0)
 			sum = sum - 2;
-		}
-		else if (sum >= 1)
-		{
+		else if (sum - 1 >= 0)
 			sum = sum - 1;
-		}
 	}
 	printf("%d\n", sum);
 	return (0);
