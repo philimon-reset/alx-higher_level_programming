7194a8906ff36c72991d261fa163af720a1dd353
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-28 23:10:41 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,29 +12,44 @@
 
 int main(int argc, char **argv)
 {
-	int total, change;
+	int sum, n;
 
-	if (argc < 2)
+	if (argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	change = atoi(argv[1]);
-
-	for (total = 0; change > 0; total++)
+	sum = atoi(argv[1]);
+	if (sum > 0)
+		printf("0\n");
+	while (a > 0)
 	{
-		if (change - 25 >= 0)
-			change = change - 25;
-		else if (change - 10 >= 0)
-			change = change - 10;
-		else if (change - 5 >= 0)
-			change = change - 5;
-		else if (change - 2 >= 0)
-			change = change - 2;
-		else if (change - 1 >= 0)
-			change = change - 1;
+		if (a >= 25)
+		{
+			n++;
+			a = a - 25;
+		}
+		else if (a >= 10)
+		{
+			n++;
+			a = a - 10;
+		}
+		else if (a >= 5)
+		{
+			n++;
+			a = a - 5;
+		}
+		else if (a >= 2)
+		{
+			n++;
+			a = a - 2;
+		}
+		else if (a >= 1)
+		{
+			n++;
+			a = a - 1;
+		}
 	}
-	printf("%d\n", total);
+	printf("%d\n", sum);
 	return (0);
 }
