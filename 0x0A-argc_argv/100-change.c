1a13f81198386718b6c3c538e7b146a4e741107d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-28 20:53:23 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,9 +8,11 @@
  * Return: 0
  */
 
-int main(int argc, char **argv))
+int main(int argc, char **argv)
 {
-	int sum;
+	int a;
+	int coin[4] = {25, 10, 5, 1};
+	int i = 1, n = 0;
 
 	if (argc < 2)
 	{
@@ -18,53 +20,25 @@ int main(int argc, char **argv))
 		return (1);
 	}
 	
-	sum = atoi(argv[1]);
-	if (sum < 0)
-	{
+	a = atoi(argv[1]);
+	if (a < 0)
 		printf("0\n");
-	}
-	printf("%d", greed(sum));
-	return (0);
-	
-}
-
-/**
- * main - main function
- * @argc: number of arguments
- * @argv: array of pointers
- *
- * Return: 0
- */
-
-int greed(int a)
-{
-	int coin[4] = {25, 10, 5, 1};
-	int i = 0, n = 0;
 	
 	while (a >= 25)
 	{
-		n++;
-		a = a - 25;
+	    n++;
+	    a = a - 25;
 	}
 	
-	while (a > 0)
+	while (i < 4)
 	{
-		if (a >= 10)
+		if (coin[i] <= a)
 		{
 			n++;
-			a = a - 10;
-		}
-		if (a >= 5)
-		{
-			n++;
-			a = a - 5
-		}
-		if (a >= 1)
-		{
-			n++;
-			a = a - 1;
+			a = a - n * coin[i];
 		}
 		i++;
 	}
-	return (n);
+	printf("%d", n);
+	return (0);
 }
