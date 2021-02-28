ecdfb8cdd89a3346dbe6c49b59c8313c7d7779fd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-28 23:22:03 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,3 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,33 +5,34 @@
  * main - main function
  * @argc: number of arguments
  * @argv: array of pointers
- *
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
-	int sum, n;
+	int total, change;
 
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	sum = atoi(argv[1]);
-	for (n = 0; sum > 0; n++)
+	
+	change = atoi(argv[1]);
+	
+	for (total = 0; change > 0; total++)
 	{
-		if (sum - 25 >= 0)
-			sum = sum - 25;
-		else if (sum - 10 >= 0)
-			sum = sum - 10;
-		else if (sum - 5 >= 0)
-			sum = sum - 5;
-		else if (sum - 2 >= 0)
-			sum = sum - 2;
-		else if (sum - 1 >= 0)
-			sum = sum - 1;
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", change);
 	return (0);
 }
