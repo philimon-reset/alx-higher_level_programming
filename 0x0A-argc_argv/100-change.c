269ccab5746a77af3e84a674a68a6e3e87318807
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-02-28 21:30:48 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,47 +15,39 @@ int main(int argc, char **argv)
 	int i = 1, n = 0;
 
 	if (argc < 2)
-	{
 		printf("Error\n");
 		return (1);
-	}
-
 	a = atoi(argv[1]);
 	if (a < 0)
 		printf("0\n");
-
-	else if (argc == 2)
+	while (a >= 25)
 	{
-		while (a >= 25)
+		n++;
+		a = a - 25;
+	}
+	while (a > 0)
+	{
+		if (coin[i] <= a)
 		{
 			n++;
-			a = a - 25;
+			a = a - coin[i];
 		}
-
-		while (a > 0)
+		if (coin[i + 1] <= a)
 		{
-			if (coin[i] <= a)
-			{
-				n++;
-				a = a - coin[i];
-			}
-			if (coin[i + 1] <= a)
-			{
-				n++;
-				a = a - coin[i + 1];
-			}
-			if (coin[i + 2] <= a)
-			{
-				n++;
-				a = a - coin[i + 2];
-			}
-			if (coin[i + 3] <= a)
-			{
-				n++;
-				a = a - coin[i + 3];
-			}
-			a--;
+			n++;
+			a = a - coin[i + 1];
 		}
+		if (coin[i + 2] <= a)
+		{
+			n++;
+			a = a - coin[i + 2];
+		}
+		if (coin[i + 3] <= a)
+		{
+			n++;
+			a = a - coin[i + 3];
+		}
+		a--;
 	}
 	printf("%d", n);
 	return (0);
