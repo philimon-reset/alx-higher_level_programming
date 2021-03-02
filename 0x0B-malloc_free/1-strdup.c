b995cee9c766b39ae38b998ea632395aa51cd901
philimon-reset <philimonderib2019@gmail.com>
2021-03-02 17:58:53 +0000

    start

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * create_array - function to create array
+ * @size: size of array
+ * @c: specific char to initaliize
+ *
+ * Return: NULL if size 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int j = 0;
+	char *str;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+        else if (str != NULL)
+	{
+		for (; j < size; j++)
+		{
+			str[j] = 'H';
+		}
+		return (str);
+	}
+	else
+		return (NULL);
+	return (0);
+}
