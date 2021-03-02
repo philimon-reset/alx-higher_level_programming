a2bc32602c8b43e5a2d2f71f8861644004eca6a8
philimon-reset <philimonderib2019@gmail.com>
2021-03-02 11:36:44 +0000

    add

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,11 @@
+B#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limit.h>
+
+int _putchar(char c);
+char *create_array(unsigined int size, char c);
+
+#endif /*HOLBERTON_H*/
