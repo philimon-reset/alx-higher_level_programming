1733e45a572b82705105b47cb95eb042f679a2ed
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 17:28:18 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 	ptr = malloc();
 	if (ptr != NULL)
 	{
