31b8d84c9dcd6dae0649abf8c1e2c54178afae67
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 20:27:18 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,11 +10,13 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int j = 0, a = _strlen(s1);
+	int j = 0, a = _strlen(s1), b = _strlen(s2);
 	unsigned int i = 0;
 	char *str;
 
 	str = malloc(a + n + 1);
+	if (n >= b)
+		return (s2);
 	if (str != NULL)
 	{
 		for (; j < a; j++)
