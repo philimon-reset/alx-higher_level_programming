59877fc78bd28092bcbd3b6e245d5ebd9a309d40
philimon-reset <philimonderib2019@gmail.com>
2021-03-04 12:34:09 +0000

    2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+
+/**
+ * str_concat - function to concatinate two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL if size 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int j = 0, i = 0, a = _strlen(s1), b = _strlen(s2);
+	char *str;
+
+	str = malloc(a + b + 1);
+	if (str != NULL)
+	{
+		for (; j < a; j++)
+		{
+			str[j] = s1[j];
+		}
+		for (; i < b; i++)
+		{
+			str[i + j] = s2[i];
+		}
+		str[i + j] = '\0';
+	}
+	else
+		return (NULL);
+	return (str);
+}
+
+/**
+ * _strlen - return the length of a a string
+ * @s: string to be checked
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int var;
+	int output = 0;
+
+	if (s != NULL)
+	{
+		for (var = 0; s[var] != '\0'; var++)
+		{
+			output += 1;
+		}
+	}
+	return (output);
+}
