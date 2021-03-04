ce7bca7e2961a8795553bb2addc7f49a6eb967e9
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 21:06:14 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = (max - min) + 1;
+	size = (max - min);
 	array = malloc(sizeof(*array) * size);
 	if (array != NULL)
 	{
