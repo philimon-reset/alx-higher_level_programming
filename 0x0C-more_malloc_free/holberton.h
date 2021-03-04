c5a5d8a2ecf83e561735f2d4d557050407f43284
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 17:06:21 +0300

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -10,5 +10,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
 
 #endif /*HOLBERTON_H*/
