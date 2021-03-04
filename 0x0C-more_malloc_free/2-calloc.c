687b71867b226db867aa040f8889a5b3ca350cef
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 20:50:46 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int j = 0;
 	char *array;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	array = malloc(nmemb * size);
 	if (array != NULL)
