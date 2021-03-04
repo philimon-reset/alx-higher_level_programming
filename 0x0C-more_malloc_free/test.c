c3e26357a3b2566fb67d8e30bbfe174a6b8a6a6c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 20:29:32 +0300

    Update test.c

--- 0x0C-more_malloc_free/test.c
+++ 0x0C-more_malloc_free/test.c
@@ -2,36 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void simple_print_buffer(char *buffer, unsigned int size);
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -39,13 +9,10 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char *a;
+    char *concat;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Holberton");
-    strcpy(a + 9, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
-    free(a);
+    concat = string_nconcat("Holberton ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
     return (0);
 }
