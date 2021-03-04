d16c109a7febe36256bbe428fba145affc3ab4e1
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-04 21:02:09 +0300

    Update test.c

--- 0x0C-more_malloc_free/test.c
+++ 0x0C-more_malloc_free/test.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
+
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
@@ -10,7 +11,7 @@ void simple_print_buffer(char *buffer, unsigned int size);
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
     unsigned int i;
 
@@ -38,13 +39,10 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char *a;
+    int *a;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Holberton");
-    strcpy(a + 9, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
     free(a);
     return (0);
 }
