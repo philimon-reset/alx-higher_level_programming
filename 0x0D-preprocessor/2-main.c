87bd2eb9d88d0a75972c9d0324de3a42895a4fbc
philimon-reset <philimonderib2019@gmail.com>
2021-03-08 11:43:22 +0000

    add

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *(argv + 0));
+	return (0);
+}
