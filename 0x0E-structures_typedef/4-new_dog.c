32f39f9a2fca932eadfa1b6041cd8c283cb2e771
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-08 22:24:54 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,52 @@
-#include "holberton.h"
+#include "dog.h"
 
 /**
- * main - main function
- * @argc: number of arguments
- * @argv: array of pointers
+ * print_dog - print struct dog
+ * @d: pointer to struct dog
  *
- * Return: 0
+ * Return: void function
  */
 
-int main(int argc __attribute__((unused)), char **argv)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	printf("%s\n", *(argv + 0));
-	return (0);
+	int a = _strlen(name), b = _strlen(owner);
+	char *cpy_name, *cpy_owner;
+	struct dog *temp;
+
+	cpy_name = malloc(sizeof(char *) * (a + 1));
+	cpy_owner = malloc(sizeof(char *) * (b + 1));
+	temp = malloc(sizeof(struct *));
+	if ((*cpy_name != NULL) && (*cpy_owner != NULL))
+	{
+		(*temp).name = name;
+		(*temp).age = age;
+		(*temp).owner = owner;
+	}
+	else
+	{
+		free(cpy_name);
+		free(cpy_owner);
+		free(temp);
+		return (NULL);
+	}
+	return (temp);
+}
+
+/**
+ * _strlen - return the length of a a string
+ * @s: string to be checked
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int var;
+	int output = 0;
+
+	for (var = 0; s[var] != '\0'; var++)
+	{
+		output += 1;
+	}
+	return (output);
 }
