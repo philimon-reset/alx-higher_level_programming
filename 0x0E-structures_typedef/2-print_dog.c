54cc276551076c40794070c2965fb33a62939c00
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-08 22:06:22 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,12 @@
 
 void print_dog(struct dog *d)
 {
-	if ((*d).name = NULL)
-		(*d).name = "(nil)";
-	if ((*d).owner = NULL)
-		(*d).owner = "(nil)";
+	if (d != NULL)
+	{
+		if ((*d).name == NULL)
+			(*d).name = "(nil)";
+		if ((*d).owner == NULL)
+			(*d).owner = "(nil)";
+	}
 	printf("Name: %s\nAge: %lf\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
