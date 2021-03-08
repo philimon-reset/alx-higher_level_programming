92ffc0e2d09c4509f0c9b289e1ec64b796ed31da
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-08 18:34:15 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,13 @@
  * @owner: owner of dog
  *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }dog_t;
-typedef struct dog dog_t;
+
 dog_t *my_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /*HOLBERTON_H*/
