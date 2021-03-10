b92ba92f8ff4f0bac7e84ed0fd5fc323b335ace2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-10 16:21:01 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,17 +13,17 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 	if (!get_op_func(argv[2]))
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(99);
 	}
 	if (((argv[2][0] == '/') || (argv[2][0] == '%')) && (argv[3][0] == 0))
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 	p = get_op_func(argv[2]);
