bfb3076acf35ee4bbee8033c0d1ae8aebdb51bc5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-10 14:53:33 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,28 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter
+ * int_index - function that searches for an integer
  * @array: name
  * @size: size of array
- * @action: pointer to function
- * 
- * Return: void
+ * @cmp: pointer to function
+ *
+ * Return: return index of integer
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	size_t i;
+	int i;
 
-	if ((array != NULL) && (action != NULL))
+	if (size <= 0)
+		return (-1);
+	if ((array != NULL) && (cmp != NULL))
 	{
 		for (i = 0; i < size; i++)
 		{
-			action(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				return (i);
+			}
 		}
 	}
 }
