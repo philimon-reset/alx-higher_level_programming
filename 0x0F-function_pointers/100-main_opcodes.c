c3f80545baf63293be66af7aa9246d8bb7dacbcd
philimon-reset <philimonderib2019@gmail.com>
2021-03-10 19:40:34 +0000

    add

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument
+ * Return: return opertation
+ */
+
+int main(int argc, char **argv)
+{
+	int i = argc;
+
+	i = argv[i];
+	return (i);
+}
