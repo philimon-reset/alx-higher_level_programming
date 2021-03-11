3a84f48e16c1a9d37a0394b2ab3cbea332f49d8c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-11 21:42:33 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,6 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /*VARIADIC_FUNCTIONS_H*/
