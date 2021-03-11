5bb59b8e74fbe9ac979e2b46430a89a10ae65cf0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-11 22:22:29 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,27 +19,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			str = va_arg(args, char *);
-			if (str == NULL)
+			if (str != NULL)
 			{
-				printf("(nil)%s", separator);
-			}
-			else
-			{
-				if (i != (n - 1))
+				if (i != n - 1)
 				{
 					if (separator != NULL)
-					{
 						printf("%s%s", va_arg(args, char *), separator);
-					}
 					else
-					{
 						printf("%s", va_arg(args, char *));
-					}
 				}
 				else
 				{
 					printf("%s", va_arg(args, char *));
 				}
+			}
+			else
+			{
+				printf("(nil)%s", separator);
 			}
 		}
 	}
