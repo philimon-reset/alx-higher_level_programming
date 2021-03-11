b9ee0c6db2cf394360048bbe5338d4bb2ba3f639
philimon-reset <philimonderib2019@gmail.com>
2021-03-11 19:44:12 +0000

    add

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,16 +1,52 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - print anything
- * @format: format
+ * print_strings - print strings with given separator
+ * @separator: delimiter
+ * @n: number of arguments passed
  *
  * Return: void function
  */
-void print_all(const char * const format, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	int i;
-	char *str;
+	unsigned int i;
 	va_list args;
 
-	va_start
+	va_start(args, n);
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i != n - 1)
+			{
+				if (separator != NULL)
+				{
+					printf("%s%s", nil_check(va_arg(args, char *)), separator);
+				}
+				else
+				{
+					printf("%s", nil_check(va_arg(args, char *)));
+				}
+			}
+			else
+			{
+				printf("%s", nil_check(va_arg(args, char *)));
+			}
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
+
+/**
+ * nil_check - print strings with given separator
+ * @s: check if its NULL
+ *
+ * Return: (nil) if null
+ */
+char *nil_check(char *s)
+{
+	if (s == NULL)
+		s = "(nil)";
+	return (s);
 }
