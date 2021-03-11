c8d1920c771d079d9a8673b2e22d7def9d5bce2e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-11 17:35:05 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,11 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-int checker(char *c);
+int sum_them_all(const unsigned int n, ...);
 
-#endif /*FUNCTION_POINTERS_H*/
+#endif /*VARIADIC_FUNCTIONS_H*/
