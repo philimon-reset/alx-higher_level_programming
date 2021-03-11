eaf4e40a2257ae8921e207fe98fda235ae6acb56
philimon-reset <philimonderib2019@gmail.com>
2021-03-11 15:06:13 +0000

    Add

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print integers with given separator
+ * @separator: delimiter
+ * @n: number of arguments passed
+ *
+ * Return: void function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = va_arg(args, int);
+		if (i == (n - 1))
+		{
+			printf("%d\n", sum);
+		}
+		else
+		{
+			if (separator != NULL)
+			{
+				printf("%d%c ", sum, separator[0]);
+			}
+		}
+	}
+	va_end(args);
+}
