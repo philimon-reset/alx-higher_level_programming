512952f0fdbbd84e622400bf32c86b9e5da1d6c0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-11 22:10:36 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			if (i != (n - 1))
 			{
-				printf("%d%c ", sum, separator[0]);
+				printf("%d%s ", sum, separator);
 			}
 			else
 			{
