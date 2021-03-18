f95309d5e18e60230ddc73021127d45b6b67a24d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-18 23:46:55 +0300

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,11 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	if (head == NULL)
-		free(head);
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
+		free(temp->str);
 		free(temp);
 	}
 }
