45b72d901b8b7d3c0890bd652ebfa143534b3da0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-18 21:59:10 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 	{
 		while (temp != NULL)
 		{
-			printf("[%d] %s\n", h.len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 			temp = temp->next;
 			node++;
 		}
