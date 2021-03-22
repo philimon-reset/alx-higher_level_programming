13562a5ff589d652fc9e10ea12897c870599612f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-22 11:52:14 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_list - a function that prints all the elements of a list_t list.
+ * print_listint - a function that prints all the elements of a list_t list.
  * @h: value of list
  *
  * Return: any data type list
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
 	const list_t *temp = h;
 	int node = 0;
