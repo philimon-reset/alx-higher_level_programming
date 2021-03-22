961625d6fd1643fff259a5fd8e169f7bd8108897
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-22 12:04:11 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * add_node_end - a function that adds node to end of file.
+ * add_nodeint_end - a function that adds node to end of file.
  * @head: pointer to a struct
  * @str: name of new node
  *
  * Return: struct
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n);
 {
 	listint_t *temp = malloc(sizeof(list_t));
 	listint_t *last = *head;
