91e03c32035b0f40f7d118b045113b1a1e166c54
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-22 16:17:26 +0300

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,9 +7,9 @@
  *
  * Return: struct
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp = malloc(sizeof(list_t));
+	listint_t *temp = malloc(sizeof(listint_t));
 
 	if (temp == NULL)
 		return (NULL);
