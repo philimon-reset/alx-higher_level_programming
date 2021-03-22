b52cc8503255050e164b2e8f24cbba870e31cf91
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-22 11:54:07 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,28 +7,26 @@
 
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct list_s
+typedef struct listint_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+istint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 int _strlen(const char *s);
-void free_list(list_t *head);
-void start_up_func(void)__attribute__((constructor));
+void free_listint(listint_t *head);
+
 
 #endif /*LISTS_H*/
