3f496365d04b295be2b8616254c445363ae4ce25
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-22 13:40:17 +0300

    Update 4-free_list.c

--- 0x13-more_singly_linked_lists/4-free_list.c
+++ 0x13-more_singly_linked_lists/4-free_list.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * free_list - a function that frees linked lists.
+ * free_listint - a function that frees linked lists.
  * @head: pointer to a struct
  *
  * Return: void function
  */
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *temp;
+	listint_t *temp;
 
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->str);
+		free(temp->n);
 		free(temp);
 	}
 }
