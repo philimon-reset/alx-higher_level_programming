5b5b14bac3d47eb269a05878932674db2acb44d5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 16:23:11 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -61,7 +61,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 size_t list_len(const listint_t *h)
 {
 	const listint_t *temp = h;
-	unsigned int node = 0;
+	size_t node = 0;
 
 	while (temp != NULL)
 	{
