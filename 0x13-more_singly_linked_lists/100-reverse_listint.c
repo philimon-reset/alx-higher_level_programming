9de2e7a4431ff4610796427d0a2cbc6292352ef6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 17:20:32 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *next = NULL;
 	listint_t *prev = NULL;
 
-	if (head != NULL)
+	if (head == NULL)
 		return (NULL);
 	while (*head != NULL)
 	{
