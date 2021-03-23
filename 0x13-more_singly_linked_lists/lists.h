573c093c02d1526547e0d7e759953cb3777fdb65
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 17:06:23 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -33,5 +33,6 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 size_t list_len(const listint_t *h);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /*LISTS_H*/
