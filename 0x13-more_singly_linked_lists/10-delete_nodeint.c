33a5565c3fe98cc96948f48d8e196424478c1f2b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 17:04:30 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev = NULL;
 	unsigned int i = 0;
 
-	if (index > list_len(*head))
+	if (index > list_len(*head) || list_len(*head) == 0)
 	{
 		return (-1);
 	}
