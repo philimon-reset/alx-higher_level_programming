c5add04272d0ed475da7089640acc08077553ef8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 16:40:55 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if ((i + 1) == idx)
 			prev = (*head);
-		head = &(*head)->next);
+		head = &(*head)->next;
 		i++;
 	}
 	return (NULL);
