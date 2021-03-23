93242bd44dd61eb73988722ec0fea6c69d48d660
philimon-reset <philimonderib2019@gmail.com>
2021-03-23 12:25:53 +0000

    added

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to a struct
+ * @idx: index given
+ * @n: data
+ *
+ * Return: struct needed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *c;
+	listint_t *prev;
+	unsigned int i = 0;
+
+	if (temp == NULL)
+		return (NULL);
+	prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	if (head != NULL)
+	{
+		if (idx > list_len(*head))
+		{
+			free(temp);
+			return (NULL);
+		}
+		for (c = (*head); (c != NULL); c = c->next)
+		{
+			if ((i + 1) == idx)
+			{
+				prev = c;
+			}
+			if ((i) == idx)
+			{
+				temp->next = c;
+				if (prev != NULL)
+				{
+					prev->next = temp;
+				}
+				else
+				{
+					(*head) = temp;
+				}
+				return (temp);
+			}
+			i++;
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * list_len - a function that prints number of nodes.
+ * @h: value of list
+ *
+ * Return: any data type list
+ */
+size_t list_len(const listint_t *h)
+{
+	const listint_t *temp = h;
+	unsigned int node = 0;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		node++;
+	}
+	return (node);
+}
