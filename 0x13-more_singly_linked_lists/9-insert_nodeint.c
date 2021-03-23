7703b8388ee58ed3e5fbeb9670a33c824ad23d97
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 14:23:54 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,12 +10,54 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
+	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *current;
+	listint_t *prev;
 	int count = 0;
 
-	while (idx--)
+	prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	if ((idx < 0) || (idx > listint_len(*head)))
 	{
-		if (idx == 0)
+	    return (NULL);
+	}
+	if (idx == 0)
+	{
+		temp->next = (*head);
+		(*head) = temp;
+	}
+	for (current = (*head); current != NULL; current = current->next, count++)
+	{
+		if ((count + 1) == idx)
 		{
-			temp == (*head);
+			prev = current;
+		}
+		if ((count) >= idx)
+		{
+			break;
+		}
+	}
+	temp->next = current;
+	prev->next = temp;
+	return (temp);
+}
+
+/**
+ * listint_len - a function that prints number of nodes.
+ * @h: value of list
+ *
+ * Return: any data type list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp = h;
+	int node = 0;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		node++;
+	}
+	return (node);
 }
