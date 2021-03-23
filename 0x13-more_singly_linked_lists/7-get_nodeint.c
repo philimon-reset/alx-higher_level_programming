630e0dd2055ede6a2b7ae6303f1f3f1b87016ee8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 13:07:50 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,20 +9,20 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *temp = NULL;
+	listint_t *temp;
 	unsigned int count = 0;
 
 	while (head != NULL)
 	{
 		temp = head;
-		if (count == index)
+		if (count <= index)
 		{
-			return (temp);
+			if (count == index)
+				return (temp);
 		}
 		else
 			return (NULL);
 		count++;
 		head = head->next;
 	}
-	return (temp);
 }
