fcf019a96cb9f99bcff69e1a54bfff5cc047ac09
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-23 12:38:35 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void free_listint2(listint_t **head);
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (temp != NULL)
 	{
-		temp = *head;
-		*head = head->next;
-		free(temp);
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = head->next;
+			free(temp);
+		}
+		(*head) = NULL;
 	}
-	(*head) = NULL;
 }
