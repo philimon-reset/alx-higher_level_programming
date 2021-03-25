c1198aefdaa86e50a51d1d9395edfc3a4c094eec
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:09:48 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 
 /**
- * get_bit - returns the value of a bit at a given index.
+ * set_bit - sets the value of a bit at a given index to 1
  * @n: character
  * @index: index needed
  *
  * Return: number at index
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8 - 1)
+	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	return ((n >> index) & 1);
+	if ((*n >> index) & 1)) != 1)
+		n[index] = 1;
+	return (1);	
 }
