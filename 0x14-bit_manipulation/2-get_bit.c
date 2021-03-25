6ee5798c4a4153b21dc8c3b0309b1b80361d7f17
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:00:43 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,6 @@
 #include "holberton.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: character
- *
- * Return: void function
- */
-
-void print_binary(unsigned long int n)
-{
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
-}
-
-/**
  * get_bit - returns the value of a bit at a given index.
  * @n: character
  * @index: index needed
@@ -23,12 +9,7 @@ void print_binary(unsigned long int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	char *a = print_binary(n);
-	int len = 0;
-
-	while (a)
-		len++;
-	if (index > len)
+	if (index > sizeof(unsigned long int))
 		return (-1);
 	return ((n >> index) & 1);
 	
