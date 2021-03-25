b59edd92643ba480e94f21a7a09ebb12e54ed161
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:42:57 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,6 +11,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	*n = (n) | (0 << index);
+	*n = (*n) | (0 << index);
 	return (1);
 }
