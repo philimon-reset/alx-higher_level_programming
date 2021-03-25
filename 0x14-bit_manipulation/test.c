910feff429eec91ded0a882efdac3197cbb0bcce
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:01:40 +0300

    Update test.c

--- 0x14-bit_manipulation/test.c
+++ 0x14-bit_manipulation/test.c
@@ -8,17 +8,13 @@
  */
 int main(void)
 {
-    unsigned int n;
+    int n;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
+    n = get_bit(1024, 10);
+    printf("%d\n", n);
+    n = get_bit(98, 1);
+    printf("%d\n", n);
+    n = get_bit(1024, 0);
+    printf("%d\n", n);
     return (0);
 }
