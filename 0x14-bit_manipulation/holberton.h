32a0498d26a787ff2d7fbfaf191b4ab23cee12e3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 20:17:58 +0300

    Update holberton.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEC(x) (((n & 1) == 0) ? ('0') : ('1'))
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int _strlen(const char *s);
