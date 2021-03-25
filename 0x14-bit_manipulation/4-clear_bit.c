6c9aeb3ce977bccd60ea9d27d1f15077095287bf
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:37:37 +0300

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * set_bit - sets the value of a bit at a given index to 1
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: character
  * @index: index needed
  *
  * Return: number at index
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
 		return (-1);
