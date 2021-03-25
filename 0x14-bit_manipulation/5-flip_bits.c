0f7edf43b767d7cef4dd81c8d77723f584e0d5e1
philimon-reset <philimonderib2019@gmail.com>
2021-03-25 18:35:57 +0000

    added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the value of a bit at a given index to 1
+ * @n: character
+ * @index: index needed
+ *
+ * Return: number at index
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	*n = (n) | (1 << index);
+	return (1);
+}
