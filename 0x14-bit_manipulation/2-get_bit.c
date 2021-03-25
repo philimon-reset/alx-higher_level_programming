da7fd95b5dbbab47718fcfc7e25e5d68c8eb2fd4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 20:59:41 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,3 +13,23 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: character
+ * @index: index needed
+ *
+ * Return: number at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	char *a = print_binary(n);
+	int len = 0;
+
+	while (a)
+		len++;
+	if (index > len)
+		return (-1);
+	return ((n >> index) & 1);
+	
+}
