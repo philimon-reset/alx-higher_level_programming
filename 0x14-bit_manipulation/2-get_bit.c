676c1db38062daa5652f592a53636a37267589ac
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:01:52 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
+	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
 	return ((n >> index) & 1);
 }
