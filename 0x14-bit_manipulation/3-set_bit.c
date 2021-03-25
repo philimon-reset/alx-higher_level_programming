948148ccb120c82e0d28713c604b395debde7535
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 21:14:00 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	if (((*n >> index) & 1) != 1)
+	if (((n >> index) & 1) != 1)
 		n[index] = '1';
 	return (1);	
 }
