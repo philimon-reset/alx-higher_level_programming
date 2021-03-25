8b26418668da9e1707e93abb28cce97e7619f6fa
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-25 22:13:31 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,14 +3,19 @@
 /**
  * flip_bits - number of bits needed to flip one number to another
  * @n: character
- * @m: index needed
+ * @m: second character
  *
  * Return: number at index
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	if (n == NULL || (index > sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	*n = (n) | (1 << index);
-	return (1);
+	int a = n ^ m;
+	int len;
+
+	while (a)
+	{
+		if ((a >> 1) == 1)
+			len++;
+		a >> 1;
+	}	
 }
