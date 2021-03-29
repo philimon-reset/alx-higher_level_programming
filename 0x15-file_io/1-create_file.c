406e080c25b8858b0d2c8914f7bd6bde08a9d8f4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 19:49:27 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,8 @@ int create_file(const char *filename, char *text_content)
 	int fd, sz, oz;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0600);
-	if ((filename == NULL) || (fd == -1) || (buf == NULL))
+	if ((filename == NULL) || (fd == -1))
 	{
-		free(buf);
 		return (-1);
 	}
 	if (text_content == NULL)
