6f31b58a9b3b045fe4d2b28e40f4ba6a2e1d19cd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 21:23:32 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,44 +10,28 @@
 
 int main(int argc, char **argv)
 {
-	int fd, oz;
+	int fd[2], r1, w1;
 	int f1 = argv[1];
 	int f2 = argv[2];
-	char buf[1024];
+	char *buf = malloc(sizeof(char) * 1024);
 
-	fd[0] = open(argv[1], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
-	fd[1] = open(argv[2], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	fd[0] = open(f1, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	fd[1] = open(f2, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", fd);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (fd[0] == -1)
 		return (-1);
-	if ((argv[2] == NULL) || (fd[1] == -1))
+	if ((f1 == NULL) || (fd[0] == -1))
 	{
 		close(fb[0]);
-		dprintf(STDERR_FILENO , "Error: Can't write to %s\n", argv[2], fd[1]);
+		dprintf(STDERR_FILENO , "Error: Can't read from file %s\n", f1);
 		exit(98);
 	}
-	
-}
-
-/**
- * _strlen - return the length of a a string
- * @s: string to be checked
- *
- * Return: lenght of string
- */
-
-int _strlen(char *s)
-{
-	int var;
-	int output = 0;
-
-	for (var = 0; s[var] != '\0'; var++)
+	while ((r1 = read(f[0], buf, 1024)) != 0)
 	{
-		output += 1;
+		w1 = write(f[1], buf, r1);
 	}
-	return (output);
 }
