31fc5682a04af2da82c6081d3fca1e7586b3e5f8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 20:05:41 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, oz;
 
-	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if ((filename == NULL) || (fd == -1))
 	{
 		return (-1);
