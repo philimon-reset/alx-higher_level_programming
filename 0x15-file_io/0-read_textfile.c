a0c93260a49d0d4e76b4d1b3c8ad98dc56db7bf9
philimon-reset <philimonderib2019@gmail.com>
2021-03-29 14:54:30 +0000

    added

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *(argv + 0));
+	return (0);
+}
