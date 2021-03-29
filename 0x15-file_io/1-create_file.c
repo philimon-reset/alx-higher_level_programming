1721b86ec2a7eca8b07a9e5aa17170d680603249
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 19:48:20 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, sz, oz;
-	char *buf = malloc(sizeof(char) * _strlen(text_content));
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0600);
 	if ((filename == NULL) || (fd == -1) || (buf == NULL))
@@ -24,10 +23,9 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	sz = read(fd, buf, _strlen(text_content));
 	if (sz == -1)
 		return (-1);
-	oz = write(fd, buf, _strlen(text_content));
+	oz = write(fd, *text_content, _strlen(text_content));
 	if (oz == -1)
 		return (-1);
 	close(fd);
