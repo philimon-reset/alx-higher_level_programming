9bbe76fb433d90bdc5084adf4135c40b03b0ed52
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 19:36:30 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		oz = write(fd, "", 0);
+		oz = write(fd, "", _strlen(text_content));
 		close(fd);
 		return (1);
 	}
