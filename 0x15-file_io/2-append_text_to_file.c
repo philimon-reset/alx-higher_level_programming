cef102b671e688f0bdb88f3af772947956875670
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 20:04:48 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
 
 /**
- * create_file - appends text at the end of a file.
+ * append_text_to_file - appends text at the end of a file.
  * @filename: file descripter
  * @text_content: count
  *
  * Return: int
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, oz;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if ((filename == NULL) || (fd == -1))
 	{
+		return (-1);
+	}
+	if ((text_content == NULL) && (filename == NULL))
+	{
+		close(fd);
 		return (-1);
 	}
 	if (text_content == NULL)
