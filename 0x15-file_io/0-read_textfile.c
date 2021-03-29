16f28d1d8604ac8b3610ba99776ec10d8ac9e6af
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 18:31:35 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,16 +13,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, sz;
 	char *buf = malloc(sizeof(char) * letters);
 
-	fd = open(*filename, O_RDONLY);
-	if ((*filename == NULL) || (fd == -1))
-		return (0);
-	sz = read(fd, buf, letters);
-	buf[sz] = '\0';
-	close(fd);
+	if (buf != NULL)
+	{
+		fd = open(*filename, O_RDONLY);
+		if ((*filename == NULL) || (fd == -1))
+			return (0);
+		sz = read(fd, buf, letters);
+		if (sz == -1)
+			return (0);
+		buf[sz] = '\0';
 
-	fd = open(*filename, O_CREAT | O_RDONLY, 00200);
-	if ((*filename == NULL) || (fd == -1))
-		return (0);
-	sz = write(STDOUT_FILENO, buf, letters);
-	close(fd);
+		if ((*filename == NULL) || (fd == -1))
+			return (0);
+		sz = write(STDOUT_FILENO, buf, letters);
+		if (sz == -1)
+			return (0);
+		close(fd);
+	}
 }
