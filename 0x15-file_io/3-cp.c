62ff6981e96300df92415494abe3c7e85317abdd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-03-29 21:49:22 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,23 +15,42 @@ int main(int argc, char **argv)
 	int f2 = argv[2];
 	char *buf = malloc(sizeof(char) * 1024);
 
-	fd[0] = open(f1, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
-	fd[1] = open(f2, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (fd[0] == -1)
-		return (-1);
+	fd[0] = open(f1, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if ((f1 == NULL) || (fd[0] == -1))
 	{
 		close(fb[0]);
 		dprintf(STDERR_FILENO , "Error: Can't read from file %s\n", f1);
 		exit(98);
 	}
+	fd[1] = open(f2, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	if ((f2 == NULL) || (fd[1] == -1))
+	{
+		close(fb[1]);
+		dprintf(STDERR_FILENO , "Error: Can't write to ", f2);
+		exit(99);
+	}
 	while ((r1 = read(f[0], buf, 1024)) != 0)
 	{
 		w1 = write(f[1], buf, r1);
+		if (w1 != r1)
+		{
+			close(fb[1]);
+			dprintf(STDERR_FILENO , "Error: Can't write to ", f2);
+			exit(99);
+		}
+	}
+	if (close(f1) == -1)
+	{
+		dprintf(STDERR_FILENO , "Error: Can't close fd %d", f1);
+	}
+	
+	if (close(f2) == -1)
+	{
+		dprintf(STDERR_FILENO , "Error: Can't close fd %d", f2);
 	}
 }
