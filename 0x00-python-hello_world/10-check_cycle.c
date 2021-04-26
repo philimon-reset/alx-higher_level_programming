31e241a86c5d78d25c898fece565c76898aa0b00
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-26 22:33:37 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,28 @@
 #include "lists.h"
+/**
+ * check_cycle - check if loop exists in cycle
+ * @list: struct to be checked
+ *
+ * Return: 1 if it is, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle = malloc(sizeof(listint_t));
+	listint_t *hare = malloc(sizeof(listint_t));
+	listint_t *temp = list;
+
+	if (turtle == NULL || hare == NULL)
+		return (0);
+	turtle->n = list->n;
+	hare->n = list->n;
+	while (temp != NULL)
+	{
+		turtle->next = temp->next;
+		hare->next = (temp->next)->next;
+		if (turtle == hare)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);	
+}
