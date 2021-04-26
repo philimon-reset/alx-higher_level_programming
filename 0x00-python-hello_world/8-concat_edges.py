6221a060a106ff113776c080e6803e506e00af94
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-26 19:57:45 +0300

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word = "Holberton"
-
-print("Welcome to {}!".format(str1))
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66]
+print(str)
