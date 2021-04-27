bed7a5e0464b56771efdf8ae285830c245dd8197
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 14:01:20 +0300

    Update 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if (number > 0):
-    print("is positive")
+    print("{} is positive".format(number))
 elif (number < 0):
-    print("is negative")
+    print("{} is negative"format(number))
 elif (number == 0):
-    print("is zero")
+    print("0 is zero")
