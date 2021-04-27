6fb01e08460592e9fd3de6d098b8e519b878afd4
philimon-reset <philimonderib2019@gmail.com>
2021-04-27 12:01:46 +0000

    5th done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@ for i in range(0, 100):
     if (len(str(i)) < 2):
         print("0{}".format(i), end=", ")
     elif (len(str(i)) == 2 and i != 99):
-        print("{}, ".format(i), end=", ")
+        print("{}".format(i), end=", ")
     else:
         print("{}".format(i))
