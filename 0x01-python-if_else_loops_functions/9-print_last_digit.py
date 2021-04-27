eea7932dc2a77cc39b609562b7d2b03cd32c87a4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 23:45:36 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,7 @@
 
 def print_last_digit(number):
         new = (str(number))[-1]
+        if (number < 0):
+                new = int(new)
         print("{}".format(new), end="")
         return new
