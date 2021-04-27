08ad2aaf6ec4c9e173cba6ba014befd24a78126b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 23:49:41 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,12 @@
-#
+#/usr/bin/python3
+
+
+for num in range(1,100):
+    str_ = ""
+    if num % 3 == 0:
+        str_ = str_ + "Fizz"
+    if num % 5 == 0:
+        str_ = str_ + "Buzz"
+    if num % 5 != 0 and num % 3 != 0:
+        str_ = str_ + str(num)
+    print(str_)
