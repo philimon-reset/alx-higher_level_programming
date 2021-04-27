874b59e5d772d9b99947d3b850720b173c574ca0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 15:05:13 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
+for i in range(100):
     if (len(str(i)) < 2):
         print("0{}".format(i), end=", ")
     elif (len(str(i)) == 2 and i != 99):
         print("{}".format(i), end=", ")
-    else:
-        print("{}".format(i))
