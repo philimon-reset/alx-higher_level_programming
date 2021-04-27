0907dc6027b567245c18b31188d5afe27ddc2078
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 22:42:42 +0300

    Update main.py

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-islower = __import__('7-islower').islower
+uppercase = __import__('8-uppercase').uppercase
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+uppercase("holberton")
+uppercase("Holberton School 98 Battery street"
