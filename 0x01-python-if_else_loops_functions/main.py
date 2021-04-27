1ee9aa1a1ef61472d3b1db75b330c93b2b622ad2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 23:55:44 +0300

    Update main.py

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+fizzbuzz()
+print("")
