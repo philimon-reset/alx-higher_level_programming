a0a3e6b6b44d29a12d0b7eb9d91a03ad6d482567
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 15:15:15 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for i in range(10):
         if (i != 9 or a != 9):
             print("{}{}".format(i, a), end=", ")
         elif (i == 9 and a == 9):
-            print("{}{}".format(i, a), end="")
+            print("{}{}".format(i, a))
