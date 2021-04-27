736a0f7195c843d3cc264c5f4f68526f31d8c28c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 15:18:27 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for a in range(10):
-        if (i != 9 or a != 9):
-            print("{}{}".format(i, a), end=", ")
-        elif (i == 9 and a == 9):
-            print("{}{}".format(i, a))
+for i in range(100):
+    if (i != 99):
+        print("{02n}".format(i), end=", ")
+    elif (i == 99):
+        print("{:02n}".format(i))
