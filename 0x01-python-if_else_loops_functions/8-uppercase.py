5ba82d510c46f70af9de0de3b303bbf6ed3c4d68
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-27 22:47:41 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(str[i]) > 96 and ord(str[i]) < 123:
-            i = ord(str[i]) - 32
+        if ord(i) > 96 and ord(i) < 123:
+            i = ord(i) - 32
     print("{}".format(i), end="")
