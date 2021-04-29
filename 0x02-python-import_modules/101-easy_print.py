ddabf9677e910d06bc8fb236e0193e82ce8fed87
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 23:40:09 +0300

    Update 101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-        import os
-        os.sys.stdout.write("#pythoniscool\n")
-        
+        import os; os.sys.stdout.write("#pythoniscool\n")
