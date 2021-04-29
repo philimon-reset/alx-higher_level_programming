b5680c11ed16c6a28253fa0d6da177e8241c891f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 14:28:07 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
+
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{:i} + {:i} = {:n}".format(a, b, add(a, b)))
+        a = 1
+        b = 2
+        print("{:i} + {:i} = {:n}".format(a, b, add(a, b)))
