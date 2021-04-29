599aaabeaf25419ed3bc2319a27612478aa5a477
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 22:50:58 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,14 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     ls = ['+', '-', '*', '/']
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     elif sys.argv[2] not in ls:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == ls[0]:
         print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
         sys.exit(0)
