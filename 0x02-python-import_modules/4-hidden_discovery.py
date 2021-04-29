0544c36360caf515447b03f45e6fe05682643828
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 21:57:02 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for i in 
+    name = dir(hidden_4)
+    for i in name:
+        if name[i][0] != "__":
+            print(name[i])
