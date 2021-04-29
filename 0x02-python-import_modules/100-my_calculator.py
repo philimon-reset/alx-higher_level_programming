eadde5e02b76170831ff07d2a8241182ceed06bd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 22:23:47 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
-    print("{:n} - {:n} = {:n}".format(a, b, sub(a, b)))
-    print("{:n} * {:n} = {:n}".format(a, b, mul(a, b)))
-    print("{:n} / {:n} = {:n}".format(a, b, div(a, b)))
+    ls = ["+", "-", "*", "/"]
+    if len(sys.argv) == 1:
+        print("Usage: {:str} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+    elif sys.argv[2] not in ls:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if sys.argv[2] == ls[0]:
+        print("{:n} + {:n} = {:n}".format(int(sys.argv[1]), int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == ls[1]:
+        print("{:n} - {:n} = {:n}".format(int(sys.argv[1]), int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == ls[2]:
+        print("{:n} * {:n} = {:n}".format(int(sys.argv[1]), int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+    elif sys.argv[2] == ls[3]:
+        print("{:n} / {:n} = {:n}".format(int(sys.argv[1]), int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
