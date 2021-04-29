88417c56c73f6b4e400051ec5d5110a41f61f149
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 14:36:55 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments.".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+        if (len(sys.argv) == 2)
+            print("{} argument:".format(len(sys.argv) - 1))
+            for i in range(1, len(sys.argv)):
+                print("{}: {}".format(i, sys.argv[i]))
+        if (len(sys.argv) == 1) 
+            print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
