104f6a8363b8a4d4f0e0e606301e20303402120d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 15:11:12 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
         import sys
         total = 0
         for i in range(1, len(sys.argv)):
-          total += int(sys.argv[i])
+                total += int(sys.argv[i])
         print("{:n}".format(total))
