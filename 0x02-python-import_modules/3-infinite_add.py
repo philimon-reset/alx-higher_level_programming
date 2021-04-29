ff8f37faf0f5815246df6c091d96e61585cc69ce
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 14:45:43 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 total = 0
-for i in range(1, len(sys.argv)):
-    total += int(sys.argv[i])
-print("{}".format(total))
+total = sum(int(sys.argv))
+print("{:n}".format(total))
