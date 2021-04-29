6ce321a2a3382c479365ec6ddf192bb38d1c48ee
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 23:54:58 +0300

    Update 103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-        from add_0 import add
-        a = 1
-        b = 2
-        print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+import string
+print(string.ascii_uppercase)
