28665c32e7714ce2c26d11829c6924d53fac6a89
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-04-29 22:45:47 +0300

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    ls = ["+", "-", "*", "/"]
+    ls = ['+', '-', '*', '/']
     if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
