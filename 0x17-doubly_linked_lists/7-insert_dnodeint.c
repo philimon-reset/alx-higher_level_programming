3d2ffa0dac7676b587bf59c5ae89f30c42e74838
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 02:51:00 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_mode = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = get_dnodeint_at_index(*h, idx);
+	dlistint_t *temp = get_dnodeint_at_index2(*h, idx);
 
 	if (new_mode == NULL || temp == NULL)
 		return (NULL);
@@ -32,14 +32,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 }
 
 /**
- * get_dnodeint_at_index - get the addressof node at given index.
+ * get_dnodeint_at_index2 - get the addressof node at given index.
  * @head: head of list.
  * @index: index of needed node.
  *
  * Return: address of indexed node.
  */
 
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index2(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *temp = head;
 	unsigned int size = 0, node = 0;
