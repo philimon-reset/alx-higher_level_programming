ac8bc97fa5e7f492ea162fbc90540e0166afc5a1
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 00:00:40 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  * Return: address of new node.
  */
 
-dlistint_t *add_dnodeint_end (dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *temp = *head;
