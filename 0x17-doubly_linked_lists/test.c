3ad15254b89826f3b378df3e3e25421076543702
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 02:48:43 +0300

    Update test.c

--- 0x17-doubly_linked_lists/test.c
+++ 0x17-doubly_linked_lists/test.c
@@ -11,7 +11,7 @@
 int main(void)
 {
     dlistint_t *head;
-    int sum;
+    dlistint_t *node;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -22,8 +22,9 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    sum = sum_dlistint(head);
-    printf("sum = %d\n", sum);
+    print_dlistint(head);
+    node = get_dnodeint_at_index(head, 5);
+    printf("%d\n", node->n);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
