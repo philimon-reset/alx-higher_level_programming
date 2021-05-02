5eff80d32be2f4438ada9b9c6a1a37aeb86f6594
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 22:56:05 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		size += 1;
 		op = op->next;
 	}
-	if (new_mode == NULL || h == NULL)
+	if (h == NULL)
 		return (NULL);
 	new_mode->n = n;
 	new_mode->next = NULL;
