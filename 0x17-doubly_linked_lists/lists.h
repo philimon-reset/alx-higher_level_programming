318c82015b2c8f453825b6d5ca179218d3891171
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 21:18:56 +0300

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -31,7 +31,9 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
-dlistint_t *get_dnodeint_at_index2(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index2(dlistint_t *head, unsigned int index, unsigned int size);
+dlistint_t *add(dlistint_t **head, const int n);
+dlistint_t *end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /*LISTS_H*/
