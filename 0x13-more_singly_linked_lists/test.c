4849bd01e5649e4e903cc480d2c259b18400a8db
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 10:32:19 +0300

    Update test.c

--- 0x13-more_singly_linked_lists/test.c
+++ 0x13-more_singly_linked_lists/test.c
@@ -6,24 +6,26 @@
 /**
  * main - check the code for Holberton School students.
  *
- * Return: Always 0.
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    listint_t *head;
+    dlistint_t *head;
 
     head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    reverse_listint(&head);
-    print_listint(head);    
-    free_listint2(&head);
-    return (0);
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
 }
