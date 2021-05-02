60940f41911cb86a158a246f3f37fb0288ac4862
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 22:47:14 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,9 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		if (*head != NULL)
-		{
 			(*head)->prev = NULL;
-		}
 		free(temp);
 		return (1);
 	}
