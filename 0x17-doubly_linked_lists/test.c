8b25b2ccad557b5eb809f43000c0c0fc9fd3e642
philimon-reset <philimonderib2019@gmail.com>
2021-05-02 20:26:51 +0000

    added

--- 0x17-doubly_linked_lists/test.c
+++ 0x17-doubly_linked_lists/test.c
@@ -13,17 +13,9 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
-    add_dnodeint_end(&head, 0);
-    add_dnodeint_end(&head, 1);
-    add_dnodeint_end(&head, 2);
-    add_dnodeint_end(&head, 3);
-    add_dnodeint_end(&head, 4);
-    add_dnodeint_end(&head, 98);
-    add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 8, 4096);
+    insert_dnodeint_at_index(NULL, 0, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
