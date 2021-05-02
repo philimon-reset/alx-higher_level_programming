834091d5ed99d0b5d156fb582b255a107611f8db
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-02 23:27:27 +0300

    Update test.c

--- 0x17-doubly_linked_lists/test.c
+++ 0x17-doubly_linked_lists/test.c
@@ -15,7 +15,7 @@ int main(void)
     head = NULL;
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(NULL, 0, 4096);
+    insert_dnodeint_at_index(&head, 0, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
