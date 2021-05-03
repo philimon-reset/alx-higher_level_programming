fd6630f0e942b9626507e54cbc7cf53e99c8e740
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-03 13:56:28 +0300

    Update mul.c

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - function that returns a * b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
