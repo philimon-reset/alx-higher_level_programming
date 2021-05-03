3fddd6c2d87b59fc64a546665ad64eb35251e39d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-03 13:52:27 +0300

    Update div.c

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * op_div - function that divides a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
