3cd4f4b5364e3bdebbb28d2dbc4f45652d06f32f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-03 13:53:11 +0300

    Update mod.c

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mod - function that mods a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
