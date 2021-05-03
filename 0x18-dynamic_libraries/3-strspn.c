a4fa068a356bc24e8477ac7f1494fe15ca3e0897
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-03 12:32:41 +0300

    Update 3-strspn.c

--- 0x18-dynamic_libraries/3-strspn.c
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -22,25 +22,3 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (output);
 }
-
-/**
- * _strchr - function that fills memory with a constant byte.
- * @s: destenation of string
- * @c: character to replace
- *
- * Return: a pointer to the resulting string
- */
-
-char *_strchr(char *s, char c)
-{
-	int j = 0;
-
-	for (; s[j] != '\0' && s[j] != c; j++)
-	{
-		continue;
-	}
-	if (s[j] == c)
-		return (s + j);
-	else
-		return (NULL);
-}
