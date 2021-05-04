9fecf81ac9ad1d17e0b7a244bac22edd717587dc
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-04 23:42:21 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = my_list[:]
+    new = my_list.copy()
     if idx < 0 or idx > len(my_list):
         return (new)
     new[idx] = element
