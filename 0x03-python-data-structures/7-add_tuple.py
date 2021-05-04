df3662befe21c7da5e55b56714000b2586e5f6c8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-04 23:14:50 +0300

    Update 7-add_tuple.py

--- 0x03-python-data-structures/7-add_tuple.py
+++ 0x03-python-data-structures/7-add_tuple.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=())
+def add_tuple(tuple_a=(), tuple_b=()):
         if len(tuple_a) < 2:
             if len(tuple_a) == 0:
-                new_tuple = tuple_b[], tuple_b[1]
+                new_tuple = tuple_b[0], tuple_b[1]
                 return (new_tuple)
             new_tuple = tuple_a[0] + tuple_b[0], tuple_b[1]
+            return (new_tuple)
         elif len(tuple_b) < 2:
-            new_tuple = tuple_a[a],
+            if len(tuple_b) == 0:
+                new_tuple = tuple_a[0], tuple_a[1]
+                return (new_tuple)
+            new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1]
             return (new_tuple)
         new_tuple = tuple_a[a] + tuple_b[a], tuple_a[a + 1] + tuple_b[a + 1]
     return (new_tuple)
