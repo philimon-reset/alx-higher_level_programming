8f312fcc1092d4c52fb5ec858c241cd8669a0b20
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-04 23:19:32 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data-structures/6-print_matrix_integer.py
+++ 0x03-python-data-structures/6-print_matrix_integer.py
@@ -4,4 +4,4 @@ def print_matrix_integer(matrix=[[]]):
         for a in range(len(i) - 1):
             if a < len(i) - 1:
                 print("{} ".format(i[a])
-            print("{}".format(i[a]), end="")
+            print("{}".format(i[a]), end='')
