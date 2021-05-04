019a61523dbcc17d5698ee1a467db8087ca89fe4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-04 23:45:25 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_ = ""
-    for i in range(len(my_string) - 1):
-        if my_string[i] not in "cC":
-            new_ += my_string[i]
+    for i in my_string:
+        if i not in "cC":
+            new_ += i
     return (new_)
