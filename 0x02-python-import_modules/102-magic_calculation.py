3ae92c768f202002a510de3a99908e1b136df53f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-05 23:05:20 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,4 +2,10 @@
 
 
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub:
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a,b)
