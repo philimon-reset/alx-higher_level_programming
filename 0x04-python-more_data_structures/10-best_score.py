bdf34e116dd22b7bacc0e3bcf29adb6cfcde6e4e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 07:03:12 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def best_score(a_dictionary): 
+    max = 0
+    for i in a_dictionary:
+        if a_dictonary[i] > max:
+            max = a_dictionary[i]
+    return max
