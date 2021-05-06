5e4d3ba2279164f5bc985768fec453a9d47be2ac
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:33:25 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def uniq_add(my_list=[]):
