5f53d7f17a9fa10396fe47a18bbcefd21fa84345
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 19:03:13 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def roman_to_int(roman_string):
+    if isinstance(roman_string) and not in None:
+        
+    return 0
