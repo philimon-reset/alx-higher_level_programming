d84db016a7f30db38ae83fa27b703a6a6c143604
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:45:58 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def multiply_by_2(a_dictionary):
+    new = a_dictionary[:]
+    for i in a_dictionary:
+        new[i] *= 2
+    return new
