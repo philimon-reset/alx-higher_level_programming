a7b270a48dd8795ccd786f8909574c86212b1eb6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 17:42:52 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-  total = 0
-  new = []
-  for i in range(len(my_list)):
-    if my_list[i] not in new:
-      total += my_list[i]
-      new.append(my_list[i])
-  return (total)
+        total = 0
+        new = []
+        for i in range(len(my_list)):
+          if my_list[i] not in new:
+            total += my_list[i]
+            new.append(my_list[i])
+        return (total)
