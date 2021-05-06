ec7f194abf2149ae3e722023093ab9591c138825
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:28:15 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,6 @@
 def simple_delete(a_dictionary, key=""):
     for i in a_dictionary:
         if i == key:
-            del a_dictionary[i]
+            break;
+    del a_dictionary[i]
     return a_dictionary
