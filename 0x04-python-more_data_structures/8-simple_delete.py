3410d8d6fc1f07af32516fc34cd30fa2ac93eb0d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:36:04 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            break;
-    if i == key:
-        del a_dictionary[i]
+    a_dictionary = map(lambda i: del a_dictionary[i] if i == key, a_dictionary)
     return a_dictionary
