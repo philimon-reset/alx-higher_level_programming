88be4b775ce5bac6892defabdc229a70f35adfaf
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:27:21 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def simple_delete(a_dictionary, key=""):
+    for i in a_dictionary:
+        if i == key:
+            del a_dictionary[i]
+    return a_dictionary
