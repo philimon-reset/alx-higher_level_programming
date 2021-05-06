5a78a15cd526ae5b471cf3e258e5bfdec8ec99a9
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 22:11:33 +0300

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda x: x ** 2, [x for x in matrix]))
+def complex_delete(a_dictionary, value):
+    if value not in a_dictionary.values():
+        return a_dictionary
+    keys = []
+    vals = []
+    for key in a_dictionary.keys():
+        if a_dictionary[key] != value:
+            keys.append(key)
+            vals.append(a_dictionary[key])
+    a_dictionary.clear()
+    a_dictionary.update(dict([(k, v) for k, v in zip(keys, vals)]))
+    return a_dictionary
