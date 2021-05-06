3d0bef50818db308245bb8ec0d8efeef1482edcb
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:05:37 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def search_replace(my_list, search, replace):
