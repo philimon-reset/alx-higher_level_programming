ec28cea62a86e8b3334bd323988156b6ebdfea16
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 18:23:18 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,10 @@ def best_score(a_dictionary):
     total = 0
     if a_dictionary == None:
         return None
-    max = list(a_dictionary.values())[0]
+    max_ = list(a_dictionary.values())[0]
     for i in a_dictionary:
-        if a_dictionary > max:
-            max = a_dictionary[i]
+        if a_dictionary[i] > max_:
+            max_ = a_dictionary[i]
             total += a_dictionary[i]
     if total == 0:
         return None
