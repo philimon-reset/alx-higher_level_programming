a03af8542d74414fecbe5bf521c5c7c21eff409f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:23:07 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,5 @@ def update_dictionary(a_dictionary, key, value):
     for i in a_dictionary:
         if i == key:
             a_dictionary[i] = value
-        else:
-            a_dictionary.update({key:value})
+    a_dictionary.update({key:value})
     return a_dictionary
