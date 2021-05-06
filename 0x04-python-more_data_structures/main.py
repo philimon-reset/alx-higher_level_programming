b6058dbf1c366c52e4490175b93e487bf848e39a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 18:47:06 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-best_score = __import__('10-best_score').best_score
+multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+my_list = [1, 2, 3, 4, 6]
+new_list = multiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)
