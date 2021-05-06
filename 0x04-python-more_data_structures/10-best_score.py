ef466437d8f2b9bb86d07e97cdccd085dced3707
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 07:04:41 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary): 
     max = 0
     for i in a_dictionary:
-        if a_dictonary[i] > max:
+        if a_dictionary[i] > max:
             max = a_dictionary[i]
     if max != 0:
         return i
