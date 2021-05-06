e3844eb84b8e6c6e775d61b0668c417926fded8e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:24:25 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+simple_delete = __import__('8-simple_delete').simple_delete
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+new_dict = simple_delete(a_dictionary, 'track')
 print_sorted_dictionary(a_dictionary)
-
 print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
 print_sorted_dictionary(new_dict)
+
 print("--")
+print("--")
+new_dict = simple_delete(a_dictionary, 'c_is_fun')
 print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict)
