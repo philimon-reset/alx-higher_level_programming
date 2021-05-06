fdc560e8baccf373f535005fcb27a778387685c2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 22:09:02 +0300

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda a: list(map(lambda x: x * x), a), matrix))
+    return list(map(lambda a: list(map(lambda x: x*x), a), matrix))
