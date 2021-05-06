777f883eedafaf15b9298ecad0de1248bdd9b706
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:42:14 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+uniq_add = __import__('2-uniq_add').uniq_add
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
