01fa755b7219258cf6ac57122bfe5bf966ec525b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 22:04:17 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list == 0):
+    if len(my_list) == 0:
         return 0
-    mul = 0
     total = 0
     div = 0
     for i in my_list:
-        mul = i[0] * i[1]
         div += i[1]
-        total += mul
+        total += i[0] * i[1]
     total /= div
     return total
