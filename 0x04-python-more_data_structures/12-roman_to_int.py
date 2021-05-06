b6838325dc006033604abef1485e3667d998eecf
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 19:46:45 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,7 @@ def roman_to_int(roman_string):
     dic_t = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100,
              'D' : 500, 'M' : 1000}
     for i in roman_string:
-        if i in dic_t:
-            total += dic_t[i]
+        if i == dic_t['I']:
+            total -= dic_t[i]
+        total += dic_t[i]
     return total
