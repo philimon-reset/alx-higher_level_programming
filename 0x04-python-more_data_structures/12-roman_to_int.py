d1a04b64361fdfb8e2d316df7415d7ed3f5781fd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 19:56:21 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str)) and (not None):
         return 0
     total = 0
-    dic_t = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100,
-             'D' : 500, 'M' : 1000}
+    dic_t = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+             'D': 500, 'M': 1000}
     for i in roman_string:
             total += dic_t[i]
     if len(roman_string) == 2 and (total == 6 or total == 11):
