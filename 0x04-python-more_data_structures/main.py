5654c1c78ed225ccb1c50c6635a90ee39955bf36
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 06:46:24 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-simple_delete = __import__('8-simple_delete').simple_delete
+multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-new_dict = simple_delete(a_dictionary, 'track')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
-
-print("--")
-print("--")
-new_dict = simple_delete(a_dictionary, 'c_is_fun')
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
