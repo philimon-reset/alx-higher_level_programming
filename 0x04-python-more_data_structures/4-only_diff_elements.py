2965798f98f92e906a22484a6d18e3c613dee070
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:56:04 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    x = {}
-    x = {x for x in set_1 if x not in set_2}
-    return (x)
+    return {x for x in set_1 if x not in set_2} + {x for x in set_2 if x not in set_1}
