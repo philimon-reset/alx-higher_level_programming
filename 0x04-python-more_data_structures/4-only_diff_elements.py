c7f9586039a59c672ad9cfa16fb0fea94e2b45b4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:55:12 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    square = lambda x: x ** 2
-    new = matrix[:]
-    for i in new:
-        i = list(map(square, i))
-    print(new)
+def only_diff_elements(set_1, set_2):
+    x = {}
+    x = {x for x in set_1 if x not in set_2}
+    return (x)
