90d7ef78345e0e54a34240a4acec5511ed87f7b7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 17:44:39 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
         new = my_list[:]
         for i in range(len(my_list)):
-          if my_list[i] == search:
-            new[i] = replace
+                if my_list[i] == search:
+                        new[i] = replace
         return new
