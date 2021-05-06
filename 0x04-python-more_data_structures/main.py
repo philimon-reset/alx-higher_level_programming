635b30e2c4fa9ab1c844015d60a8b6af4b0a13b5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-06 05:53:40 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-common_elements = __import__('3-common_elements').common_elements
+only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
