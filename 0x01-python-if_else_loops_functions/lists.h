9a25c0b9f1aa70851750188785e4512a0ea6f9f3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-08 15:00:35 +0300

    Update lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -21,6 +21,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
