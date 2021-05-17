472f8032a1c760ccc10ea10f1584e9785fdf5ae0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-17 15:45:58 +0300

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    for i in range(list_length):
+        new.append(0)
+    len1 = len(my_list_1)
+    len2 = len(my_list_2)
     try:
-        a = 0
-        for i in range(x):
-            print(my_list[i], end="")
-            a += 1
-        print("")
-        return x
-    except IndexError:
-        print("")
-        return a
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                new[i] = result
+            except ZeroDivisionError:
+                result = 0
+                print("division by 0")
+            except IndexError:
+                print("out of range")
+            except TypeError:
+                print("wrong type")
+    finally:
+        return new
