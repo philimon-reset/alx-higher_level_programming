a9e8d3d56f896d6d93338e7cc9fa35bf510ca76a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 12:27:05 +0300

    Update 4-square.py

--- 0x06-python_classes/4-square.py
+++ 0x06-python_classes/4-square.py
@@ -2,14 +2,23 @@
 class Square():
     def __init__(self, size=0):
         self.__size = size
-        self.size = size
         if type(size) != int:
             raise TypeError("size must be integer")
-        elif int(size) < 0:
-            raise ValueError("size must be >=0")
-    def area(self):
-        if (self.size) != int:
-            raise TypeError("size must be integer")
-        elif int(self.size) < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
         return self.__size ** 2
+
