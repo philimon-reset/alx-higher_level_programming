50a709d9aec182751741df0a6e1a8c72a85af3da
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 22:10:40 +0300

    Update main.py

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-Square = __import__('4-square').Square
+Square = __import__('6-square').Square
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+my_square_1 = Square(3)
+my_square_1.my_print()
 
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+print("--")
 
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
