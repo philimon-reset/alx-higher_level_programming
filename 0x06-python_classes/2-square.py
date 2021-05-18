7a6684b028ae0d0482f769a53a7d0261855212c4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 15:12:09 +0300

    Update 2-square.py

--- 0x06-python_classes/2-square.py
+++ 0x06-python_classes/2-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 class Square():
+    """square class"""
     def __init__(self, size=0):
+        """ Instance of class Square
+    Arguments:
+        @size: size of side of square"""
         self.__size = size
         if type(size) != int:
             raise TypeError("size must be integer")
-        elif int(size) < 0:
-            raise ValueError("size must be >=0")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
