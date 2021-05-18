819d5f9cde58f2a8926cf6438bf8d6678184d159
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 15:18:33 +0300

    Update 6-square.py

--- 0x06-python_classes/6-square.py
+++ 0x06-python_classes/6-square.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 class Square():
+    """square class"""
     def __init__(self, size=0, position=(0, 0)):
+        """ Instance of class Square
+    Arguments:
+        @size: size of side of square
+        @position: amount of spaces used"""
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ getter of size
+    Return:
+            value of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ setter of the size
+    Arguments:
+        @value: value of size"""
         self.__size = value
         if type(value) != int:
             raise TypeError("size must be integer")
@@ -18,10 +29,16 @@ class Square():
 
     @property
     def position(self):
+        """ getter of position
+    Return:
+            value of position"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """ setter of the position
+    Arguments:
+        @value: value of postion"""
         self.__position = value
         if (type(value) != tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -29,11 +46,15 @@ class Square():
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
-        if (self.__size == 0):
+        """ square made using the character #
+            or a blank line if @size == 0"""
+        if (self.size == 0)
             print("")
-        else:
-            for i in range(self.size):
-                print((self.__position[0] * " ") + ("#" * self.__size))
+        for i in range(self.size):
+            print("#" * self.size)
 
     def area(self):
+        """ area of square
+            Return:
+                    area of square."""
         return self.__size ** 2
