ef3f100ea95c95df5b46dfba9559ee0794f15d1e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 20:52:30 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,10 @@
 class Square():
     """square class"""
 
-    def __init__(self, size):
+    def __init__(self, size=None):
         """ Instance of class Square
 
         Args: 
-            size:  of side of square
+            size (int):  of side of square
         """
         self.__size = size
