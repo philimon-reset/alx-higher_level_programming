8d99c3ac34c32b6338e7610982b286aab9322018
philimon-reset <philimonderib2019@gmail.com>
2021-05-18 08:44:35 +0000

    1 up to 5

---
+++ 0x06-python_classes/4-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square():
+    def __init__(self, size=0):
+        self.__size = size
+        self.size = size
+        if type(size) != int:
+            raise TypeError("size must be integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >=0")
+    def area(self):
+        if (self.size) != int:
+            raise TypeError("size must be integer")
+        elif int(self.size) < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size ** 2
