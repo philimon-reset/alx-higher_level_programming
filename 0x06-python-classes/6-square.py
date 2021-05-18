00bd92e5112294a7284305c45a22742bbc1f9f68
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-18 22:28:23 +0300

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,9 +82,5 @@ class Square():
         else:
             for x in range(self.position[1]):
                 print()
-            for row in range(self.size):
-                for spaces_X in range(self.position[0]):
-                    print(" ", end="")
-                for row in range(self.size):
-                    print("#", end="")
-                print()
+            for i in range(self.size):
+                print((self.position[0] * " ") + ("#" * self.size))
