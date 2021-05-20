882bc8d9873d7751c6d4b95bf552a555247fa8a6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-20 13:24:14 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,11 @@
-#
+#!/usr/bin/python3
+def add_integer(a, b=98):
+        """ add integer 
+    Arguments:
+        @a: first integer
+	@b: second integer"""
+	if not isinstance(a, int) and not isinstance(a, float):
+		raise TypeError("a not int")
+	if not isinstance(b, int) and not isinstance(b, float):
+		raise TypeError("b not int")
+	return int(a) + int(b)
