92b85271b275d29c9f400259754c4d6921a9fc60
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 18:22:05 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,14 +5,17 @@ def matrix_divided(matrix, div):
         @matrix: matrix to be changed
         @div: divisable number"""
         if not isinstance(matrix, list):
-                raise TypeError("matrix must be matrix")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         elif not isinstance(div, int) and not isinstance(div, float):
                 raise TypeError("div must be a number")
         elif div == 0:
                 raise ZeroDivisionError("division by Zero")
         for i in range(len(matrix)):
                 if not isinstance(matrix[i], list):
-                        raise TypeError("matrix must be matrix")
+                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                for x in matrix[i]:
+                        if not isinstance(x, int) and not isinstance(x, float):
+                                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
                 L = len(matrix[0])
                 if len(matrix[i]) != L:
                         raise TypeError("Each row of the matrix must have the same size")
@@ -20,8 +23,6 @@ def matrix_divided(matrix, div):
         for i in range(len(matrix)):
                 new = []
                 for x in matrix[i]:
-                        if not isinstance(x, int) and not isinstance(x, float):
-                                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
                         a = round(x/div, 2)
                         new.append(a)
                 large.append(new)
