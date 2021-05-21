1ea990b9dc67619a992360ead3136f8adfaca864
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 23:06:56 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""Module containing a function that prints a string in a certain format"""
-
-
 def text_indentation(text):
-    """ prints a string line by line based on delimiters
+        """ text_indentation
         Arguments:
-                 @text: text to be printed
-    """
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-    text = text.strip()
-    if len(text) == 0:
-        return
-    temp = text[0]
-    delims = ['.', ':', '?']
-    for char in text:
-        if char == ' ' and temp in delims:
-            continue
-        temp = char
-        print(char, end="")
-        if char in delims:
-            print("\n\n", end="")
+        @text: text to be changed"""
+        if type(text) is not str:
+                raise TypeError("text must be a string")
+        text = text.strip()
+        if len(text) == 0:
+                return
+        temp = text[0]
+        delims = ['.', ':', '?']
+        for char in text:
+                if char == ' ' and temp in delims:
+                        continue
+                temp = char
+                print("{}".format(char), end = "")
+                if char in delims:
+                        print()
+                        print()
