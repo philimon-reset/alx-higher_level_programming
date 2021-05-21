a558d253979e0565b3455faa488078e30ed23b9f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 23:11:27 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,7 @@ class TestMax(unittest.testcase):
 		"""
 		test correct output"""
 		self.assertAlmostEqual(max_integer([1,2,3,5,7,1], 7))
+		self.assertAlmostEqual(max_integer([2], 2))
 		self.assertAlmostEqual(max_integer([], None))
+		self.assertAlmostEqual(max_integer(["h", "e", "l", "l"], "l")
 		self.assertAlmostEqual(max_integer("hello", "h"))
-		
