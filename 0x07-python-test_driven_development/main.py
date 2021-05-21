cd15975ec530b6335153864d7ba0e82813baf6ea
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 18:49:22 +0300

    Update main.py

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-             [1, 2, 3], [4, 2, 5]
+temp = [
+            [1, 2, 3], [4, 2, 5]
 ]
 
-print(matrix_divided(matrix, 5))
-print(matrix)
+print(matrix_divided(temp,5))
+print(temp)
