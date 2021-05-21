adebcd2427b3b46253a252a8a15a2984c17fc61f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 22:34:35 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,21 +4,24 @@ def matrix_divided(matrix, div):
         Arguments:
         @matrix: matrix to be changed
         @div: divisable number"""
+        error_1 = "matrix must be a matrix (list of lists) of integers/floats"
+        error_2 = "Each row of the matrix must have the same size"
+        div_error = "div must be a number"
         if not isinstance(matrix, list):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(error_1)
         elif not isinstance(div, int) and not isinstance(div, float):
-                raise TypeError("div must be a number")
+                raise TypeError(div_error)
         elif div == 0:
                 raise ZeroDivisionError("division by Zero")
         for i in range(len(matrix)):
                 if not isinstance(matrix[i], list):
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                        raise TypeError(error_1)
                 for x in matrix[i]:
                         if not isinstance(x, int) and not isinstance(x, float):
-                                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                                raise TypeError(error_1)
                 L = len(matrix[0])
                 if len(matrix[i]) != L:
-                        raise TypeError("Each row of the matrix must have the same size")
+                        raise TypeError(error_2)
         large = []
         for i in range(len(matrix)):
                 new = []
