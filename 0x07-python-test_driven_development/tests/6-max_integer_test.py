ad88c8612876d0ffd577f8126fb0ada244748795
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 23:14:58 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import unitest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMax(unittest.testcase):
 	""" test class
