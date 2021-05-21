868b013bfe1adcf7991f00f963f8d82c89242a42
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 18:07:43 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@ def add_integer(a, b=98):
         @a: first integer
         @b: second integer"""
         if not isinstance(a, int) and not isinstance(a, float):
-                raise TypeError("a not int")
+                raise TypeError("a must be an integer")
         if not isinstance(b, int) and not isinstance(b, float):
-                raise TypeError("b not int")
+                raise TypeError("b must be an integer")
         return int(a) + int(b)
