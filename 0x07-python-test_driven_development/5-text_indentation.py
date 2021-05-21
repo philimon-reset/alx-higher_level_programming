22f7a8986a371d3d068eaf0f7ebd32110beca4de
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 22:39:21 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 def text_indentation(text):
-	""" text_indentation
-	Arguments:
-	@text: text to be changed"""
-	if type(text) is not str:
-		raise TypeError("text must be a string")
-	text = text.strip()
-	if len(text) == 0:
-		return
-	temp = text[0]
-	delims = ['.', ':', '?']
-	for char in text:
-		if char == ' ' and temp in delims:
-			continue
-		temp = char
-		print(char, end="")
-		if char in delims:
-			print("\n\n", end="")
+        """ text_indentation
+        Arguments:
+        @text: text to be changed"""
+        if type(text) is not str:
+                raise TypeError("text must be a string")
+        text = text.strip()
+        if len(text) == 0:
+                return
+        temp = text[0]
+        delims = ['.', ':', '?']
+        for char in text:
+                if char == ' ' and temp in delims:
+                        continue
+                temp = char
+                print(char, end="")
+                if char in delims:
+                        print("\n\n", end="")
