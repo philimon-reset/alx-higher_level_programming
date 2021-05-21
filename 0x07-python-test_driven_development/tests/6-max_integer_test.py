4ff657ccbe41b9efb60d6aa90c2cf99ad24a23ea
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 23:25:05 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,32 @@
+  
 #!/usr/bin/python3
-import unittest
-max_integer = __import__('M-max_integer').max_integer
+"""Unittest for max_integer([..])
+"""
 
-class TestMaxInteger(unittest.Testcase):
-	""" test class
-	"""
-	def test_max(self):
-		"""
-		test correct output"""
-		self.assertAlmostEqual(max_integer([1,2,3,5,7,1], 7))
-		self.assertAlmostEqual(max_integer([2], 2))
-		self.assertAlmostEqual(max_integer([], None))
-		self.assertAlmostEqual(max_integer(["h", "e", "l", "l"], "l"))
-		self.assertAlmostEqual(max_integer("hello", "h"))
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """test class for max_integer"""
+    def test_ProperInput(self):
+        """test proper input"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
+        self.assertEqual(max_integer([2.0, 2.1, 2.3]), 2.3)
+
+    def test_EmptyInput(self):
+        """test empty input"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_ImproperInput(self):
+        """test improper input"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+            max_integer(["a", 3, 2.0])
+            max_integer([[1, 2], 6])
+            max_integer([{1, 2}, 6])
 
 if __name__ == "__main__":
     unittest.main()
