07c963e39c0ea5960be741b9cc13c62ab2ecb9dd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-21 22:37:45 +0300

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_square(size):
-	if isinstance(size, float) and size < 0:
-		raise TypeError("size must be integer")
-	elif not isinstance(size, int):
-		raise TypeError("size must be integer")
-	elif size < 0:
-		raise TypeError("size must be >= 0")
-	elif size == 0:
-		print()
-	for row in range(1, size + 1):
-		print("#" * size)
+        if isinstance(size, float) and size < 0:
+                raise TypeError("size must be integer")
+        elif not isinstance(size, int):
+                raise TypeError("size must be integer")
+        elif size < 0:
+                raise TypeError("size must be >= 0")
+        elif size == 0:
+                print()
+        for row in range(1, size + 1):
+                print("#" * size)
