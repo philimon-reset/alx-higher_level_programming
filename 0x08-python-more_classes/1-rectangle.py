a809a13be9e7091e56d302626b68994769f6cf00
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-24 20:00:59 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,45 @@
 #!/usr/bin/python3
-"""square class main"""
+"""Module consisting of Rectangle class to represent rectangles"""
 
 
-class Square():
-    """square class"""
+class Rectangle():
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
 
-    def __init__(self, size=None):
-        """ Instance of class Square
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
 
-        Args:
-           size (int):  of side of square
-        """
-        self.__size = size
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
