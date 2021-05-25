257be64ed4d219e7ab57bd761fd34dcad963527d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-25 07:18:47 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle():
         """ perimeter of rectangle
         Return:
         perimeter of rectangle."""
-        if self.__width == 0 or self.__height = 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__width + self.__height)
