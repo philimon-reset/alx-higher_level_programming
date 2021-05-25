f63ba8a95708a038864afffc8750adebcda9a12a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-25 06:53:59 +0300

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -24,12 +24,14 @@ class Rectangle():
             type(self).number_of_instances += 1
             self.__width = width
             self.__height = height
+
     @property
     def width(self):
         """ width of rectangle
         Return:
         width of rectangle."""
         return self.__width
+
     @width.setter
     def width(self, value):
         """ setter of the width
@@ -48,6 +50,7 @@ class Rectangle():
         Return:
         height of rectangle."""
         return self.__height
+
     @height.setter
     def height(self, value):
         """ setter of the width
@@ -65,6 +68,7 @@ class Rectangle():
         Return:
         area of rectangle."""
         return self.__width * self.__height
+
     def perimeter(self):
         """ perimeter of rectangle
         Return:
@@ -73,6 +77,7 @@ class Rectangle():
             return 0
         else:
             return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """ rectangle made using the character #
         """
@@ -85,13 +90,16 @@ class Rectangle():
             else:
                 string += (type(self).print_symbol * self.__width)
         return string
+
     def __repr__(self):
         """ represent self for eval"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
     def __del__(self):
         """delete self and count"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the largest rectangle"""
