29d0244116267f42a55c5e886d4c2127e3945ddf
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-25 07:06:58 +0300

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,8 +4,6 @@
 
 class Rectangle():
     """Rectangle Class"""
-    number_of_instances = 0
-
     def __init__(self, width=0, height=0):
         """ Instance of class Rectangle
         Arguments:
