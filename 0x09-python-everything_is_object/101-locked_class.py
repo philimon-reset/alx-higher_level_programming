2b410f362b5d9cca3962e0d1e649dc1589b439a0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-26 16:40:26 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,3 @@
 class LockedClass():
     """locked class for one attribute"""
     __slots__ = ["first_name"]
-
-    def __init__(self, first_name):
-        """assigined set attribute"""
-        self.first_name = first_name
