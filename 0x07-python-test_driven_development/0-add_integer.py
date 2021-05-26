fe9c0030516150ec44dd18c759baa5fc2e0995cc
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-26 12:49:22 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""add_integer"""
+
+
 def add_integer(a, b=98):
-        """ add integer
+        """ add_integer
         Arguments:
         @a: first integer
         @b: second integer"""
