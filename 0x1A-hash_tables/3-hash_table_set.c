6598a27b354a19d918a00ac9bfaf38ab47130466
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 23:53:01 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			temp2 = temp2->next;
 		}
 		if (strcmp(key, temp2->key) == 0)
-			return (temp);
+			return (1);
 		if (ht->array[new] == NULL)
 			ht->array[new] = temp;
 		else
