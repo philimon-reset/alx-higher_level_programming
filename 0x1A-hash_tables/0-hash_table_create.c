678d79901abc8006a8d74c9a22e098e7ab8d1364
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 14:15:20 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table.
+ * @size: size of table
+ *
+ * Return: hash_table or NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *temp;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			temp->array[i] = NULL;
+		}
+		return temp;
+	}
+	return NULL;
+}
