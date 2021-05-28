d92385fd9305f777ed641f2cf8c79846d483707e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 23:22:04 +0300

    Update test.c

--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -11,8 +11,19 @@
 int main(void)
 {
     hash_table_t *ht;
+    int i;
 
     ht = hash_table_create(1024);
     hash_table_set(ht, "betty", "holberton");
+    hash_table_set(ht, "hetairas", "mentioner");
+    hash_table_set(ht, "depravement", "serafins");
+    hash_table_set(ht, "joyful", "synaphea");
+    printf("start\n");
+    for (i = 0; i < 1024; i++)
+    {
+        if (ht[i] == NULL)
+            printf("%d\t----\t", i);
+        else:
+            printf("%d\t%s----\t", i, ht->array);
     return (EXIT_SUCCESS);
 }
