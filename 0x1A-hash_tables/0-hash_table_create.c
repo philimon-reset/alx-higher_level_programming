2d481519ee714cec9ef6c0bbc92cc987c49555ce
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 20:29:53 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
-	hash_table_t *temp = NULL;
 
 	temp = malloc(sizeof(hash_table_t));
 	if (temp == NULL)
 		return (NULL);
 	temp->size = size;
-	temp->*array = malloc(size * sizeof(hash_node_t));
+	temp->array = &(malloc(size * sizeof(hash_node_t)));
 	if (temp->array == NULL)
 	{
 		free(temp);
