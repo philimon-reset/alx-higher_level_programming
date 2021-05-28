92817bbbbe0e3b2f9ad2d7cf4561e58e497a6a58
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 22:43:53 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  *
  * Return: void
  */
-void *add_node(hash_node_t **head, char *cpy_v, char *key)
+void *add_node(hash_node_t **head, char *cpy_v,const char *key)
 {
 	hash_node_t *temp = malloc(sizeof(hash_node_t));
 
