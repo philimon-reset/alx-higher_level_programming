750025919306db48c11d5332bfceb9713612d812
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 23:02:26 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int new;
 	char *cpy_v = strdup(value);
-	const unsigned char *cpy_k = strdup(key);
+	const unsigned char *cpy_k = strdup((unsigned char *)key);
 	hash_node_t *temp = NULL;
 
 
