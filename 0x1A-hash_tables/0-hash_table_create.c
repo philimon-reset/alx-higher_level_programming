2a5f0f00f8145edb1910e6d95e983c670cf989e7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-28 19:09:48 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i = 0;
 	hash_table_t *temp = NULL;
-	hash_node_t *node = NULL;
 
 	if (size > 0)
 	{
@@ -24,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 			return (NULL);
 		}
 		for (; i < size; i++)
-			temp->array[i] = node;
+			temp->array[i] = NULL;
 		return (temp);
 	}
 	return (NULL);
