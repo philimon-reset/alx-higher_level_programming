bb93c9d1bd9f0838c08f610fc3eeef5b82f9af6d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-30 20:14:12 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int new;
+	hash_node_t *temp = ht->array[new];
 
 	if (ht == NULL)
 		return (NULL);
@@ -17,8 +18,37 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		new = key_index((unsigned char *)key, ht->size);
 		if (ht->array[new] == NULL)
 			return (NULL);
+		else if (check(ht, key))
+		{
+			while (temp->key != key)
+				temp = temp->next;		
+			return (temp->value);
+		}
 		else
 			return ((ht->array[new])->value);
 	}
 	return (NULL);
+}
+
+/**
+ * check - check if the current linked ist is being overwirten
+ * @ht: hash table
+ * @key: key to be checked
+ *
+ * Return: 1 if same 0 if not
+ */
+int check(hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int new;
+
+	new = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[new];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (1);
+		temp = temp->next;
+	}
+	return (0);
 }
