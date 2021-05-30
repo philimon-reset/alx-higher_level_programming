b366f892382c0a5b682b9fc2cd84c0893989d45c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-30 19:42:13 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp = NULL;
 
 
-	if (key != NULL && value != NULL)
+	if (key != NULL && value == NULL)
 	{
 		new = key_index((unsigned char *)key, ht->size);
 		temp = malloc(sizeof(hash_node_t));
