b72a7461878d9e800f4111261c9c7a982b492f81
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-30 20:04:09 +0300

    Update test.c

--- 0x1A-hash_tables/test.c
+++ 0x1A-hash_tables/test.c
@@ -11,11 +11,33 @@
 int main(void)
 {
     hash_table_t *ht;
+    char *value;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "holberton");
-    hash_table_set(ht, "hetairas", "mentioner");
-    hash_table_set(ht, "depravement", "serafins");
-    hash_table_set(ht, "joyful", "synaphea");
+    hash_table_set(ht, "c", "fun");
+    hash_table_set(ht, "python", "awesome");
+    hash_table_set(ht, "Jennie", "and Jay love asm");
+    hash_table_set(ht, "N", "queens");
+    hash_table_set(ht, "Asterix", "Obelix");
+    hash_table_set(ht, "Betty", "Holberton");
+    hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "c", "isfun");
+
+    value = hash_table_get(ht, "python");
+    printf("%s:%s\n", "python", value);
+    value = hash_table_get(ht, "Jennie");
+    printf("%s:%s\n", "Jennie", value);
+    value = hash_table_get(ht, "N");
+    printf("%s:%s\n", "N", value);
+    value = hash_table_get(ht, "Asterix");
+    printf("%s:%s\n", "Asterix", value);
+    value = hash_table_get(ht, "Betty");
+    printf("%s:%s\n", "Betty", value);
+    value = hash_table_get(ht, "98");
+    printf("%s:%s\n", "98", value);
+    value = hash_table_get(ht, "c");
+    printf("%s:%s\n", "c", value);
+    value = hash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);
     return (EXIT_SUCCESS);
 }
