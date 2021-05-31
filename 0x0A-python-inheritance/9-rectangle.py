409b0f5c7804bad1bf5956d663a983804523aa6d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:08:35 +0300

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
-"""inheritance from list"""
+"""base geometry class"""
 
 
-""" check if its the same class """
-def inherits_from(obj, a_class):
+class BaseGeometry():
+        """base geometry class main part"""
+        def area(self):
+                """area of class"""
+                raise Exception("area() is not implemented")
 
-        if issubclass(obj, a_class):
-                return True
-        return False
+        def integer_validator(self, name, value):
+                if not isinstance(value, int):
+                        raise TypeError("{} must be an integer".format(name))
+                if value <= 0:
+                        raise TypeError("{} must be greater than 0".format(name))
+
+"""Rectangle class to represent a square"""
+
+
+class Rectangle(BaseGeometry):
+        """Rectangle Class"""
+        def __init__(self, width=0, height=0):
+                self.integer_validator("width", width)
+                self.integer_validator("height", height)
+                self.__width = width
+                self.__height = height
+
+        def area(self):
+                """ area of rectangle """
+                return self.width * self.height
+
+        def __str__(self):
+                """returns when called"""
+                return "[Rectangle] {}/{}".format(self.width, self.height)
