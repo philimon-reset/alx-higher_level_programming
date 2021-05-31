ebd44bfd86de97ea141a2df76cdfecfd8e29babf
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 23:12:23 +0300

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,7 +2,6 @@
 """look up class"""
 
 
-def lookup(object):
-    """start of lookup
-        Return: list of used methods and attributes"""
-    return dir(object)
+def add_attribute(obj, name, value)
+    """start of lookup"""
+    return setattr(obj, name, value)
