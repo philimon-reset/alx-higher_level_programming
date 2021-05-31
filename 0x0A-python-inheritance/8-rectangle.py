144814af9b40dc9f63a8683b9ca38d4c4d3c7f76
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:57:46 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-"""inheritance from list"""
+"""base geometry class"""
 
 
-""" check if its the same class """
-def inherits_from(obj, a_class):
+class BaseGeometry():
+        """base geometry class main part"""
+        def area(self):
+                """area of class"""
+                raise Exception("area() is not implemented")
 
-        if issubclass(obj, a_class):
-                return True
-        return False
+        def integer_validator(self, name, value):
+                if not isinstance(value, int):
+                        raise TypeError("{} must be an integer".format(name))
+                if value <= 0:
+                        raise TypeError("{} must be greater than 0".format(name))
+
+"""Rectangle class to represent a square"""
+
+
+class Rectangle(BaseGeometry):
+        """Rectangle Class"""
+        def __init__(self, width=0, height=0):
+                self.integer_validator(str(width), width)
+                self.integer_validator(str(height), height)
+                self.__width = width
+                self.__height = height
