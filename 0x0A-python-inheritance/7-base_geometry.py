52fae4f0edda8a9933268648cbeb3a457e83af1d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:33:15 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,14 +3,14 @@
 
 
 class BaseGeometry():
-        """base geometry class main part"""
-        def area(self):
-                """area of class"""
-                raise Exception("area() is not implemented")
+    """base geometry class main part"""
+    def area(self):
+            """area of class"""
+            raise Exception("area() is not implemented")
 
-        def integer_validator(self, name, value):
-                """integer checker"""
-                if not isinstance(value, int):
-                        raise TypeError("{} must be an integer".format(name))
-                if value <= 0:
-                        raise ValueError("{} must be greater than 0".format(name))
+    def integer_validator(self, name, value):
+            """integer checker"""
+            if not isinstance(value, int):
+                    raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+                    raise ValueError("{} must be greater than 0".format(name))
