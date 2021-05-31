5c9d982dea4a9c2a30bebdb9af01d65236c948e2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:10:50 +0300

    Update main.py

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+is_same_class = __import__('2-is_same_class').is_same_class
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
