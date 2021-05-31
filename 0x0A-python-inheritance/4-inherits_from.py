bc1f873fdcf8431b7eb33de730cd812b2583e16e
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:29:50 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,8 @@
 """inheritance from list"""
 
 
-        """ check if its the same class """
-        def inherits_from(obj, a_class):
-                if issubclass(obj, a_class):
-                        return True
-                return False
+""" check if its the same class """
+def inherits_from(obj, a_class):
+        if issubclass(obj, a_class):
+                return True
+        return False
