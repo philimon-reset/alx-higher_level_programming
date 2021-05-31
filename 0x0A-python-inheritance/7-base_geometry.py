e932911549a3a58ce5f150b74c906f1da17194e6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:25:02 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,4 +13,4 @@ class BaseGeometry():
                 if not isinstance(value, int):
                         raise TypeError("{} must be an integer".format(name))
                 if value <= 0:
-                        raise TypeError("{} must be greater than 0".format(name))
+                        raise ValueError("{} must be greater than 0".format(name))
