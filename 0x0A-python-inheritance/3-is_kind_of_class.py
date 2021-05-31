b22182b231c59dfb96885fd9ca94bef72aa79335
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 22:11:12 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@
 
 def is_kind_of_class(obj, a_class):
         """inherited or same"""
-        if isinstance(obj, a_class) or issubclass(obj, a_class):
+        if isinstance(obj, (a_class)) or type(obj) is a_class:
                 return True
         return False
