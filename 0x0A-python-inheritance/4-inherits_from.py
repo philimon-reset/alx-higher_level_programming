ea2a5463a538de32e51435639b3586efdde8a6b7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 22:17:25 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
         """inherit"""
-        if issubclass(obj, (a_class)) and type(obj) is not a_class:
+        if issubclass(type(obj), (a_class)) and type(obj) is not a_class:
                 return True
         return False
