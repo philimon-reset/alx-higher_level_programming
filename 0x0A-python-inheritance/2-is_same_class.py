e321fdf8b040af0e29058d2065a7016f2bd0c989
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:43:02 +0300

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,6 @@
 
 def is_same_class(obj, a_class):
         """ same class """
-        if isinstance(obj, a_class):
+        if isinstance(obj, (a_class)):
                 return True
         return False
