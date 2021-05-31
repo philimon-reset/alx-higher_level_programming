13cc04dcc2efaf59e15a04e521c36066eca71e16
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:21:56 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@
 
 """ check if its the same class """
         def is_kind_of_class(obj, a_class):
-                if isinstance(obj, a_class) or issubclass(Triangle, Polygon):
+                if isinstance(obj, a_class) or issubclass(obj, a_class):
                         return True
                 return False
