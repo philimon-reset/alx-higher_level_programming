625328b62ad76a6812398a4d220c1637ea15db71
philimon-reset <philimonderib2019@gmail.com>
2021-05-31 17:15:13 +0000

    added

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""inheritance from list"""
+
+
+""" check if its the same class """
+        def is_same_class(obj, a_class):
+                if type(obj) == a_class:
+                        return True
+                return False
