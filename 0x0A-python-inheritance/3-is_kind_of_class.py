13c026d6cc38fe4f8b0af1eb38c70c1bdfb2461a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:17:52 +0300

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,7 @@
 
 
 """ check if its the same class """
-        def is_same_class(obj, a_class):
+        def is_kind_of_class(obj, a_class):
                 if type(obj) == a_class:
                         return True
                 return False
