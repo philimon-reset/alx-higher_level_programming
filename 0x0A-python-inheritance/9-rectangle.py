34df518fa22389fdb178ffb87bf0fbde9da00b09
philimon-reset <philimonderib2019@gmail.com>
2021-05-31 17:35:33 +0000

    added

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""inheritance from list"""
+
+
+""" check if its the same class """
+def inherits_from(obj, a_class):
+
+        if issubclass(obj, a_class):
+                return True
+        return False
