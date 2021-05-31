38d9ec4b0b0d6179fec9383b29cd7aefb7e4601c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:47:30 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""inheritance from list"""
+"""base geometry class"""
 
 
-""" check if its the same class """
-def inherits_from(obj, a_class):
+class BaseGeometry():
+        """base geometry class main part"""
+        def area(self):
+                """area of class"""
+                raise Exception("area() is not implemented")
 
-        if issubclass(obj, a_class):
-                return True
-        return False
+        def integer_validator(self, name, value):
+                if not isinstance(value, int):
+                        raise TypeError("{} must be an integer".format(name))
+                if value <= 0:
+                        raise TypeError("{} must be greater than 0".format(name))
