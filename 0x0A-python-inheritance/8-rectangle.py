64527f146e632dff7a50f31af4af6e11f545c1b1
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 22:22:26 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,9 +11,9 @@ class BaseGeometry():
     def integer_validator(self, name, value):
             """integer checker"""
             if not isinstance(value, int):
-                    raise TypeError("{} must be an integer".format(name))
+                    raise TypeError("{:s} must be an integer".format(name))
             if value <= 0:
-                    raise ValueError("{} must be greater than 0".format(name))
+                    raise ValueError("{:s} must be greater than 0".format(name))
 
 """Rectangle class to represent a square"""
 
@@ -23,6 +23,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width=0, height=0):
             """init"""
             self.integer_validator("width", width)
-            self.integer_validator("height", height)
             self.__width = width
+            self.integer_validator("height", height)
             self.__height = height
