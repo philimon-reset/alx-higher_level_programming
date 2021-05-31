ccfe9a25cb8a340ee510b5de2951d772f5f815d7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 22:50:11 +0300

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry():
 
     def integer_validator(self, name, value):
             """integer checker"""
-            if type(value) is not int:
+            if not isinstance(value, int):
                     raise TypeError("{} must be an integer".format(name))
             if value <= 0:
                     raise ValueError("{} must be greater than 0".format(name))
