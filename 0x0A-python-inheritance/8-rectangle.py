f4de0b638c6c69703c15d3989e5066e9f8814fb2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:32:35 +0300

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,26 +3,26 @@
 
 
 class BaseGeometry():
-        """base geometry class main part"""
-        def area(self):
-                """area of class"""
-                raise Exception("area() is not implemented")
+    """base geometry class main part"""
+    def area(self):
+            """area of class"""
+            raise Exception("area() is not implemented")
 
-        def integer_validator(self, name, value):
-                """integer checker"""
-                if not isinstance(value, int):
-                        raise TypeError("{} must be an integer".format(name))
-                if value <= 0:
-                        raise ValueError("{} must be greater than 0".format(name))
+    def integer_validator(self, name, value):
+            """integer checker"""
+            if not isinstance(value, int):
+                    raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+                    raise ValueError("{} must be greater than 0".format(name))
 
 """Rectangle class to represent a square"""
 
 
 class Rectangle(BaseGeometry):
-        """Rectangle Class"""
-        def __init__(self, width=0, height=0):
-                """init"""
-                self.integer_validator("width", width)
-                self.integer_validator("height", height)
-                self.__width = width
-                self.__height = height
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+            """init"""
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            self.__height = height
