8709ec6bd6a8ff938666e3b81abb7efc0cf9a209
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:28:58 +0300

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -43,4 +43,5 @@ class Square(Rectangle):
         def __init__(self, size):
                 """init"""
                 self.integer_validator("size", size)
+                super().__init__(size, size)
                 self.__size = size
