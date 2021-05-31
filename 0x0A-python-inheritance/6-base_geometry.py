a42457efcac4908078742da1cb739ffca85b0c7a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 20:40:15 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""inheritance from list"""
+"""base geometry class"""
 
 
-""" check if its the same class """
-def inherits_from(obj, a_class):
-
-        if issubclass(obj, a_class):
-                return True
-        return False
+class BaseGeometry():
+    """area of class"""
+    def area(self):
+                raise Exception("area() is not implemented")
