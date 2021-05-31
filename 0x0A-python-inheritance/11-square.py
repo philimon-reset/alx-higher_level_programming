15309b66486fbdd0d04d2c0a6e802b95579ae973
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-05-31 21:29:26 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -43,7 +43,7 @@ class Square(Rectangle):
         def __init__(self, size):
                 """init"""
                 self.integer_validator("size", size)
-                super().__init__(self, size)
+                super().__init__(size, size)
                 self.__size = size
 
         def area(self):
