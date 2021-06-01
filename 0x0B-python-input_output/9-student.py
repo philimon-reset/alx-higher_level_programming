d4188501a8000687b29f36336552f08692c2ace8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 19:20:28 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,6 +9,7 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """ class to json """
         return self.__dict__
