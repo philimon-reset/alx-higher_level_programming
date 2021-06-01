9bf04b2d9acdb8f5b89524cac6e47ba66d929838
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 18:16:08 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""start doc"""
+
+
 def read_file(filename=""):
     """ read to stdout """
-    with open(filename, mode='r', enncoding="utf-8") as mfile:
-        for i in mfile:
-            print("{}".format(i))
+    with open(filename, mode='r', encoding="utf-8") as mfile:
+        print(mfile.read())
