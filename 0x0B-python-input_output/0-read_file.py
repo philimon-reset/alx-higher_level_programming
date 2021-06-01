1a4328a5d2089d042033c396457f7db70aae1f65
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 18:13:14 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """ read to stdout """
     with open(filename, enncoding="utf-8") as mfile:
-        sys.stdout.write(mfile)
+        mfile.read()
