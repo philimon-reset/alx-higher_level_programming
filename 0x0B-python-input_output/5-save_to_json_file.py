235d7d2a6261de88dfdca7575f17f54dfa746745
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 18:59:23 +0300

    Update and rename 5-save_to_json_string.py to 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,4 +5,4 @@
 def save_to_json_file(my_obj, filename):
     """ append to stdout """
     with open(filename, mode='a', encoding="utf-8") as mfile:
-        return json.dump(my_obj, mfile)
+        json.dump(my_obj, mfile)
