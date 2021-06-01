46bccbb4efc5f8a1c78710c4e1601612780cd5f2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 11:33:33 +0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 """base geometry class"""
 
-Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""Rectangle class to represent a square"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+            """init"""
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            self.__height = height
+
+    def area(self):
+            """ area of rectangle """
+            return self.__width * self.__height
+
+    def __str__(self):
+            """returns when called"""
+            return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
 """class to represent a square"""
 
 
