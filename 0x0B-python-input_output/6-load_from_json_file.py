433c646d204cde20433007580f53ea764331b38a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 19:06:14 +0300

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ load json from file """
     with open(filename, mode='w') as mfile:
-        json.load(mfile, my_obj)
+        return json.load(mfile)
