1da6ee7d8c282eaa4c68b354b5b1fbd8c9ddee93
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 19:04:31 +0300

    Update and rename 6-load_from_json_string.py to 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""start doc"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ load json from file """
+    with open(filename, mode='w') as mfile:
+        json.load(my_obj, mfile)
