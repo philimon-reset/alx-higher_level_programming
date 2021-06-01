d12c94e663547a3e4ac64cb80f09790b4e98ba02
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 20:15:18 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,13 +2,29 @@
 """start doc"""
 
 
-def append_write(filename="", text=""):
-    """ append to stdout """
-    with open(filename, mode='a', encoding="utf-8") as mfile:
-        mfile.write(text)
-        count = 0
-        while True:
-            if count == len(text):
-                break
-            count += 1
-    return count
+class Student():
+    """ student info gather"""
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ class to json
+            @attrs: attribute list"""
+        if attrs is None:
+            return self.__dict__
+        temp = self.__dict__.keys()
+        new = {}
+        for i in attrs:
+            if type(i) != str:
+                return self.__dict__
+            if i in temp:
+                new.update({i: self.__dict__[i]})
+        return new
+
+    def reload_from_json(self, json):
+        """replace all attriubtes"""
+        for i in json:
+            self.__dict__[i] = json[i]
