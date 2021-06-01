50e580271149cfe8600a9b04d6694e700d3b4b91
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 23:46:56 +0300

    Create 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" start doc """
+
+def pascal_triangle(n):
+  """ interview question """
+  large = []
+  if n <= 0:
+      return large
+  for a in range(1, n+1):
+      temp = []
+      for v in range(0, a):
+          temp.append(1)
+      large.append(temp)
+
+  for i in range(1, n):
+      for b in range(i, 0, -1):
+          large[i][b - 1] = large[i - 1][b - 2] + large[i - 1][b - 1]
+          large[i][i] = 1
+          large[i][0] = 1
+  return large
+  
