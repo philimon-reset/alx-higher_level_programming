afa0c8b7f7e3ef191de4aa21e0969b24f08d4f05
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 21:41:46 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""contains append_after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file"""
+    with open(filename, "r+", encoding="utf-8") as f:
+        content = f.readlines()
+        f.seek(0)
+        f.truncate(0)
+        for i in range(len(content)):
+            f.write(content[i])
+            if search_string in content[i]:
+                f.write(new_string)
