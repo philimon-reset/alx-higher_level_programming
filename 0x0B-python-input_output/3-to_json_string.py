ceb624e9d35fac908f17fb26ad48d3c319a35ecb
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 18:47:31 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,13 +2,6 @@
 """start doc"""
 
 
-def append_write(filename="", text=""):
-    """ append to stdout """
-    with open(filename, mode='a', encoding="utf-8") as mfile:
-        mfile.write(text)
-        count = 0
-        while True:
-            if count == len(text):
-                break
-            count += 1
-    return count
+def to_json_string(my_obj):
+    """ json dump """
+    return (json.dumps(my_obj))
