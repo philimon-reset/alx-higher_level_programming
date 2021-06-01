fd4930b3c273b872958acd72cf689c1ce9075028
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-01 19:01:13 +0300

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@
 
 def save_to_json_file(my_obj, filename):
     """ append to stdout """
-    with open(filename, mode='w') as mfile:
+    with open(filename, 'w') as mfile:
         json.dump(my_obj, mfile)
