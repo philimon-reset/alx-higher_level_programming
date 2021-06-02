d32e53c8f73a65057ddfc4eb316479dbe19b08bd
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-03 01:20:03 +0300

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,6 @@
 
 def add_attribute(obj, name, value):
     """start of lookup"""
-    if not hasattr(obj, obj.__dict__):
+    if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
