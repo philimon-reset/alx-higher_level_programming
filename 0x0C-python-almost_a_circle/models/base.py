3dbb1575529e72da8dfc861dd8be863b3f634ab2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-06 22:01:34 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,9 +10,10 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        temp = []
+        if list_dictionaries == None or len(list_dictionaries) == 0:
+            return str(temp)
         temp = json.dumps(list_dictionaries[0])
-        if temp == None or len(temp) == 0:
-            return temp
         return temp
 
     @classmethod
