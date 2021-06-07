f2d2462cb7b09a6ef9fc8e262ee9e35b407844a5
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-07 17:51:21 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+""" main base class """
+
+
 class Base():
+    """ start of base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init """
         if id != None:
             self.id = id
         else:
@@ -10,17 +16,28 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ convert list rep of json file to string rep """
         temp = []
         if list_dictionaries == None or len(list_dictionaries) == 0:
             return str(temp)
         temp = json.dumps(list_dictionaries)
         return temp
 
+    @staticmethod
+    def from_json_string(json_string):
+        """ convert string rep of json file to list rep """
+        temp = []
+        if json_string == None or len(json_string) == 0:
+            return temp
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
+        """ save string rep of rep file to new file """
         temp = []
         if list_objs == None or len(list_objs) == 0:
-            return str(temp)
+            with open(cls.__name__ + ".json", 'w', encoding='utf8') as json_file:
+                json_file.write(temp)
         for v in list_objs:
             temp.append(v.to_dictionary())
         temp = cls.to_json_string(temp)
@@ -29,13 +46,13 @@ class Base():
 
     @classmethod
     def create(cls, **dictionary):
+        """ create dummy instances to fill in and update """
         if cls.__name__ == 'Rectangle':
             temp = cls(1, 1)
         elif cls.__name__ == 'Square':
             temp = cls(1)
         temp.update(**dictionary)
         return temp
-
 
 if __name__ == "__main__":
 
