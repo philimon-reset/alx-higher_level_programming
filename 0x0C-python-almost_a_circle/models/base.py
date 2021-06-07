78ce976316918c3776d9a5371c52c25ff8e33782
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-07 23:20:00 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,20 +67,3 @@ class Base():
             temp = cls(1)
         temp.update(**dictionary)
         return temp
-
-if __name__ == "__main__":
-
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
