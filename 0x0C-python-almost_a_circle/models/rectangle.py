4d50b2dd242463a832e6cdcfe4794e7f2f407850
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:11:31 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,7 @@ class Rectangle(Base):
                 print((self.x * " ") + ("#" * self.__width))
             else:
                 print((self.x * " ") + ("#" * self.__width))
+
+    def __str__(self):
+        """ staright printed value of instance """
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
