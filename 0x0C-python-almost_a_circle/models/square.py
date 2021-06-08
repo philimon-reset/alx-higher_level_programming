2aae757b93cf71ea65892e83948eff0decc1277f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:37:40 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,38 +14,4 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width)
-
-    @property
-    def size(self):
-        """ returns size """
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """ set width and height to same value """
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        """ update value of square attributes """
-        a = ["id", "width", "x", "y"]
-        if len(args) != 0 and args is not None:
-            for i in range(len(args)):
-                if i > len(a) - 1:
-                    break
-                setattr(self, a[i], args[i])
-        else:
-            for i in kwargs.keys():
-                if i in a:
-                    if i == "size":
-                        setattr(self, a[1], kwargs[i])
-                    else:
-                        setattr(self, i, kwargs[i])
-
-    def to_dictionary(self):
-        """ list rep of instances """
-        temp = {}
-        a = ["id", "size", "x", "y"]
-        for i in a:
-            temp[i] = getattr(self, i)
-        return temp
+    
