0f946a877c2bb60d99a16615e83e4de5c95e6fd2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:22:04 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,16 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """ update attribute value """
+        a = ["id", "width", "height", "x", "y"]
+        if len(args) != 0 and args is not None:
+            for i in range(len(args)):
+                if i > len(a) - 1:
+                    break
+                setattr(self, a[i], args[i])
+        else:
+            for i in kwargs.keys():
+                if i in a:
+                    setattr(self, i, kwargs[i])
