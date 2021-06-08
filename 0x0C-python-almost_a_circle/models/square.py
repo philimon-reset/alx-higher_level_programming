fea30597c120f3628121b7868a044b092a6b18d2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:49:49 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,16 +6,46 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """class to represent a Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Instantiation of a Square object
-        Args:
-            size (int): the size of the square
-            x (int): the x coordinate
-            y (int): the y coordinate
-            id (int): the id of the object
-        """
+        """square class"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ staright printed value of instance """
         return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                     self.y, self.width)
+    
+    @property
+    def size(self):
+        """ returns size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ set width and height to same value """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """ update value of square attributes """
+        a = ["id", "width", "x", "y"]
+        if len(args) != 0 and args is not None:
+            for i in range(len(args)):
+                if i > len(a) - 1:
+                    break
+                setattr(self, a[i], args[i])
+        else:
+            for i in kwargs.keys():
+                if i in a:
+                    if i == "size":
+                        setattr(self, a[1], kwargs[i])
+                    else:
+                        setattr(self, i, kwargs[i])
+
+    def to_dictionary(self):
+        """ list rep of instances """
+        temp = {}
+        a = ["id", "size", "x", "y"]
+        for i in a:
+            temp[i] = getattr(self, i)
+        return temp
+
