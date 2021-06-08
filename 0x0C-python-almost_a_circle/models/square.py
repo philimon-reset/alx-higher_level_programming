4271af65c30d0ebaf5d33e9d50b6fb9e1467c01a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:43:46 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" square class """
+"""contains Square class to represent squares"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ square class with rectangle and base inheritance """
+    """class to represent a Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """ init """
+        """Instantiation of a Square object
+        Args:
+            size (int): the size of the square
+            x (int): the x coordinate
+            y (int): the y coordinate
+            id (int): the id of the object
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
