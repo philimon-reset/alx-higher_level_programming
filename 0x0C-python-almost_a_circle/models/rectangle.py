b7fa3b2322df573b65e4105010ea39c93801fea8
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 15:07:49 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+    """ area of rectangle """
+    return self.__width * self.__height
