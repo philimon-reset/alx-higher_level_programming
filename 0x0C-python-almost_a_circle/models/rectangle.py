0385e7fc1969af9e6b3cb4d3dc64a48bf78f5ecb
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-08 14:04:10 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,3 +32,39 @@ class Rectangle(Base):
     def y(self):
         """ vertical offset """
         return self.__y
+    
+    @width.setter
+    def width(self, value):
+        """ set width of rectangle or set error value """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ set height of rectangle or set error value """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """ set x of rectangle or set error value """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """ set y of rectangle or set error value """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
