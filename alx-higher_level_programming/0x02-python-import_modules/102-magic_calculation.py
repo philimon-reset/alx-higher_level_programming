588d5cc0dda29ea8afd79d7a004fdeddf9f5383d
philimon-reset <philimonderib2019@gmail.com>
2021-06-18 15:29:22 +0000

    added

--- alx-higher_level_programming/0x02-python-import_modules/102-magic_calculation.py
+++ alx-higher_level_programming/0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-        from add_0 import add
-        a = 1
-        b = 2
-        print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
