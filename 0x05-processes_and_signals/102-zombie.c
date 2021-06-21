3e4a3d8267c72157e5033b7ddd288cc7b3e142a7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-21 20:55:12 +0300

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+int infinite_while(void);
+
+int main()
+{
+	pid_t child;
+	int n = 0;
+
+	while (n < 6)
+	{
+		child = fork();
+		printf("Zombie process created, PID: %d\n", getpid());
+		n++;
+	}
+	infinite_while();
+}
+
+/**
+ * infinite_while - indefinite while loop
+ *
+ * Return: void function
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
