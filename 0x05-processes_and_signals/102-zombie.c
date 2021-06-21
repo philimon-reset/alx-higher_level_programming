8be7dbaa3cfd3799382509acebb57eed56f93668
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-21 21:05:03 +0300

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include <errno.h>
-#include <dirent.h>
-#include <signal.h>
 int infinite_while(void);
 
 int main()
