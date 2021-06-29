f399c6182fa18ee1cae91eeb5140446414396678
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-29 16:10:42 +0300

    Add files via upload

---
+++ console.py
@@ -0,0 +1,134 @@
+import cmd
+import models
+import io
+import sys
+import json
+
+
+class HBNBCommand(cmd.Cmd):
+    """Simple command processor example."""
+    classes = ["BaseModel", "FileStorage"]
+
+    prompt = '(hbnb)'
+
+    def help_greet(self):
+        print("greet [person]")
+        print("Greet the named person")
+
+    def do_create(self, line):
+        """ Create Basemodel instance"""
+        if line and line in self.classes:
+            temp_ins = models.dummy_ins[self.classes[0]]()
+            models.storage.save()
+            if models.storage.all().keys():
+                for i, dic in models.storage.all().items():
+                    if (i.split('.')[1] == temp_ins.id):
+                        models.storage.reload()
+            else:
+                models.storage.save()
+            print(temp_ins.id)
+        elif line:
+            print("** class doesn't exist **")
+        else:
+            print("** class name missing ** ")
+
+    def do_show(self, line):
+        """ show Basemodel instance"""
+        if line:
+            liner = line.split()
+            if len(liner) > 1:
+                if (liner[0] and liner[1]) and liner[0] in self.classes:
+                    a = 0
+                    for i, dic in models.storage.all().items():
+                        a += 1
+                        if i.split('.')[1] == liner[1]:
+                            print(dic)
+                            break
+                        elif (a == len(models.storage.all())):
+                            print("** no instance found **")
+                elif liner[0] not in self.classes:
+                    print("** class doesn't exist **")
+                else:
+                    print("** no instance found **")
+            elif line not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class name missing ** ")
+
+    def do_all(self, line):
+        """ print all instances """
+        if line in self.classes:
+            temp = []
+            for i, di in models.storage.all().items():
+                temp.append(str(di))
+            print(temp)
+        else:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        """ Destroy instance """
+        if line:
+            liner = line.split()
+            if len(liner) > 1:
+                if (liner[0] and liner[1]) and liner[0] in self.classes:
+                    a = 0
+                    for i, dic in models.storage.all().items():
+                        a += 1
+                        if i.split('.')[1] == liner[1]:
+                            models.storage.all().pop(i).save()
+                            break
+                        # THE FUCKKKKKK
+                        elif (a == len(models.storage.all())):
+                            print("** no instance found **")
+                else:
+                    print("** no instance found **")
+            elif line not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class name missing ** ")
+
+    def do_update(self, line):
+        """ update instance """
+        if line:
+            liner = line.split()
+            if len(liner) > 1:
+                if (liner[0] and liner[1]) and liner[0] in self.classes:
+                    a = 0
+                    for i, dic in models.storage.all().items():
+                        a += 1
+                        if i.split('.')[1] == liner[1]:
+                            # working space
+                            pass
+                        elif (a == len(models.storage.all())):
+                            print("** no instance found **")
+                else:
+                    print("** no instance found **")
+            elif line not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class name missing ** ")
+
+    def do_EOF(self, line):
+        """EOF Quit command to exit the program"""
+        return True
+
+    def do_quit(self, line):
+        """ Quit command to exit the program. """
+        return True
+
+    def postloop(self):
+        print
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
+    else:
+        HBNBCommand().cmdloop()
