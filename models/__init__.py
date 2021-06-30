3350adab09db2e51579eac79882f97af6af58e90
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-30 14:15:14 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-from models.engine import file_storage
-from models.base_model import BaseModel
-from models.user import User
-
-dummy_classes = {"BaseModel": BaseModel, "User": User}
-
-storage = file_storage.FileStorage()
-storage.reload()
-=======
 #!/usr/bin/python3
 from models.engine import file_storage
 from models.base_model import BaseModel
@@ -23,4 +12,3 @@ dummy_classes = {"BaseModel": BaseModel, "User": User,
 
 storage = file_storage.FileStorage()
 storage.reload()
->>>>>>> phili
