6e0de11faf00e081717ccb3cdbf6a8d57e5ac2a6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-14 00:00:17 +0300

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,7 @@
---
+-- as
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+  FROM tv_shows
+  INNER JOIN tv_show_ratings
+  ON tv_shows.id =tv_show_ratings.show_id
+  GROUP BY tv_shows.title
+  ORDER BY rating DESC;
