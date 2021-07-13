96b7a3590920f6c582c1c0e4dbbebafaae851123
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-13 18:49:42 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- create new user with all privileges
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
