527f1cf096e0751818f3d5275cda48674c5b624d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-13 19:11:37 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS `cities`
 (
   `id` INT NOT NULL UNIQUE AUTO_INCREMENT,
