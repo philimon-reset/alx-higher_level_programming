61b3d86c677a3e318cdbba7f1cd51a0faf4ba1c9
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-13 19:03:28 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,10 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT cities (
-  id INT NOT NULL AUTO_INCREMENT,
-  state_id INT NOT NULL,
-  name VARCHAR(256),
-  PRIMARY KEY (id),
-  FOREIGN KEY (state_id) REFERENCES states(id)
+  
+       id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id),
+       FOREIGN KEY (state_id) REFERENCES states(id)
 );
