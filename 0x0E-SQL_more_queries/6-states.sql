f65f350639507d0cca557bae46491a4248534f58
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-13 17:54:51 +0300

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,6 @@
---
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.`states` (
+  `id` INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL
+);
