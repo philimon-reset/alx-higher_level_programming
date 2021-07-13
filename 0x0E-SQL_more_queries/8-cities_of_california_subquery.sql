489b47fdc1493cc92271b256e1a6e0d2140ccde3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-13 20:02:14 +0300

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,1 +1,2 @@
---
+-- lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT `cities` FROM (SELECT `name` FROM `states` WHERE `name`='California') ORDER BY cities.id;
