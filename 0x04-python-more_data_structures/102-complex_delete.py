2d1ee98741d1904e7703fa75439b50a33671a88b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-14 16:01:08 +0300

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for i in a_dictionary.keys():
+    for i in a_dictionary:
         if a_dictionary[i] is value:
             a_dictionary.pop(i)
     return a_dictionary
