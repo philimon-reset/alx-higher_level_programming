028d137b6797f2b482e40e6889252dca26ae1549
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-14 15:37:43 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,143 @@
+#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * _realloc - function that allocates memory for an array
- * @ptr: number of elements to be allocated
- * @old_size: size
- * @new_size: new size
+ * _prntstr - prints a string
  *
- * Return: pointer to allocated memory
+ * @s: string to print
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void _prntstr(char *s)
 {
-	int j;
-	void *s;
+	while (*s)
+		_putchar(*s++);
+}
 
-	if (old_size == new_size)
-		return (ptr);
-	if (ptr == NULL)
+/**
+ * numstrchk - checks arg array to see if the are numeric strings, converts
+ * from ascii to byte int, and returns their length. Segfault on null pointer.
+ *
+ * @s: string to check
+ *
+ * Return: Length of string. Exit 98 if not numeric.
+ */
+long int numstrchk(char *s)
+{
+	long int len = 0;
+
+	if (*s == 0)
 	{
-		ptr = malloc(new_size);
-		return (ptr);
+		_prntstr("Error\n");
+		exit(98);
 	}
-	if (new_size == 0 && ptr != NULL)
+
+	while (*s)
 	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr != NULL)
-	{
-		s = malloc(new_size);
-		for (; j < min(old_size, new_size); j++)
+		if (*s < '0' || *s > '9')
 		{
-			((char *)s)[j] = ((char *)ptr)[j];
+			_prntstr("Error\n");
+			exit(98);
 		}
-		free(ptr);
+		*s -= '0';
+		len++;
+		s++;
 	}
-	else
-		return (NULL);
+	return (len);
+}
+
+/**
+ * _calloc_buffer - allocate a block of memory of size * num and init to '0'
+ *
+ * @num: number of elements to allocate
+ * @size: size of element
+ *
+ * Return: pointer to allocated space, exit 98 on failure
+ */
+void *_calloc_buffer(long int num, long int size)
+{
+	void *ret;
+	char *ptr;
+
+	ret = malloc(num * size);
+	if (ret == 0)
+	{
+		exit(98);
+	}
+
+	size = size * num;
+	ptr = ret;
+	ptr[--size] = 0;
+	while (size--)
+		ptr[size] = '0';
+
+	return (ret);
+}
+
+/**
+ * trimzero - moves pointer position to after last leading 0 in a string,
+ * or last zero if all zeros
+ *
+ * @s: char * we want to move
+ *
+ * Return: new position
+ */
+char *trimzero(char *s)
+{
+	while (*s == '0')
+		if (*(s + 1) != 0)
+			s++;
+		else
+			break;
 	return (s);
 }
 
 /**
- * min - minumum of a and b
- * @a: first integer
- * @b: second integer
+ * main - multiply two  positive integer strings of arbitrary size
  *
- * Return: return the minimum
+ * @ac: number of arguments
+ * @av: arugments
+ *
+ * Return: 0 if successful, 98 if failure
  */
-int min(unsigned int a, unsigned int b)
+int main(int ac, char **av)
 {
-	if (a < b)
-		return (a);
-	return (b);
+	long int len1, len2, lenres, i, j;
+	char *res;
+
+	if (ac != 3)
+	{
+		_prntstr("Error\n");
+		return (98);
+	}
+	av[2] = trimzero(av[2]);
+	av[1] = trimzero(av[1]);
+	if (*av[1] == '0' || *av[2] == '0')
+	{
+		_prntstr("0\n");
+		return (0);
+	}
+	len1 = numstrchk(av[1]);
+	len2 = numstrchk(av[2]);
+	lenres = len1 + len2;
+	res = _calloc_buffer(lenres + 1, sizeof(char));
+
+	for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
+		for (j = len2 - 1; j >= 0; j--, i--)
+		{
+			res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
+			res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
+			if (res[i] > '9')
+			{
+				res[i] -= 10;
+				res[i - 1]++;
+			}
+		}
+
+	if (*res == '0')
+		_prntstr(res + 1);
+	else
+		_prntstr(res);
+	_putchar('\n');
+	free(res);
+
+	return (0);
 }
