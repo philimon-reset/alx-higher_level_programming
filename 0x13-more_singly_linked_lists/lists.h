f51733c927a5d268a4a5ae682165d7e6fcb6b014
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-14 15:35:43 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,6 +22,8 @@ struct listint_s *next;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *find_listint_loop_fl(listint_t *head);
+size_t free_listint_safe(listint_t **h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 int _strlen(const char *s);
