09ede437c64af7ca3190e184317c9bf5da00694a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-14 22:49:53 +0300

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
   FROM tv_shows
   INNER JOIN tv_show_ratings
-  ON ttv_shows.id = tv_show_ratings.show_id
+  ON tv_shows.id = tv_show_ratings.show_id
   INNER JOIN tv_show_genres
   ON tv_shows.id = tv_show_genres.show_id
   INNER JOIN tv_genres
