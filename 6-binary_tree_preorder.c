a008aa7db5fef690d221a4a097be3cff75b5bb5f
philimon-reset <philimonderib2019@gmail.com>
2021-07-19 15:59:18 -0700

    preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is the root node
+ * @node: node to be checked
+ * 
+ * Return: 1 is it is and 0 if not
+ */
+ 
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
