e65f774673e2993a67781e8afc51e40bc7c73b05
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-19 22:02:33 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,3 +9,17 @@
  * Return: binary tree
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+
+	return (node);
+}
