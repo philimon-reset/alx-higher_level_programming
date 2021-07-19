ad23012f0e335d06ff6e1a7b08677b0557458410
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-19 23:36:21 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,25 +1,27 @@
 #include "binary_trees.h"
 
-
 /**
- * binary_tree_node - creates a binary tree node
+ * binary_tree_insert_left - creates a binary tree node
  * @parent: a pointer to the parent node
  * @value: the value to put in the new node
  *
  * Return: binary tree
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
 
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	node->n = value;
-
+	else if (parent->left != NULL)
+	{
+		parent->left->parent = node;
+		node->left = parent->left;
+	}
+	parent->left = node;
 	return (node);
 }
