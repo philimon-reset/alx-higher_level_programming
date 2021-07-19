d9fb5abf9a135f55585bf4477ff89b05ed7b107c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 00:17:26 +0300

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,25 +1,22 @@
 #include "binary_trees.h"
 
-
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: a pointer to the parent node
- * @value: the value to put in the new node
+ * binary_tree_delete - delete a binary tree node
+ * @tree: a pointer to the parent node
  *
- * Return: binary tree
+ * Return: void function
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node;
-
-	node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL)
-		return (NULL);
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	node->n = value;
-
-	return (node);
-}
+ 
+void binary_tree_delete(binary_tree_t *tree)
+ {
+	if (tree == NULL)
+	{
+		free(tree);
+		return;
+	}
+	else
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+ }
