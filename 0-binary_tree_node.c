cb09762ee25a1e2714cc1b5bbdecfa0f93b680ee
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-19 21:27:22 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,11 @@
 #include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: a pointer to the parent node
+ * @value: the value to put in the new node
+ *
+ * Return: binary tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
