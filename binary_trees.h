53a5fbd8ca7ff0dd8a2cd1fc61dc6b56ae2752ee
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-19 21:29:18 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,9 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,5 +23,6 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
