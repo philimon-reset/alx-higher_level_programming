48775eb80348485ab32cf2a575e3ca8d64670eb9
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 00:45:15 +0300

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if a node is a leaf
+ * binary_tree_is_root - checks if a node is the root node
  * @node: node to be checked
  * 
  * Return: 1 is it is and 0 if not
  */
  
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL)
+	if (node->parent = NULL)
 		return (1);
 	return (0);
 }
