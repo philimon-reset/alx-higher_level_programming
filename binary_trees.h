7537a758d5c204616c13641fe4967652d16a77af
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-19 22:10:20 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -24,5 +24,6 @@ typedef struct binary_tree_s
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
