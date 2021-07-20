37c986d644a0f793ceaa439a70840b759e65d783
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 03:19:53 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -31,6 +31,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 size_t height(const binary_tree_t *tree, size_t H);
 
 #endif /* _BINARY_TREES_H_ */
