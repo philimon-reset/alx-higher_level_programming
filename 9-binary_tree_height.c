0de93e9af6d063bcc58bf393d930f0171b716953
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 03:16:30 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,13 +1,12 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_height - measure the height of a binary tree
  * @tree: tree to be measured
- * 
+ *
  * Return: height of the tree
  */
- 
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t H = 0;
