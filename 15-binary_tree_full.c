353782a1c2b810a8fb65f1c724f3658c8536f020
philimon-reset <philimonderib2019@gmail.com>
2021-07-20 14:06:23 -0700

    full project

---
+++ 15-binary_tree_full.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - count possible balance factor
+ * @tree: tree to be search
+ *
+ * Return: balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int height_L = height_max(tree->left);
+	int height_R = height_max(tree->right);
+
+	if (tree == NULL)
+		return (0);
+	return (height_L - height_R);
+}
+
+/**
+ * max- max value of a and b
+ * @a: first value
+ * @b: second value
+ *
+ * Return: max value between a and b
+ */
+
+int max(int a, int b)
+{
+	if (a >= b)
+		return (a);
+	return (b);
+}
+
+/**
+ * height_max - max height
+ * @tree: tree to be checked
+ *
+ * Return: max height of tree
+ */
+
+int height_max(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + max(height_max(tree->left), height_max(tree->right)));
+}
