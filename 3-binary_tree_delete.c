b57691b32c3a30778c9d08987bf6ca5d99b20a57
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 03:13:43 +0300

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,9 +6,8 @@
  *
  * Return: void function
  */
- 
 void binary_tree_delete(binary_tree_t *tree)
- {
+{
 	if (tree == NULL)
 	{
 		free(tree);
@@ -19,4 +18,4 @@ void binary_tree_delete(binary_tree_t *tree)
 		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
 	}
- }
+}
