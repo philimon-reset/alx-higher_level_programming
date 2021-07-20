bef736f54f3768c7ddbefc4bfd8025ef086e3bc7
philimon-reset <philimonderib2019@gmail.com>
2021-07-20 10:29:46 -0700

    balance and nodes

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - count possible nodes of tree
+ * @tree: tree to be search
+ *
+ * Return: number of nodes
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		nodes++;
+	if (tree->left != NULL)
+	{
+		nodes++;
+		binary_tree_nodes(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		nodes++;
+		binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
