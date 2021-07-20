a9cf42be0bcc1c3e9fd0b482a4a60fe334b18a9d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 04:03:27 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,42 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measure the height of a binary tree
- * @tree: tree to be measured
+ * binary_tree_depth - depth of the tree
+ * @tree: tree to be checked
  *
- * Return: height of the tree
+ * Return: depth of tree
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t H = 0;
+	size_t D = 0;
 
 	if (tree == NULL)
 		return (0);
-	H = height(tree, H);
-	return (H);
-}
-
-/**
- * height - copy of height function due to prototype limits
- * @tree: tree to be checked
- * @H: height of tree
- *
- * Return: height of tree
- */
-size_t height(const binary_tree_t *tree, size_t H)
-{
-	if (tree->left == NULL && tree->right == NULL)
-		return (H);
-	if (tree->left != NULL)
+	while (tree->parent != NULL)
 	{
-		H++;
-		height(tree->left, H);
+		D++;
+		tree = tree->parent;
 	}
-	if (tree->right != NULL)
-	{
-		H++;
-		height(tree->right, H);
-	}
-	return (H);
+	return (D);
 }
