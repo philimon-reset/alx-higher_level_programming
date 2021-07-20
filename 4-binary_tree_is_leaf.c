5885f005e124f948f5752e5c89e65a637e493852
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 03:14:55 +0300

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,10 +3,10 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: node to be checked
- * 
+ *
  * Return: 1 is it is and 0 if not
  */
- 
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node->left == NULL && node->right == NULL)
