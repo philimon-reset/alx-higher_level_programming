ad47bf07468dbbacfd127fae056213e19a1f88cc
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-20 04:40:23 +0300

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,22 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - depth of the tree
+ * binary_tree_size - size of the tree
  * @tree: tree to be checked
  *
- * Return: depth of tree
+ * Return: size of tree
  */
 
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t D = 0;
+	size_t size = 1;
 
 	if (tree == NULL)
 		return (0);
-	while (tree->parent != NULL)
-	{
-		D++;
-		tree = tree->parent;
-	}
-	return (D);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
