28c3353bedea5b5a49ee2256020ad31902c9a752
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 12:44:10 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -46,5 +46,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int height_max(const binary_tree_t *tree);
 int max(int a, int b);
+binary_tree_t *sibling(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
