53cff0ffa17fda1a92c667cca53cfb8ad8166c2b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 15:25:46 +0300

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -20,7 +20,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL || tree->right != NULL)
+	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 	node_L = binary_tree_is_full(tree->left);
 	node_R = binary_tree_is_full(tree->right);
