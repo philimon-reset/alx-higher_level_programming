8ced9941193564db2416b147f0a261e4e16da34b
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:31:58 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,34 +9,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t H = 0;
-
 	if (tree == NULL)
 		return (0);
-	H = height(tree, H);
-	return (H);
-}
-
-/**
- * height - copy of height function due to prototype limits
- * @tree: tree to be checked
- * @H: height of tree
- *
- * Return: height of tree
- */
-size_t height(const binary_tree_t *tree, size_t H)
-{
-	if (tree->left == NULL && tree->right == NULL)
-		return (H);
-	if (tree->left != NULL)
-	{
-		H++;
-		height(tree->left, H);
-	}
-	if (tree->right != NULL)
-	{
-		H++;
-		height(tree->right, H);
-	}
-	return (H);
+	return (1 + max(height_max(tree->left), height_max(tree->right)));
 }
