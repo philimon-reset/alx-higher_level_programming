0e24ab4944ee9bc39e30290623e6f23b0ea1983c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:23:42 +0300

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,10 +10,12 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
-		func(tree->n);
-	if (tree->left != NULL)
-		binary_tree_preorder(tree->left, func);
-	if (tree->right != NULL)
-		binary_tree_preorder(tree->right, func);
-}
+	if (tree != NULL && func != NULL)
+	{
+		if (tree != NULL)
+			func(tree->n);
+		if (tree->left != NULL)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right != NULL)
+			binary_tree_preorder(tree->right, func);
+	}
