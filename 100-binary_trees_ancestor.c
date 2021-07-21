9df88a128dae605016c20a9d76da4cb8aa652187
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 16:47:05 +0300

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,11 +9,13 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
+	binary_tree_t *nodes;
 	if (first->parent == second || second->parent == NULL)
 		return (first->parent);
 	if (second->parent == first || first->parent == NULL)
 		return (second->parent);
 	if (first->parent == second->parent)
 		return (first->parent);
-	binary_trees_ancestor(first->parent, second->parent);
+	nodes = binary_trees_ancestor(first->parent, second->parent);
+	return (nodes);
 }
