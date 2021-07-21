b7d0a59a89b06cfda077fa5617c2cb6af659c873
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 12:29:43 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,14 +11,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	else
-	{
-		if (node->parent->left == NULL || node->parent->right == NULL)
-			return (NULL);
-		if (node->parent->left == node)
-			return (node->parent->right);
-		if (node->parent->right == node)
-			return (node->parent->left);
-	}
-	return (NULL);
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
 }
