ceb0be506ab5634d6c0b0e9078bcc40eb0e9b8de
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:27:29 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,24 +10,13 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-	{
-		func(tree->n);
-		return;
-	}
-	else
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
-		{
 			binary_tree_postorder(tree->left, func);
-		}
 		if (tree != NULL)
-		{
 			func(tree->n);
-		}
 		if (tree->right != NULL)
-		{
 			binary_tree_postorder(tree->right, func);
-		}
 	}
 }
