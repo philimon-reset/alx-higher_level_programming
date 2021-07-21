a4bb3ee25062da71cb8c7709945953361f5bfbb0
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:26:03 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,24 +10,13 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-	{
-		func(tree->n);
-		return;
-	}
-	else
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
-		{
 			binary_tree_inorder(tree->left, func);
-		}
 		if (tree != NULL)
-		{
 			func(tree->n);
-		}
 		if (tree->right != NULL)
-		{
 			binary_tree_inorder(tree->right, func);
-		}
 	}
 }
