90d6f709fdb6026b1bc40215d4d250aee9b14643
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:52:11 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,10 +9,12 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 1;
+	size_t nodes = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->parent == NULL)
+		nodes = 1;
 	if (tree->left != NULL)
 	{
 		nodes++;
