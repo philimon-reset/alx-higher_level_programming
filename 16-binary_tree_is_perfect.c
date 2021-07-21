63a10b4ea86b9dbeda65d4b50ff88e48c272f4f6
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 12:17:33 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,7 +21,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 			return (1);
 		if (tree->left == NULL || tree->right == NULL)
 			return (0);
-		return (binary_tree_is_perfect(tree->left) && 
+		return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
 	}
 	return (0);
