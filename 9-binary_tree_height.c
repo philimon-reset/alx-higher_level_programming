72cd8d62263f2fbf7a50818f8e42d6606de85596
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 13:32:22 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,3 +13,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	return (1 + max(height_max(tree->left), height_max(tree->right)));
 }
+
+/**
+ * max- max value of a and b
+ * @a: first value
+ * @b: second value
+ *
+ * Return: max value between a and b
+ */
+
+int max(int a, int b)
+{
+	if (a >= b)
+		return (a);
+	return (b);
+}
