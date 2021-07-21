3c386a4db68e3b9da299da46790f4803d2e7e6bc
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 12:29:02 +0300

    Update and rename 17-binary_tree_siblings.c to 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - check if there siblings
+ * @node: node to be search
+ *
+ * Return: sibling of node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	else
+	{
+		if (node->parent->left == NULL || node->parent->right == NULL)
+			return (NULL);
+		if (node->parent->left == node)
+			return (node->parent->right);
+		if (node->parent->right == node)
+			return (node->parent->left);
+	}
+	return (NULL);
+}
