c26c950a8f6de8172c45a2338319751858da6b81
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-21 15:54:13 +0300

    Update 100-binary_tree_ancestor.c

--- 100-binary_tree_ancestor.c
+++ 100-binary_tree_ancestor.c
@@ -1,25 +1,15 @@
-#include "binary_trees.h"
-
-
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: a pointer to the parent node
- * @value: the value to put in the new node
- *
- * Return: binary tree
+ * binary_trees_ancestor - calculate the ancestor of the nodes
+ * @first: first nodes
+ * @second: second nodes
+ * 
+ * Return: common ancestor of both
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+ 
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	binary_tree_t *node;
-
-	node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL)
-		return (NULL);
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
-	node->n = value;
-
-	return (node);
+	if (first->parent == second || second->parent == NULL)
+		return (second);
+	if (second->parent == first || first->parent == NULL)
+		return (first);
 }
