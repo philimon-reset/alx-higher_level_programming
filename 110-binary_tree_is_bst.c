e1b3511d974d0fd0caf52aa257ff3feb0863ddf4
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-22 22:02:22 +0300

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,29 +1,35 @@
 #include "binary_trees.h"
-
+#include <limits.h>
+int is_bst(const binary_tree_t *tree, int min, int max);
 
 /**
- * binary_tree_rotate_right - check if tree if complete
+ * binary_tree_is_bst - check if tree is bst
  * @tree: tree to be checked
  *
  * Return: 1 or 0
  */
-
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	binary_tree_t *left = tree->left;
-
 	if (tree == NULL)
-		return (NULL);
-	if (tree->left != NULL)
-	{
-		tree->left = left->right;
-		if (left->right != NULL)
-			left->right->parent = tree;
-		left->right = tree;
-		tree->parent = left;
-		left->parent = tree->parent;
-		tree = left;
-		return (tree);
-	}
-	return (NULL);
+		return (0);
+	return (is_bst(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * is_bst - check if tree is bst
+ * @tree: tree to be checked
+ * @min: min value
+ * @max: max value
+ *
+ * Return: 1 if it is and 0 if not
+ */
+
+int is_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (is_bst(tree->left, min, tree->n - 1) &&
+	is_bst(tree->right, tree->n + 1, max));
 }
