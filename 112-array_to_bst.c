2a284f1b7884c3d9d80f27c153793c4efe586ba7
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-22 23:45:50 +0300

    Update 112-array_to_bst.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -11,13 +11,15 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	size_t i = 0;
-	bst_t *tree = NULL;
+	bst_t *tree = NULL, *tree2 = NULL;
 
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		tree = bst_insert(&tree, array[i]);
+		if (tree2 == NULL)
+			continue;
 	}
 	return (tree);
 }
