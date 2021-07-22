cfb201ae5b48c2b160e4f386cb25a323f8825e44
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-22 19:03:35 +0300

    Update 104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,21 +10,18 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *left = tree->left, *right = tree->right, *temp;
+	binary_tree_t *left = tree->left;
 
 	if (tree == NULL)
 		return (NULL);
 	if (tree->left != NULL)
 	{
-		temp = tree;
 		tree->left = left->right;
-		tree->right = temp;
 		if (left->right != NULL)
 			left->right->parent = tree;
 		left->right = tree;
 		tree->parent = left;
-		left->parent = temp->parent;
-		tree = left;
+		left->parent = tree->parent;
 		return (left);
 	}
 	return (NULL);
