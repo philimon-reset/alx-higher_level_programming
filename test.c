675c34cb9a3be7e700b90108fa599c2339678c7d
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-22 22:10:16 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,24 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * launch_test - Test ancestor function and print informations
- *
- * @n1: First node
- * @n2: Second node
- */
-void launch_test(binary_tree_t *n1, binary_tree_t *n2)
-{
-    binary_tree_t *ancestor;
-
-    ancestor = binary_trees_ancestor(n1, n2);
-    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
-    if (!ancestor)
-        printf("(nil)\n");
-    else
-        printf("%d\n", ancestor->n);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -26,20 +8,24 @@ void launch_test(binary_tree_t *n1, binary_tree_t *n2)
 int main(void)
 {
     binary_tree_t *root;
+    int bst;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
+    root->right = binary_tree_node(root, 128);
     root->left->right = binary_tree_node(root->left, 54);
-    root->right->right = binary_tree_node(root->right, 128);
+    root->right->right = binary_tree_node(root, 402);
     root->left->left = binary_tree_node(root->left, 10);
-    root->right->left = binary_tree_node(root->right, 45);
-    root->right->right->left = binary_tree_node(root->right->right, 92);
-    root->right->right->right = binary_tree_node(root->right->right, 65);
-    binary_tree_print(root);
 
-    launch_test(root->left, root->right);
-    launch_test(root->right->left, root->right->right->right);
-    launch_test(root->right->right, root->right->right->right);
+    binary_tree_print(root);
+    bst = binary_tree_is_bst(root);
+    printf("Is %d bst: %d\n", root->n, bst);
+    bst = binary_tree_is_bst(root->left);
+    printf("Is %d bst: %d\n", root->left->n, bst);
+
+    root->right->left = binary_tree_node(root->right, 97);
+    binary_tree_print(root);
+    bst = binary_tree_is_bst(root);
+    printf("Is %d bst: %d\n", root->n, bst);
     return (0);
 }
