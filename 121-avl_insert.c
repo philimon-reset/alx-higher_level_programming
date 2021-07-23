1b39bbe57c062b80ccb7722f20880b4938c45f61
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-23 05:44:35 +0300

    Update 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,26 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * bst_is - check if tree is bst
- * @tree: tree to be checked
- * @min: min value
- * @max: max value
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: tree to be inserted
+ * @value: value to be inserted
  *
- * Return: 1 if it is and 0 if not
+ * Return: tree inserted
  */
 
-int bst_is(const binary_tree_t *tree, int min, int max)
+bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (tree == NULL)
-		return (1);
-	if (tree->n < min || tree->n > max)
-		return (0);
-	if (balance(tree) <= 1 && balance(tree) >= -1)
+	if (tree != NULL)
 	{
-		return (bst_is(tree->left, min, tree->n - 1) &&
-		bst_is(tree->right, tree->n + 1, max));
+		if (*tree == NULL)
+		{
+			(*tree) = binary_tree_node(*tree, value);
+			return (*tree);
+		}
+		if (value == (*tree)->n)
+			return (NULL);
+		if (value < (*tree)->n)
+		{
+			if ((*tree)->left == NULL)
+			{
+				(*tree)->left = binary_tree_node(*tree, value);
+				return ((*tree)->left);
+			}
+			else
+				return (bst_insert(&((*tree)->left), value));
+		}
+		else
+		{
+			if ((*tree)->right == NULL)
+			{
+				(*tree)->right = binary_tree_node(*tree, value);
+				return ((*tree)->right);
+			}
+			else
+				return (bst_insert(&((*tree)->right), value));
+		}
 	}
-	return (0);
+	return (NULL);
 }
 
 /**
@@ -32,7 +52,50 @@ int bst_is(const binary_tree_t *tree, int min, int max)
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	if (tree == NULL)
-		return (0);
-	return (bst_is(tree, INT_MIN, INT_MAX));
+	avl_t *node = NULL;
+
+	if (tree != NULL)
+	{
+		node = bst_insert(tree, value);
+		if (node)
+		{
+			Cbalance(tree, value);
+		}
+		return (node);
+	}
+	return (NULL);
+}
+
+/**
+ * Cbalance - check the balance
+ * @tree: node to be check
+ * @value: value to be checked
+ *
+ * Return: void function
+ */
+
+void Cbalance(avl_t **tree, int value)
+{
+	if ((balance(*tree) > 1) && (value < (*tree)->left->n))
+	{
+		*tree = binary_tree_rotate_right(*tree);
+		return;
+	}
+	if ((balance(*tree) > 1) && (value > (*tree)->left->n))
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left);
+		*tree = binary_tree_rotate_right(*tree);
+		return;
+	}
+	if ((balance(*tree) < -1) && (value > (*tree)->right->n))
+	{
+		*tree = binary_tree_rotate_left(*tree);
+		return;
+	}
+	if ((balance(*tree) < -1) && (value < (*tree)->right->n))
+	{
+		(*tree)->right = binary_tree_rotate_right((*tree)->right);
+		*tree = binary_tree_rotate_left(*tree);
+		return;
+	}
 }
