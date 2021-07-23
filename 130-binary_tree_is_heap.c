4b482a24d3403dc0a29be12732a4b48f0fcaac1a
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-07-23 05:22:58 +0300

    Update 130-binary_tree_is_heap.c

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,25 +1,62 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - array_to_bst
- * @array: array to be checked
- * @size: size
+ * count_nodes - counts the number of nodes in a tree
+ * @root: root node of the tree
  *
- * Return: bst
+ * Return: number of nodes
  */
-
-bst_t *array_to_bst(int *array, size_t size)
+int count_nodes(const binary_tree_t *root)
 {
-	size_t i = 0;
-	bst_t *tree = NULL, *tree2 = NULL;
+	if (root == NULL)
+		return (0);
+	return (1 + count_nodes(root->left) + count_nodes(root->right));
+}
 
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
+/**
+ * is_complete -  checks if a binary tree is complete
+ * @root: root node of the tree
+ * @index: index of the node
+ * @number_nodes: number of nodes in the whole tree
+ *
+ * Return: 1 for true, 0 otherwise
+ */
+int is_complete(binary_tree_t *root, int index, int number_nodes)
+{
+	int left_complete = 0, right_complete = 0;
+
+	if (root == NULL)
+		return (1);
+	if (index >= number_nodes)
+		return (0);
+	left_complete = is_complete(root->left, 2 * index + 1, number_nodes);
+	right_complete = is_complete(root->right, 2 * index + 2, number_nodes);
+	return (left_complete && right_complete);
+}
+
+/**
+ * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid Max Binary Heap, and 0 otherwise
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	int max_left, max_right;
+
+	if (tree == NULL)
+		return (0);
+	if (!is_complete((binary_tree_t *)tree, 0, count_nodes(tree)))
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	if (tree->right == NULL)
+		return (tree->n > tree->left->n);
+	if (tree->n > tree->right->n && tree->n > tree->left->n)
 	{
-		tree2 = bst_insert(&tree, array[i]);
-		if (tree2 == NULL)
-			continue;
+		max_left = binary_tree_is_heap(tree->left);
+		max_right = binary_tree_is_heap(tree->right);
+		return (max_left && max_right);
 	}
-	return (tree);
+	return (0);
 }
