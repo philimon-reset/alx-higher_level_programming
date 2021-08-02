ccfc91fef0b4c189080fab832d32a54598f9fb6a
philimon-reset <philimonderib2019@gmail.com>
2021-08-02 13:31:28 +0000

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-function add(a, b) {
-  let c = Number(a) + Number(b);
+function add (a, b) {
+  const c = Number(a) + Number(b);
   console.log(c);
 }
 add(argv[2], argv[3]);
