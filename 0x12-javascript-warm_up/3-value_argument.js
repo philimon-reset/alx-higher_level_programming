ee4c991d73b84c22401bf5de2ff1e966d348700f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-02 15:16:39 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-try {
-  console.log(argv[3]);
-}
-catch (error) {
+if (argv[3] === undefined) {
   console.log('No argument');
+} else {
+  console.log(argv[3]);
 }
