fe17139a2afbae894953b6894466be20dd99f2a4
philimon-reset <philimonderib2019@gmail.com>
2021-08-02 13:21:33 +0000

    semi is awesome

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 const { argv } = require('process');
 
 if (argv[2] === undefined) {
-        console.log('Missing size');
+  console.log('Missing size');
 } else {
-        let rip = "X".repeat(argv[2])
-        for (let i = 0; i < argv[2]; i++) {
-                console.log(rip);
-        }
+  const rip = 'X'.repeat(argv[2]);
+  for (let i = 0; i < argv[2]; i++) {
+    console.log(rip);
+  }
 }
