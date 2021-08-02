7bf772bcbbfed115f4b6926c6f2674ace3fc26f2
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-02 16:25:38 +0300

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (argv[2] === undefined) {
+if (argv[2] === undefined || !(Number(argv[2]))) {
   console.log('Missing size');
 } else {
   const rip = 'X'.repeat(argv[2]);
