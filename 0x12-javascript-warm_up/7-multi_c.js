39961f320509c7f7cc70b36953e8fcecb61d5665
philimon-reset <philimonderib2019@gmail.com>
2021-08-02 13:09:00 +0000

    pusg

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const { argv } = require('process');
 
 if (argv[2] === undefined) {
-        console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < argv[2]; i++) {
     console.log('C is fun');
