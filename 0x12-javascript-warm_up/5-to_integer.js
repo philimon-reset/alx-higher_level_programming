049eb05495610b07c7edda81f6438d6f6f0e891c
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-02 16:07:30 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (Number(argv[2]) === NaN) {
+if (Number(argv[2]) !== NaN) {
   console.log('Not a number');
 } else {
   console.log(Number(argv[2]));
