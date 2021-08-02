9b0b9290ad248ab818199df7c1ea70608389b9fb
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-02 16:29:41 +0300

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-console.log(argv[2] + ' is ' + argv[3]);
+function add(a, b) {
+  let c = Number(a) + Number(b);
+  console.log(c);
+}
+add(argv[2], argv[3]);
