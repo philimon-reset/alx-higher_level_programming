771d3c9bcffddedced56e9616052b7b9f7736518
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-02 17:40:51 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,15 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-console.log(argv[2] + ' is ' + argv[3]);
+if (isNaN(isNaN(argv[2])) || argv.length <= 3) {
+  console.log(0);
+} else {
+  let num = [];
+  argv.forEach(x => num.push(Number(x)));
+  num.shift()
+  num.shift()
+  num.sort(function(a, b) {
+          return b - a;
+  });
+  console.log(num[1]);
+}
