7abd55e4920deee08149d5237053900f1b1f4b69
philimon-reset <philimonderib2019@gmail.com>
2021-08-02 14:41:56 +0000

    ad

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@ const { argv } = require('process');
 if (isNaN(isNaN(argv[2])) || argv.length <= 3) {
   console.log(0);
 } else {
-  let num = [];
+  const num = [];
   argv.forEach(x => num.push(Number(x)));
-  num.shift()
-  num.shift()
-  num.sort(function(a, b) {
-          return b - a;
+  num.shift();
+  num.shift();
+  num.sort(function (a, b) {
+    return b - a;
   });
   console.log(num[1]);
 }
