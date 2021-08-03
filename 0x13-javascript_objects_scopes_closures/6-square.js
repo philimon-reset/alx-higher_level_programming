6afc4f9dee65fe9a3d3f604ffd223107759e3d83
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 15:13:22 +0300

    test

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square.js');
 
-class Square extends Rectangle {
+class Square extends Square {
   constructor (size) {
     super(size, size);
     this.size = size;
