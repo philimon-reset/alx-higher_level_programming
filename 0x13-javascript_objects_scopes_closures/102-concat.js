a704fee0373ee3f883528bdeffefbff35f189ae1
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 20:38:52 +0300

    concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  number++;
-  theFunction(number);
-};
+const { argv } = require('process');
+
+const fs = require('fs');
+const full = fs.readFileSync(String(argv[2]), 'utf8');
+const full2 = fs.readFileSync(String(argv[3]),'utf8');
+fs.writeFileSync(String(argv[4]), full)
+fs.writeFileSync(String(argv[4]), full2, {flag:'a+'})
