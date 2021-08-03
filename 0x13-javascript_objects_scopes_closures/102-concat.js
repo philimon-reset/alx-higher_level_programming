f651165cdb1c2bb03d455cf8ba80f1a6fcfb3188
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 20:40:05 +0300

    added

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,6 +3,6 @@ const { argv } = require('process');
 
 const fs = require('fs');
 const full = fs.readFileSync(String(argv[2]), 'utf8');
-const full2 = fs.readFileSync(String(argv[3]),'utf8');
-fs.writeFileSync(String(argv[4]), full)
-fs.writeFileSync(String(argv[4]), full2, {flag:'a+'})
+const full2 = fs.readFileSync(String(argv[3]), 'utf8');
+fs.writeFileSync(String(argv[4]), full);
+fs.writeFileSync(String(argv[4]), full2, { flag: 'a+' });
