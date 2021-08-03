96f359790660ce1e66493857400f3ac04318cceb
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 14:39:45 +0300

    6th question

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,21 @@
 #!/usr/bin/node
-const la = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-la.forEach(x => console.log(x));
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      const rip = 'C'.repeat(this.size);
+      for (let i = 0; i < this.size; i++) {
+        console.log(rip);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
