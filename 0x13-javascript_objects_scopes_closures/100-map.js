7864246d26fc10931ba230b25bde383e9b836a37
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 16:03:00 +0300

    100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-{
-  myVar = 333;
-}
+list = require('./100-data.js').list;
+console.log(list);
+const indexed = list.map(function (x, index) { return (x * index); });
+console.log(indexed);
