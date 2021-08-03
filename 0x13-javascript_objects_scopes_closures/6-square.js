343b731c162d154c35739a53b362d7217aa19ed1
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 19:14:47 +0300

    addeD

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,13 +7,10 @@ class Square extends ASquare {
         console.log('C'.repeat(this.width));
       }
     } else {
-      super.print();
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
     }
   }
 }
 module.exports = Square;
-
-const s1 = new Square(4);
-s1.charPrint();
-
-s1.charPrint('C');
