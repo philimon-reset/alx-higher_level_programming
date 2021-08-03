27738f8a3063ac880cac42c3de193db35cffdade
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 17:18:11 +0300

    bad solution

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+const dict = require('./101-data.js').dict;
+const ND = {};
+const Empty = Object.entries(dict);
+
+console.log(dict);
+for (let i = 0; i < Empty.length; i++) {
+  ND[Empty[i][1]] = [];
+}
+for (const [key, value] of Object.entries(dict)) {
+  if (key in ND[value]) {
+    continue;
+  } else {
+    ND[value].push(key);
   }
-};
+}
+console.log(ND);
