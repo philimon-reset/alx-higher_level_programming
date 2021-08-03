e384641d6308b69d588b43bbec885778f982d98b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 15:33:43 +0300

    logme

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process');
-
-function add (a, b) {
-  const c = Number(a) + Number(b);
-  console.log(c);
-}
-add(argv[2], argv[3]);
+exports.logMe = function (item) {
+  if (typeof (this.counter) === 'undefined') {
+    this.counter = 0;
+  }
+  console.log(this.counter + ' : ' + item);
+  this.counter++;
+};
