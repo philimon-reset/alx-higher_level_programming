5ca2ccd29fc83085b188ac4e306bf60ba133737f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-03 22:06:47 +0300

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -12,15 +11,14 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_size(listint_t *head);
-
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
