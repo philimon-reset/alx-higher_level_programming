fd01b42c3c9e7e020c1a8a2fd7fde448631f4ad9
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 17:47:42 +0300

    added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,8 @@ const ASquare = require('./5-square.js');
 class Square extends ASquare {
   charPrint (c) {
     if (c !== undefined) {
-      const rip = 'C'.repeat(this.size);
-      for (let i = 0; i < this.size; i++) {
+      const rip = 'C'.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
         console.log(rip);
       }
     } else {
