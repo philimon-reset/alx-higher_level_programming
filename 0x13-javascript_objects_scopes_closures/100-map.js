9702a9a7874059364b56dc961e22f16e1bfe2bfc
Philimon Derib <philimonderib2019@gmail.com>
2021-08-03 16:07:11 +0300

    test

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const indexer = require('./100-data.js').list;
-console.log(indexer);
 const indexed = indexer.map(function (x, index) { return (x * index); });
+console.log(`${indexer}`);
 console.log(indexed);
