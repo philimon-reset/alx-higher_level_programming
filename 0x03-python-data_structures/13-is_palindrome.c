7ca37d97d612ef59979ff5df69aabdba464f9b01
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-03 22:16:59 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,8 +13,10 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
 	int size = check_size(temp), i = 0;
-	int check_A[size];
+        int *check_A = malloc(sizeof(temp) * size);
 
+        if (check_A == NULL)
+		return (0);
 	if (size == 0)
 		return (0);
 	while (temp != NULL)
