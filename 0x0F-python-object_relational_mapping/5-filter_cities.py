013b0aabc98f47cfd7456f2cf269cd40a129119b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-06 16:05:10 +0300

    join

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cur = conn.cursor()
+# HERE I have to know SQL to grab all states in my database
+user = sys.argv[4].strip("'")
+try:
+    cur.execute(
+        """SELECT cities.name
+        FROM cities
+        WHERE cities.state_id in (SELECT states.id
+        FROM states
+        WHERE states.name = '{}')
+        ORDER BY cities.id ASC""".format(user))
+    conn.commit()
+except:
+    conn.rollback()
+print([x for x in cur.fetchall()].join(", "))
+
+
+cur.close()
+conn.close()
