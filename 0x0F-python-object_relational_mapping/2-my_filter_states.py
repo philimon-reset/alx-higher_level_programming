e778f6fdff1a0eb5e9b0e504961dc0870bb54e9e
Philimon Derib <philimonderib2019@gmail.com>
2021-08-06 12:20:32 +0300

    2nd question

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,17 +6,10 @@ conn = MySQLdb.connect(host="localhost", port=3306,
                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 cur = conn.cursor()
 # HERE I have to know SQL to grab all states in my database
-try:
-    tryed = sys.argv[4]
-    cur.execute(
-        """SELECT * FROM states WHERE REGEXP_LIKE(`name`, tryed, 'c') = 1 ORDER BY states.id ASC""")
+cur.execute(
+    f"SELECT * FROM states WHERE REGEXP_LIKE(`name`, {sys.argv[4]}, 'c') = 1 ORDER BY states.id ASC")
 
-    conn.commit()
-except:
-    conn.rollback()
-
-query_rows = cur.fetchall()
-for row in query_rows:
+for row in cur.fetchall():
     print(row)
 cur.close()
 conn.close()
