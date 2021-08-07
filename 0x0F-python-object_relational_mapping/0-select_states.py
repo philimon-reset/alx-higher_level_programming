7e14e3bd0e0806a563c338fa9e53416a1e5bb720
Philimon Derib <philimonderib2019@gmail.com>
2021-08-07 19:20:08 +0300

    pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 
 if __name__ == '__main__':
+    import sys
+    import MySQLdb
     conn = MySQLdb.connect(
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
