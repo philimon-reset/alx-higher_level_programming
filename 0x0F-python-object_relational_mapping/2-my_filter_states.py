0c9b186a8844f266ddef9103138d721cebe29657
Philimon Derib <philimonderib2019@gmail.com>
2021-08-07 19:28:53 +0300

    pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute(
-        """SELECT * FROM states WHERE `name` = {} ORDER BY
+        """SELECT * FROM states WHERE BINARY `name` = {} ORDER BY
         states.id ASC""".format(sys.argv[4]))
 
     for row in cur.fetchall():
