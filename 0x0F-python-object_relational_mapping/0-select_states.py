07cecf8beb870c4561a01410646482332bd100e5
Philimon Derib <philimonderib2019@gmail.com>
2021-08-07 19:21:15 +0300

    pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,7 @@ if __name__ == '__main__':
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
-    try:
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        conn.commit()
-    except:
-        conn.rollback()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
