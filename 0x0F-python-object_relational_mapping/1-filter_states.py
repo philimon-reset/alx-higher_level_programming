631c121765f4650d6dd46720ab87203f88330db0
Philimon Derib <philimonderib2019@gmail.com>
2021-08-07 19:39:41 +0300

    pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute(
-        """SELECT * FROM states WHERE REGEXP_LIKE(`name`, '^N', 'c') = 1
+        """SELECT * FROM states WHERE BINARY REGEXP_LIKE(`name`, '^N', 'c') = 1
         ORDER BY states.id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
