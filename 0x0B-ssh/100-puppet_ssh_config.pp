b5facc90089fda1cd10c0df5b00f581512e07abf
Philimon Derib <philimonderib2019@gmail.com>
2021-08-15 21:06:46 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,12 @@
 # puppet
 file_line { 'Ciphers':
+  ensure => created,
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton'
+}
+
+file_line { 'Ciphers':
+  ensure => created,
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
 }
