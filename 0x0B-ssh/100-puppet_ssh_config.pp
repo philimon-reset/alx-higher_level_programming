36daba0f226d14fea75910a9b1a087df1829b313
Philimon Derib <philimonderib2019@gmail.com>
2021-08-15 21:26:50 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # puppet
-file_line {:
+file_line {"Declare identity file":
   ensure => created,
   path => "/etc/ssh/ssh_config",
   line => "IdentityFile ~/.ssh/holberton"
 }
 
-file_line {:
+file_line {"Turn off passwd auth":
   ensure => created,
   path => "/etc/ssh/ssh_config",
   line => "PasswordAuthentication no"
