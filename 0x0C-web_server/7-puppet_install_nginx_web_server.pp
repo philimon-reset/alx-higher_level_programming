1e7a8d7c5fda33f5d2296896de9f9ada634bbe92
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-18 23:28:06 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+# setup nginx
+
+package {'nginx':
+  ensure  => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+  content => 'Holberton School for the win!',
+}
+
+file_line {'configure redirection':
+  path => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line => "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+}
+
+service {'nginx':
+  ensure  => running,
+}
