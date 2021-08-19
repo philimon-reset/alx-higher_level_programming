5f92325d4bea497d770bfd0f1f8421b6bc5f840c
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 16:18:08 +0300

    add

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl for size
-curl -Ls -X GET "0.0.0.0:5000/catch_me"
+curl -s -i -w "You got me!" --output /dev/null "0.0.0.0:5000/catch_me"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,9 +6,5 @@
 def find_peak(list_of_integers):
     """ try to find the peak """
     if not len(list_of_integers):
-        return (None)
-    max = list_of_integers[0]
-    for x in range(len(list_of_integers) - 1):
-        if list_of_integers[x + 1] >= list_of_integers[x]:
-            max = list_of_integers[x + 1]
-    return max
+                return (None)
+    return max(list_of_integers)
