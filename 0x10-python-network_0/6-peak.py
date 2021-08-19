15f20616b7e5a83c04537dce3390f2f181803bc5
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 16:20:44 +0300

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,11 +8,5 @@ from random import randint
 def find_peak(list_of_integers):
     """ try to find the peak """
     if not len(list_of_integers):
-        return (None)
+                return (None)
     return max(list_of_integers)
-
-
-if __name__ == '__main__':
-    big_boi = [randint(-10000, 10000) for i in range(10000000)]
-    start = time.time()
-    print(find_peak(big_boi), time.time() - start)
