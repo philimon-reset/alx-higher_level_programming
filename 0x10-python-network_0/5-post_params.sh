890339e2c2b2e20820448ad01f77c320af7f964e
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 17:51:07 +0300

    final advanced

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl for setting value
-curl "$1" -X POST -i -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl "$1" -X POST -s -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
