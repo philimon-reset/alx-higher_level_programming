b6cb7c1a8a36f83d20a007ea5332df0063a1e45e
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 15:43:59 +0300

    try

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
+""" Module starting point
+    """
+
+
 def find_peak(list_of_integers):
-        if not len(list_of_integers):
-                return (None)
-        max = list_of_integers[0]
-        for x in range(len(list_of_integers) - 1):
-                if list_of_integers[x + 1] >= list_of_integers[x]:
-                        max = list_of_integers[x + 1]
-        return max
-
-
-if __name__ == '__main__':
-        print(find_peak([1, 2, 4, 6, 3]))
-        print(find_peak([4, 2, 1, 2, 3, 1]))
-        print(find_peak([2, 2, 2]))
-        print(find_peak([]))
-        print(find_peak([-2, -4, 2, 1]))
-        print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+    """ try to find the peak """
+    if not len(list_of_integers):
+        return (None)
+    max = list_of_integers[0]
+    for x in range(len(list_of_integers) - 1):
+        if list_of_integers[x + 1] >= list_of_integers[x]:
+            max = list_of_integers[x + 1]
+    return max
