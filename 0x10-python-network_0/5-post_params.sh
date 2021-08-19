c3d8facd34f38050fa3de63af2dee2deececec07
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 17:07:10 +0300

    post values

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl for setting value
-curl -i -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -i -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
