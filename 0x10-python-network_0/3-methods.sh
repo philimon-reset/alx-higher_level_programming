4aa373470660e6b67bfcac0105448fe83ca97534
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 15:12:59 +0300

    test

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl for getting available methods
-curl -s -X OPTIONS "$1" -o /dev/stdout | grep "Allow:" | cut -d ":" -f 3-
+curl -s -X OPTIONS "$1" 2>&1 | grep "Allow:" | cut -d ":" -f 3-
