b625469e460fedb20d2ba426dcbfbd2a0885d0cf
Philimon Derib <philimonderib2019@gmail.com>
2021-08-19 16:40:21 +0300

    add

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # curl for size
-curl -s -i -w "You got me!" --output /dev/null "0.0.0.0:5000/catch_me"
+curl -sb -H "Accept: application/json" "0.0.0.0:5000/catch_me"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import time
+from random import randint
 """ Module starting point
     """
 
@@ -8,3 +10,9 @@ def find_peak(list_of_integers):
     if not len(list_of_integers):
                 return (None)
     return max(list_of_integers)
+
+
+if __name__ == '__main__':
+    big_boi = [randint(-10000, 10000) for i in range(10000000)]
+    start = time.time()
+    print(find_peak(big_boi), time.time() - start)
