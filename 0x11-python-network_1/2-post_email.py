64331af36fe26503dcc5081d0ea781b2abf687cf
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 05:08:41 +0300

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""takes in a URL and an email, sends a POST request to the passed URL"""
+from urllib import request
+from urllib import parse
+from sys import argv
+
+value = {'email':argv[2]}
+data = parse.urlencode(value)
+data = data.encode('ascii')
+req = request.Request(argv[1], data)
+with request.urlopen(req) as response:
+        test = response.read()
+        print(test)
