1036fa2a4d1097ab9b1c0a1b4ac8982b054c4091
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:37:29 +0300

    email version 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,6 @@ import requests
 from sys import argv
 
 data = {'email':argv[2]}
-with requests.get(argv[1], auth=('user', 'pass'), params=data) as response:
+with requests.get(argv[1], params=data) as response:
         html = response.text
         print(html)
