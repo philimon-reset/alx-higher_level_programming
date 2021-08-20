cbe4a7028eaa50885e74b53e4e8e4f3bd15e294b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 08:41:34 +0300

    pep8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""Python script that takes your GitHub credentials to display your id"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+        req = requests.get("https://github.com", auth=(argv[1], argv[2]))
+        print(req.json())
+
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """ sends a POST request to http://0.0.0.0:5000/search_user"""
-import requests
-from sys import argv
 
-try:
-        my_obj = {'q':argv[1]}
-except:
-        my_obj = {'q': ""}
-r = requests.post("http://0.0.0.0:5000/search_user", data = my_obj)
-if len(r.json()) == 0:
-        print("No result")
-else:
-        html = r.json()
-        print("[{}] {}".format(html["id"], html["name"]))
+if __name__ == '__main__':
+        import requests
+        from sys import argv
+        try:
+                my_obj = {'q': argv[1]}
+        except:
+                my_obj = {'q': ""}
+        r = requests.post("http://0.0.0.0:5000/search_user", data=my_obj)
+        try:
+                if len(r.json()) == 0:
+                        print("No result")
+                else:
+                        html = r.json()
+                        print("[{}] {}".format(html["id"], html["name"]))
+        except:
+                print("Not a valid JSON")
