e54c6b4b86c8813e4ab03746fd918d277e2408f1
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 08:13:04 +0300

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+""" sends a POST request to http://0.0.0.0:5000/search_user"""
+import requests
+from sys import argv
+
+try:
+        my_obj = {'q':argv[2]}
+except:
+        my_obj = {'q': ""}
+r = requests.post("http://0.0.0.0:5000/search_user", data = my_obj)
+print(r.json)
