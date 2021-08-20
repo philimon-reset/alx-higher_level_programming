d72912c7b315d42b1449ac362b4f4d9b762d71f1
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:40:19 +0300

    email version 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """displays the value of the variable X-Request-Id"""
 import requests
+from sys import argv
 
 with requests.get(argv[1], auth=('user', 'pass')) as response:
     html = response.POST('https://httpbin.org/post', data = {'email':argv[2]})
