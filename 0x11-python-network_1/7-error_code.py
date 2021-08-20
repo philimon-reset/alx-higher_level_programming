e264c237305a220106e98402d284998d4950b64d
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:59:38 +0300

    status code requestS

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 """displays the body of the response."""
 import requests
 
-with requests.get("https://intranet.hbtn.io/status", auth=('user', 'pass')) as response:
+with requests.get(argv[1]) as response:
         if response.status_code >= 400:
                 print("Error code: {}".format(response.status_code))
         else:
