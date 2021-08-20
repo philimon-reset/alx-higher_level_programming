69fd636f51071b657e3470898d04f230d17e11a4
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:18:06 +0300

    test

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""fetche https://intranet.hbtn.io/status"""
+import requests
+
+with requests.get("https://intranet.hbtn.io/status", auth=('user', 'pass')) as response:
+    html = response.content()
+    print(html)
+#     print("Body response:")
+#     print("\t- type: {}".format(type(html)))
+#     print("\t- content: {}".format(html))
+#     html = html.decode("UTF-8")
+#     print("\t- utf8 content: {}".format(html))
