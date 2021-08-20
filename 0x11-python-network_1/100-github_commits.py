d68ca30eb8fff5cbdaf2ff51706b835219341009
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 10:09:14 +0300

    test final

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,8 @@ from sys import argv
 if __name__ == '__main__':
     limit = {"per_page": 10}
     req = requests.get(
-        "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    , params=limit)
+        "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]),
+        params=limit)
     html = req.json()
     for i in html:
         print("{}: {}".format(i.get("sha"),
