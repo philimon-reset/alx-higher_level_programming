4e40cb3b33593f29e2d2b1b1a9d025cead1f1272
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 05:55:03 +0300

    test

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""takes in a URL and an email, sends a POST request to the passed URL"""
+from urllib import request
+from urllib.error import HTTPError
+from sys import argv
+
+with request.urlopen(argv[1s]) as response:
+        test = response.read().decode('UTF-8')
+        print(test)
