f7fc32c2d07b1b1c526a4c0be8bb3a20a7c319b8
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 08:55:00 +0300

    editing for checker

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,8 @@
 """fetche https://intranet.hbtn.io/status"""
 if __name__ == '__main__':
     import requests
-    with requests.get("https://intranet.hbtn.io/status") as response:
-        html = response.text
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
+    response = requests.get("https://intranet.hbtn.io/status")
+    html = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
