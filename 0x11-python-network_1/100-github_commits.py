3cb71f896a01ea7cced8c9611b31a03dc5ddb8b3
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-20 16:12:13 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,9 +4,11 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
+    limit = {"per_page": 10}
     req = requests.get(
-        "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]))
+        "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    , params=limit)
     html = req.json()
-    for i in range(10):
-        print("{}: {}".format(html[i].get("sha"),
-              html[i].get("commit").get("author").get("name")))
+    for i in html:
+        print("{}: {}".format(i.get("sha"),
+              i.get("commit").get("author").get("name")))
