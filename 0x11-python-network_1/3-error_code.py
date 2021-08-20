15d31bebe5fd7940472f49a8effceb072666907b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 05:57:15 +0300

    test

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,6 +4,6 @@ from urllib import request
 from urllib.error import HTTPError
 from sys import argv
 
-with request.urlopen(argv[1s]) as response:
+with request.urlopen(argv[1]) as response:
         test = response.read().decode('UTF-8')
         print(test)
