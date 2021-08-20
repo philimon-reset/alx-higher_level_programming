985124d704015308814c1c12c204455b15d93999
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 09:20:12 +0300

    test

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-"""[summary]
-    """
+"""Please list 10 commits"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    req = requests.get("https://api.github.com/repos/{}/{}".format(argv[2], argv[1]))
+    html = req.json()
+    response = requests.get(html["commits_url"])
+    html2 = response.json()
+    print(html)
