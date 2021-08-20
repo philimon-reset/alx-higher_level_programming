e93abf08e083227f7886f833fd757e74408f514c
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 08:01:02 +0300

    status code requestS

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,9 +2,9 @@
 """displays the body of the response."""
 import requests
 
-with requests.get(argv[1]) as response:
-        if response.status_code >= 400:
-                print("Error code: {}".format(response.status_code))
-        else:
-                html = response.text
-                print(html)
+response = requests.get(argv[1])
+if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+else:
+        html = response.text
+        print(html)
