8f9e3c0c2c2dd642f0216f98db389b118b02ddbb
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 04:57:13 +0300

    2nd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 from urllib import request
 
 with request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        html = html.decode("UTF-8")
-        print("\t- utf8 content: {}".format(html))
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    html = html.decode("UTF-8")
+    print("\t- utf8 content: {}".format(html))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""displays the value of the X-Request-Id variable"""
+from urllib import request
+from sys import argv
+
+with request.urlopen(argv[1]) as response:
+    print(dict(response.info()).get("X-Request-Id"))
