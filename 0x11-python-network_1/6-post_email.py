8f17c8599bd8a0b58c64d524b6b2bef77eb1570e
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:34:16 +0300

    email version 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""displays the value of the variable X-Request-Id"""
+import requests
+
+data = {'email':argv[2]}
+with requests.get(argv[1], auth=('user', 'pass'), params=data) as response:
+        html = response.text
+        print(html)
