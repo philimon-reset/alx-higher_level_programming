d5308fffe9ceee74f9d9c80205dfd88564cd4f66
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 09:30:14 +0300

    test

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,7 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    req = requests.get("https://api.github.com/repos/{}/{}".format(argv[2], argv[1]))
+    req = requests.get("https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1]))
     html = req.json()
     response = requests.get(html["commits_url"])
     html2 = response.json()
