ee9271cb20563fb6dfdf19b160d9ab893601e324
Philimon Derib <philimonderib2019@gmail.com>
2021-08-20 07:29:34 +0300

    5th

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-        """[summary]
-        """
+"""displays the value of the variable X-Request-Id"""
+import requests
+
+with requests.get("https://intranet.hbtn.io/status", auth=('user', 'pass')) as response:
+    html = response.headers
+    print(html["X-Request-Id"])
