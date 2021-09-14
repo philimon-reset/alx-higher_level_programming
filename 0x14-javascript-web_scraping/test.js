a7a169591ffbbdd6cf6bb50230549f735ba7dd93
philimon-reset <philimonderib2019@gmail.com>
2021-09-14 17:47:55 +0300

    semistandard #

--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,11 +1,10 @@
-const { readFile } = require('fs')
+const { readFile } = require('fs');
 const { argv } = require('process');
 
-readFile(argv[2], 'utf8', (err,data) => {
-  if (err)
-  {
-    console.log(err)
+readFile(argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
