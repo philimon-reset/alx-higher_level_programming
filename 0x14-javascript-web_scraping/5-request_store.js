4d0d4d3b2681e0cd4b048c3624f768e74ace3c82
philimon-reset <philimonderib2019@gmail.com>
2021-09-14 17:54:15 +0300

    5-request_store #

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,27 +1,15 @@
 #!/usr/bin/node
-const { writeFile } = require('fs');
-const { argv } = require('process');
 const request = require('request');
+const { argv } = require('process');
+const { writeFile } = require('fs');
 
-function MakeRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        resolve(body);
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
-
-async function WriteOut () {
-  const result = await (MakeRequest(argv[2]));
-  writeFile(argv[3], result, 'utf-8', (err) => {
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  writeFile(argv[3], body, 'utf-8', err => {
     if (err) {
-      console.error(err);
+      console.log(err);
     }
   });
-}
-
-WriteOut();
+});
